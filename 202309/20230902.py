be33561b18f9caa0c54513a51578d31f1f3bca3a
Joseph Chiocchi <input.output@gmail.com>
2023-09-01 21:18:41 -0500

    solution for 20230902.py - https://leetcode.com/problems/extra-characters-in-a-string/

--- 202309/20230902.py
+++ 202309/20230902.py
@@ -1,3 +1,5 @@
+# from collections import Counter
+
 class Solution:
     """2707. Extra Characters in a String
 
@@ -11,4 +13,36 @@ class Solution:
     """
 
     def minExtraChar(self, s: str, dictionary: List[str]) -> int:
-        ...
+        """Extract the minimum number of extra characters
+
+        Proposed solution
+
+        Args:
+            s (str): The input string which must be broken up
+            dictionary (List of str): A literal dictionary of substrings to match input
+                `s` to
+        
+        Returns:
+            int: The minimum number of extra characters left over if you break up `s` optimally
+        """
+        # We'll frequently re-use this length, set to n
+        n = len(s)
+        # A holding list for all the letters we'll use
+        # Assume all characters are extra initially
+        dp = [n] * (n + 1)
+        # Empty string case
+        dp[0] = 0
+
+        # Loop through the positions in the string s
+        for i in range(1, n + 1):
+            # Check each word in the dictionary to see if it matches the substring ending at position i
+            for word in dictionary:
+                if i >= len(word) and s[i - len(word):i] == word:
+                    # If the word matches, update dp[i] with the minimum extra characters needed
+                    dp[i] = min(dp[i], dp[i - len(word)])
+
+            # Update dp[i] to consider not breaking the string at this position
+            dp[i] = min(dp[i], dp[i - 1] + 1)
+
+        # Return the value at the end of the dp array, which now represents the minimum extra characters
+        return dp[n]
