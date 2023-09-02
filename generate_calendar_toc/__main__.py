1432a59c337a2748ba322a10083f65df9d9bb5ff
Joseph Chiocchi <input.output@gmail.com>
2023-09-02 21:29:43 +0000

    nit

--- generate_calendar_toc/__main__.py
+++ generate_calendar_toc/__main__.py
@@ -60,7 +60,7 @@ for dir_ in matching_directories:
         # Does this day have a file to match?
         if idx in matching_files:
             path_ = matching_files[idx]
-            el.string.wrap(soup.new_tag("a", href = str(path_.name)))
+            el.string.wrap(soup.new_tag("a", href=str(path_.name)))
     # Set the align attribute to center, which works on markdown rendering for the <table />
     soup.table["align"] = "center"
     # The only content in the file will be the output of the HTMLCalendar
