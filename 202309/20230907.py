d5fc093d367f3dda350ff16b045d2b0fc8811fc3
Joseph Chiocchi <joe@yolk.cc>
2023-09-07 00:16:36 +0000

    Boilerplate for 20230907
    https://leetcode.com/problems/reverse-linked-list-ii/

---
+++ 202309/20230907.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/reverse-linked-list-ii/
+
+
+class Solution:
+    """92. Reverse Linked List II
+
+    Given the `head` of a singly linked list and two integers `left` and `right` where `left
+    <= right`, reverse the nodes of the list from position `left` to position `right`, and
+    return *the reversed list*.
+    """
+
+    def reverseBetween(
+        self, head: Optional[ListNode], left: int, right: int
+    ) -> Optional[ListNode]:
+        ...
