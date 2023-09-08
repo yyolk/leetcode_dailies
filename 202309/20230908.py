bb8aef355d439d1c568065b3aa98f153fbbba793
Joseph Chiocchi <joe@yolk.cc>
2023-09-08 00:16:30 +0000

    Boilerplate for 20230908
    https://leetcode.com/problems/pascals-triangle/

---
+++ 202309/20230908.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/pascals-triangle/
+
+
+class Solution:
+    """118. Pascal's Triangle
+
+    Given an integer `numRows`, return the first numRows of **Pascal's triangle**.
+
+    In **Pascal's triangle**, each number is the sum of the two numbers directly above it as
+    shown:
+
+    ![](https://upload.wikimedia.org/wikipedia/commons/0/0d/PascalTriangleAnimated2.gif)
+    """
+
+    def generate(self, numRows: int) -> List[List[int]]:
+        ...
