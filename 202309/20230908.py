78c7b58bb551f2fc44a663e51c2c9e80ce7574fe
Joseph Chiocchi <input.output@gmail.com>
2023-09-07 20:10:49 -0500

    solution 20230908.py
    - 38ms (beats 81.44%)
    - 16.17mb (beats 94.68%)

--- 202309/20230908.py
+++ 202309/20230908.py
@@ -13,4 +13,31 @@ class Solution:
     """
 
     def generate(self, numRows: int) -> List[List[int]]:
-        ...
+        """Generate a Pascal Triangle, producing the rows requested.
+
+        Proposed solution using nested for loops.
+        As all elements need to be generated the time-complexity is `O(n^2)`.
+
+        Args:
+            numRows (int): the requested number of rows
+
+        Returns:
+            List of List of int: A 2D List consisting of every row in Pascal's triangle,
+                index 0 is the top row
+        """
+        # Base list to hold the triangle's rows
+        triangle = []
+
+        for i in range(numRows):
+            # Create an empty row to fill for every loop
+            row = []
+            for j in range(i + 1):
+                # The first and last element per row in Pascal's triangle is always 1
+                if j == 0 or j == i:
+                    row.append(1)
+                # Every other element is the sum of the two numbers directly above it
+                else:
+                    row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+            triangle.append(row)
+
+        return triangle
