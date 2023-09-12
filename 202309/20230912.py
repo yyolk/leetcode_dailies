f0792eef46391d17654773ed32bb5cb28daa4659
Joseph Chiocchi <input.output@gmail.com>
2023-09-11 19:57:15 -0500

    solution 20230912.py
    - 119ms (beats 70.49%)
    - 17.2mb (beats 62.10%)

--- 202309/20230912.py
+++ 202309/20230912.py
@@ -1,4 +1,5 @@
 # https://leetcode.com/problems/minimum-deletions-to-make-character-frequencies-unique/
+from collections import Counter
 
 
 class Solution:
@@ -16,4 +17,32 @@ class Solution:
     """
 
     def minDeletions(self, s: str) -> int:
-        ...
+        """The minimum deletions to make character sequence input "good".
+
+        Proposed solution, tracking the frequency of each character and dynamic programming.
+
+        Args:
+            s (str): the input string of characters (s in string.ascii_lower)
+
+        Returns:
+            int: the minimum number of characters to delete to make s good
+        """
+        # Count occurences of each letter with a Counter
+        freqs = Counter(s)
+
+        # Track unique frequencies and total deletions
+        unique_freqs = set()
+        deletions = 0
+
+        # Iter over all freqs to fill in frequencies that are empty
+        # to make this a good string
+        for freq in freqs.values():
+            while freq in unique_freqs:
+                freq -= 1
+                deletions += 1
+            if freq > 0:
+                unique_freqs.add(freq)
+
+        # The number of deletions we've completed is the minimum possible
+        # for a good string
+        return deletions
