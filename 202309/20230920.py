7bf24a9c8124ec3a56028e7e716920802d2b6b8d
Joseph Chiocchi <input.output@gmail.com>
2023-09-19 20:08:41 -0500

    solution 20230920.py
    - 970ms (beats 66.67%)
    - 30.14mb (beats 98.66%)

--- 202309/20230920.py
+++ 202309/20230920.py
@@ -13,4 +13,59 @@ class Solution:
     """
 
     def minOperations(self, nums: List[int], x: int) -> int:
-        ...
+        """The minimum number of operations to reduce x to exactly 0
+
+        Proposed solution
+
+        Args:
+            nums (List of int): an integer array that leftmost or rightmost items can be
+                subtracted from x to reduce it to 0
+            x (int): work on number to determine the minimum operations to reduce to 0
+
+        Returns:
+            int: the minimum number of operations to reduce x to 0, -1 if not possible
+        """
+        # min_ops = float("inf")
+
+        # for i in range(len(nums)):
+        #     if nums[i] >= x:
+        #         min_ops = min(min_ops, nums[i] - x)
+
+        # if min_ops == float("inf"):
+        #     return -1
+        # else:
+        #     return min_ops
+        total_sum = sum(nums)
+        target = total_sum - x
+        n = len(nums)
+
+        # Input x is larger than what we can make subtracting _all_ nums
+        if target < 0:
+            return -1
+
+        left, right = 0, 0
+        current_sum = 0
+        # Instantiate min_operations which we'll continuously run min(new, this) against
+        min_operations = float("inf")
+
+        # Iterate through nums, starting from the right
+        while right < n:
+            current_sum += nums[right]
+            # When current_sum >= target, iterate through nums, starting from the left
+            while current_sum >= target:
+                # When current_sum matches the target...
+                if current_sum == target:
+                    # ...update min_operations with the minimum of its current value
+                    # and the length of the remaining part of the array (nums - subarray)
+                    min_operations = min(min_operations, n - (right - left + 1))
+                # Ensure we're picking out a number within our nums list indices
+                if left < n:
+                    current_sum -= nums[left]
+                    left += 1
+                # If not, move on
+                else:
+                    break
+            right += 1
+
+        # We've found a valid min_operations if our min_operations changed
+        return min_operations if min_operations != float("inf") else -1
