1f380ebb383d325a290bb8f582bb1646d21be0a2
Joseph Chiocchi <input.output@gmail.com>
2023-09-21 19:53:54 -0500

    solution 20230922.py
    - 35ms (beats 83.70%)
    - 16.23mb (beats 84.50%)

--- 202309/20230922.py
+++ 202309/20230922.py
@@ -14,4 +14,32 @@ class Solution:
     """
 
     def isSubsequence(self, s: str, t: str) -> bool:
-        ...
+        """Determines if s is a subsequence of t
+
+        Proposed solution two pointers.
+
+        Args:
+            s (str): string input to determine if subsequence
+            t (str): string input to match subsequence against
+
+        Returns:
+            bool: whether or not s is a subsequence of t
+        """
+        # Avoid computing this case
+        if len(s) > len(t):
+            return False
+
+        # Instantiate pointers for characters of each string
+        s_ptr, t_ptr = 0, 0
+
+        # Scan each input at the same time, while not disturbing relative positions
+        while s_ptr < len(s) and t_ptr < len(t):
+            # We've found a match
+            if s[s_ptr] == t[t_ptr]:
+                # Move s_ptr forward
+                s_ptr += 1
+            # Move t_ptr forward, we may have found a match before
+            t_ptr += 1
+
+        # s is a subsequence of t if our s_ptr is the length of s
+        return s_ptr == len(s)
