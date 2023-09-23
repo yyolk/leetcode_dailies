51d8a714f839fdab0085c41373d21f9aa518bc02
Joseph Chiocchi <input.output@gmail.com>
2023-09-22 19:23:22 -0500

    solution 20230923.py
    - 111ms (beats 96.96%)
    - 16.60mb (beats 99.11%)

--- 202309/20230923.py
+++ 202309/20230923.py
@@ -22,4 +22,43 @@ class Solution:
     """
 
     def longestStrChain(self, words: List[str]) -> int:
-        ...
+        """Calculate the longest chain of words that appear in the input
+
+        Proposed solution using dynamic programming
+
+        Args:
+            words (List of str): the input sequence of letter sequences to compute
+                sequential chains from
+
+        Returns:
+            int: the length of the longest possible word chain from input
+        """
+        # Sort words by length in ascending order
+        words.sort(key=len)
+
+        # Dict to store length of the longest chain for each word
+        word_chain_lengths = {}
+
+        # Initialize max_chain_length to 1 (for trivial chains)
+        max_chain_length = 1
+
+        # Iterate through the words
+        for word in words:
+            # Initialize the chain length for the current word to 1
+            word_chain_lengths[word] = 1
+
+            # Generate all possible predecessors by removing one character at a time
+            for i in range(len(word)):
+                predecessor = word[:i] + word[i + 1 :]
+
+                # If the predecessor is in the dictionary, update the chain length
+                if predecessor in word_chain_lengths:
+                    word_chain_lengths[word] = max(
+                        word_chain_lengths[word], word_chain_lengths[predecessor] + 1
+                    )
+
+            # Update max_chain_length, for every iteration
+            max_chain_length = max(max_chain_length, word_chain_lengths[word])
+
+        # max_chain_length is the final max_chain_length after iterating
+        return max_chain_length
