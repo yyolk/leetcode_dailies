fb158e22ff2b6e21e08495c9ab9779994d0d6412
Joseph Chiocchi <input.output@gmail.com>
2023-09-27 01:34:38 +0000

    solution 20230927.py
    - 32ms (beats 92.93%)
    - 16.26mb (beats 65.15%)

--- 202309/20230927.py
+++ 202309/20230927.py
@@ -16,4 +16,52 @@ class Solution:
     """
 
     def decodeAtIndex(self, s: str, k: int) -> str:
-        ...
+        """Returns the decoded input string letter at index k
+
+        Proposed solution which only computes k.
+        Overall time and complexity is O(N).
+
+        Args:
+            s (str): the encoded input, digits are special repeat instructions
+                letters are letters
+            k (int): the index (1-indexed) of the letter to return in the
+                decoded string
+
+        Returns:
+            str: the letter at k index in the decoded string
+        """
+        # Total length of the decoded string
+        total_length = 0
+        # A place to keep our current index
+        idx = 0
+
+        # Calculate the total length  of the decoded string
+        while total_length < k:
+            c = s[idx]
+            if c.isdigit():
+                # Multiply total_length by the digit
+                total_length *= int(c)
+            else:
+                # Increment total_length for a letter
+                total_length += 1
+            # Move to the next character in the input
+            idx += 1
+
+        # Backtrack to find the kth letter
+        # Move back to the last character
+        idx -= 1
+        while True:
+            c = s[idx]
+            if c.isdigit():
+                # Divide total_length by the digit
+                total_length /= int(c)
+                # Update k based on the remainder
+                k %= total_length
+            else:
+                if k % total_length == 0:
+                    # If k is a multiple of total_length, return the letter
+                    return c
+                # Decrement total_length for a letter
+                total_length -= 1
+            # Move to the previous character in the input string
+            idx -= 1
