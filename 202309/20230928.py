4811d99f7c2657f15495e3de36be9729062b0cef
Joseph Chiocchi <input.output@gmail.com>
2023-09-28 02:35:20 +0000

    solution 20230928.py
    - 66ms (beats 95.88%)
    - 16.87mb (beats 99.68%)

--- 202309/20230928.py
+++ 202309/20230928.py
@@ -11,4 +11,26 @@ class Solution:
     """
 
     def sortArrayByParity(self, nums: List[int]) -> List[int]:
-        ...
+        """Sorts an array with even integers before odd integers
+
+        Proposed solution using two list and concatenation.
+        One list for odds, one for evens.
+
+        Args:
+            nums (List of int): input nums to sort
+
+        Returns:
+            List of int: sorted input with even numbers followed by odd
+        """
+        evens = []
+        odds = []
+
+        # Insert evens and odds into their respective lists
+        for num in nums:
+            if num % 2 == 0:
+                evens.append(num)
+            else:
+                odds.append(num)
+
+        # Concatenate the lists, it's now sorted with evens on left, odds on right
+        return evens + odds
