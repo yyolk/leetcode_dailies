aa8bd307ce13b3e03ea574bcd0b7c60d0ac7ddc6
Joseph Chiocchi <joe@yolk.cc>
2023-09-28 00:17:10 +0000

    Boilerplate for 20230928
    https://leetcode.com/problems/sort-array-by-parity/

---
+++ 202309/20230928.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/sort-array-by-parity/
+
+
+class Solution:
+    """905. Sort Array By Parity
+
+    Given an integer array `nums`, move all the even integers at the beginning of the array
+    followed by all the odd integers.
+
+    Return ***any array** that satisfies this condition*.
+    """
+
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        ...
