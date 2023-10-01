d458ac3dbab9cfeff2050aa00ce91fd111eda311
Joseph Chiocchi <joe@yolk.cc>
2023-10-01 00:19:56 +0000

    Boilerplate for 20231001
    https://leetcode.com/problems/reverse-words-in-a-string-iii/

---
+++ 202310/20231001.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/reverse-words-in-a-string-iii/
+
+
+class Solution:
+    """557. Reverse Words in a String III
+
+    Given a string `s`, reverse the order of characters in each word within a sentence while
+    still preserving whitespace and initial word order.
+    """
+
+    def reverseWords(self, s: str) -> str:
+        ...
