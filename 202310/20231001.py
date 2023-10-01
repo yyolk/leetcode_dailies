352dd81a1357d323756a13259381a14a7c9ef979
Joseph Chiocchi <input.output@gmail.com>
2023-10-01 01:53:07 +0000

    solution 20231001.py
    - 42ms (beats 68.53%)
    - 16.92mb (beats 67.63%)

--- 202310/20231001.py
+++ 202310/20231001.py
@@ -9,4 +9,21 @@ class Solution:
     """
 
     def reverseWords(self, s: str) -> str:
-        ...
+        """Reverse the words while preserving whitespace and initial word order.
+
+        Proposed solution using split and reversed.
+        All joins happen at the moment of return.
+
+        Args:
+            s (str): Input string to reverse.
+
+        Returns:
+            str: The resulting special reversed string
+        """
+        results = []
+        for word in s.split():
+            # Append a reversed(...) iterator to our results list
+            results.append(reversed(word))
+
+        # Join each reversed result into a string, while joining all of them with a " "
+        return " ".join("".join(result) for result in results)
