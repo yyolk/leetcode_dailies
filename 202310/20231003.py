3b5a1dfb572ba7deefa2226740a75da60d33e8e3
Joseph Chiocchi <input.output@gmail.com>
2023-10-03 01:35:22 +0000

    solution 20231003.py
    - 34ms (beats 91.11%)
    - 16.31mb

--- 202310/20231003.py
+++ 202310/20231003.py
@@ -1,4 +1,5 @@
 # https://leetcode.com/problems/number-of-good-pairs/
+from collections import Counter
 
 
 class Solution:
@@ -10,4 +11,29 @@ class Solution:
     """
 
     def numIdenticalPairs(self, nums: List[int]) -> int:
-        ...
+        """How many identical pairs, which are good are in the input
+
+        Proposed solution using one loop and a collections.Counter for the occurences.
+
+        Args:
+            nums (List of int): The input list of numbers to scan for good pairs.
+
+        Returns:
+            int: The number of good pairs found in the input.
+        """
+        # Create a counter to count the occurences of each element
+        counter = Counter()
+
+        # Initialize the count of good pairs
+        count = 0
+
+        # Iterate through the input list
+        for num in nums:
+            # If the number is already in the counter, increment the count
+            if num in counter:
+                count += counter[num]
+
+            # Increment the count of this number in the counter
+            counter[num] += 1
+
+        return count
