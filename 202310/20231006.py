3fc03c0f0076747657f4891bfcd71211fce5f302
Joseph Chiocchi <joe@yolk.cc>
2023-10-06 00:17:15 +0000

    Boilerplate for 20231006
    https://leetcode.com/problems/integer-break/

---
+++ 202310/20231006.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/integer-break/
+
+
+class Solution:
+    """343. Integer Break
+
+    Given an integer `n`, break it into the sum of `k` **positive integers**, where `k >=
+    2`, and maximize the product of those integers.
+
+    Return *the maximum product you can get*.
+    """
+
+    def integerBreak(self, n: int) -> int:
+        ...
