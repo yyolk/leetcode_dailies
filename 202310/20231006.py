e8d3cc2d08443d570e971c18ee467f196148bc13
Joseph Chiocchi <input.output@gmail.com>
2023-10-06 01:14:15 +0000

    solution 20231006.py
    - 29ms (beats 97.96%)
    - 16.35mb

--- 202310/20231006.py
+++ 202310/20231006.py
@@ -11,4 +11,29 @@ class Solution:
     """
 
     def integerBreak(self, n: int) -> int:
-        ...
+        """The maximum product you can get from breaking the input into sum of k integers
+
+        Proposed solution using a loop.
+
+        Args:
+            n (int): The input integer.
+
+        Returns:
+            int: The maximum product of the broken input into sum of k positive
+            integers.
+        """
+        # Handle cases where n < 4 separately
+        if n == 2:
+            return 1
+        if n == 3:
+            return 2
+
+        # Initialize max_product with 1, 1 times any number is the number
+        max_product = 1
+
+        while n > 4:
+            max_product *= 3
+            n -= 3
+
+        # At this point, n is either 2 or 3 or 4
+        return max_product * n
