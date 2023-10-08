bdf865332783724693535f1bfedf7ebd72d3c86d
Joseph Chiocchi <input.output@gmail.com>
2023-10-08 00:48:21 +0000

    solution 20231008.py
    - 217ms (beats 91.55%)
    - 16.28mb (beats 98.59%)

--- 202310/20231008.py
+++ 202310/20231008.py
@@ -16,4 +16,43 @@ class Solution:
     """
 
     def maxDotProduct(self, nums1: List[int], nums2: List[int]) -> int:
-        ...
+        """Maximum dot product between inputs subsequences.
+
+        Proposed solution using dynamic programming.
+
+        Args:
+            nums1 (List of int): Input subsequence 1.
+            nums2 (List of int): Input subsequence 2.
+
+        Returns:
+            int: Maximum dot product between the input subsequences.
+        """
+        m, n = len(nums1), len(nums2)
+
+        # Initialize a 1D dp array to store maximum dot products
+        dp = [float("-inf")] * (n + 1)
+
+        # Loop through the elements of nums1
+        for i in range(1, m + 1):
+            # Store the previous diagonal value
+            prev_diag = dp[0]
+            for j in range(1, n + 1):
+                # Store the current dp value
+                temp = dp[j]
+
+                # Caclulate the maximum dot product at dp[j]
+                dp[j] = max(
+                    # Include current elements
+                    prev_diag + nums1[i - 1] * nums2[j - 1],
+                    # Exclude current element from nums2
+                    dp[j - 1],
+                    # Exclude current element from nums1
+                    dp[j],
+                    # Start a new subsequence
+                    nums1[i - 1] * nums2[j - 1],
+                )
+
+                # Update the previous diagonal value for the next iteration
+                prev_diag = temp
+
+        return dp[n]
