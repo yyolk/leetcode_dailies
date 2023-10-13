9d0977a6b470bf97fd8f5aa05dfb087dd0a13f2c
Joseph Chiocchi <input.output@gmail.com>
2023-10-13 02:15:16 +0000

    solution 20231013.py
    - 54ms (beats 90.45%)
    - 16.28mb (beats 94.79%)

--- 202310/20231013.py
+++ 202310/20231013.py
@@ -13,4 +13,27 @@ class Solution:
     """
 
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        ...
+        """Finds the minimum cost to reach the top of the floor.
+
+        Proposed solution using dynamic programming.
+
+        Args:
+            cost (List of int): The cost where the index is mapped to the i_th step
+                on the staircase.
+
+        Returns:
+            int: The minimum cost to reach the top.
+        """
+        n = len(cost)
+        # Initialize the dp array with the cost of the first two steps
+        dp = [0] * n
+        dp[0] = cost[0]
+        dp[1] = cost[1]
+
+        # Calculate the minimum cost for each step from the third step to the top
+        for i in range(2, n):
+            dp[i] = cost[i] + min(dp[i - 1], dp[i - 2])
+
+        # The minimum cost can be either starting from the last step or the second to
+        # last step, this accounts for both possible starting positions.
+        return min(dp[-1], dp[-2])
