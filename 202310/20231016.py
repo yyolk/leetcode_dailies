c30f68d80fe40592b1cc69a3f47617a6b258dcf0
Joseph Chiocchi <input.output@gmail.com>
2023-10-16 23:04:51 +0000

    solution 20231016.py
    - 32ms (beats 90.97%)
    - 16.20mb (beats 81.98%)

--- 202310/20231016.py
+++ 202310/20231016.py
@@ -14,6 +14,34 @@ class Solution:
     """
 
     def get_row(self, row_index: int) -> List[int]:
-        ...
+        """Gets the row from the computed Pascal's Triangle.
+
+        Proposed solution.
+
+        Args:
+            row_index (int): Which row to get, 0-indexed, starting from the top.
+
+        Returns:
+            List of int: The requested row of numbers in Pascal's Triangle.
+        """
+        # Initialize the first row
+        row = [1]
+
+        for i in range(1, row_index + 1):
+            # Create a new row
+            new_row = [1]
+            for j in range(1, i):
+                # Calculate the next value using the previous row
+                new_value = row[j - 1] + row[j]
+                new_row.append(new_value)
+
+            # Add the final 1 (right most value) to the new row
+            new_row.append(1)
+
+            # Update the current row
+            row = new_row
+
+        # We've reached our requested index, return the row
+        return row
 
     getRow = get_row
