260514175868c366f94806bdc25f91d2ab752381
Joseph Chiocchi <input.output@gmail.com>
2023-10-18 01:17:30 +0000

    solution 20231018.py
    - 1380ms (beats 96.47%)
    - 45.44mb (beats 81.47%)

--- 202310/20231018.py
+++ 202310/20231018.py
@@ -1,4 +1,5 @@
 # https://leetcode.com/problems/parallel-courses-iii/
+from collections import deque
 
 
 class Solution:
@@ -25,6 +26,52 @@ class Solution:
     """
 
     def minimum_time(self, n: int, relations: List[List[int]], time: List[int]) -> int:
-        ...
+        """Finds the minimum number of months needed to complete all the courses.
+
+        Proposed solution using topological sorting and dynamic programing.
+
+        Args:
+            n (int): The number of courses, each labeled from 1 to n.
+            relations (List of List of int): A 2D integer array where each element is
+                a direct dependency (prevCourse has to be completed before nextCourse).
+            time (List of int): 0-indexed integer array where time[i] denotes how many
+                months it takes to complete the (i+1)_th course.
+
+        Returns:
+            int: The minimum number of months needed to complete all the courses.
+        """
+        # Build the directed graph.
+        graph = [[] for _ in range(n)]
+        in_degree = [0] * n
+        for relation in relations:
+            prev_course, next_course = relation
+            graph[prev_course - 1].append(next_course - 1)
+            in_degree[next_course - 1] += 1
+
+        # Initialize the queue with courses having no prerequisites.
+        queue = deque()
+        for i in range(n):
+            if in_degree[i] == 0:
+                queue.append(i)
+
+        # Initialize time and dp array.
+        months = 0
+        dp = time[:]
+
+        # Perform topological sort.
+        while queue:
+            months += 1
+            for _ in range(len(queue)):
+                course = queue.popleft()
+                for next_course in graph[course]:
+                    in_degree[next_course] -= 1
+                    dp[next_course] = max(
+                        dp[next_course], dp[course] + time[next_course]
+                    )
+                    if in_degree[next_course] == 0:
+                        queue.append(next_course)
+
+        # Return the minimum time needed.
+        return max(dp)
 
     minimumTime = minimum_time
