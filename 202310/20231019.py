f0cd712ce3967da50dac9598a2dc6af5faa480de
Joseph Chiocchi <input.output@gmail.com>
2023-10-19 00:30:07 +0000

    solution 20231019.py
    - 35ms (beats 78.50%)
    - 16.26mb (beats 54.52%)

--- 202310/20231019.py
+++ 202310/20231019.py
@@ -1,4 +1,5 @@
 # https://leetcode.com/problems/backspace-string-compare/
+from itertools import groupby
 
 
 class Solution:
@@ -11,6 +12,50 @@ class Solution:
     """
 
     def backspace_compare(self, s: str, t: str) -> bool:
-        ...
+        """Simulate input strings being typed with backspaces and compare them.
+
+        Proposed solution simulating the typing in both editors.
+
+        Args:
+            s (str): Input string one.
+            t (str): Input string two.
+
+        Returns:
+            bool: True if the strings, after backspaces are accounted for, are the same.
+        """
+
+        def process_string(string: str) -> List[s]:
+            """Process a String by applying backspaces.
+
+            Args:
+                string (str): The input string to process.
+            Returns:
+                List of str: A list of characters representing the final string after
+                    backspace processing.
+            """
+
+            def generate_characters():
+                # Group characters in the input string, including "#",
+                # then calculate their count.
+                for char, group in groupby(string):
+                    if char == "#":
+                        # Use negative count for backspaces.
+                        yield char, -sum(1 for _ in group)
+                    else:
+                        yield char, sum(1 for _ in group)
+
+            result = []
+            for char, count in generate_characters():
+                if count < 0:
+                    # Apply backspace by removing characters from the result.
+                    result = result[:count]
+                else:
+                    # Add characters to the result based on their count.
+                    result.extend([char] * count)
+
+            return result
+
+        # Compare the processed strings for equality.
+        return process_string(s) == process_string(t)
 
     backspaceCompare = backspace_compare
