1208994df412ac509cbce140e0748dbdf19c39b0
Joseph Chiocchi <joe@yolk.cc>
2023-10-19 00:17:20 +0000

    Boilerplate for 20231019
    https://leetcode.com/problems/backspace-string-compare/

---
+++ 202310/20231019.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/backspace-string-compare/
+
+
+class Solution:
+    """844. Backspace String Compare
+
+    Given two strings `s` and `t`, return `true` *if they are equal when both are typed
+    into empty text editors*. `'#'` means a backspace character.
+
+    Note that after backspacing an empty text, the text will continue empty.
+    """
+
+    def backspace_compare(self, s: str, t: str) -> bool:
+        ...
+
+    backspaceCompare = backspace_compare
--- 202310/README.md
+++ 202310/README.md
@@ -123,7 +123,9 @@
    </a>
   </td>
   <td class="thu">
-   19
+   <a href="20231019.py">
+    19
+   </a>
   </td>
   <td class="fri">
    20
