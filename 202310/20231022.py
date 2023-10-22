17a4d6967ef676fdd99674fd544c5a11fb4c9ebb
Joseph Chiocchi <input.output@gmail.com>
2023-10-22 22:33:34 +0000

    solution 20231022.py
    - 1051ms (beats 56.71%)
    - 27.28mb (beats 54.27%)

--- 202310/20231022.py
+++ 202310/20231022.py
@@ -12,7 +12,39 @@ class Solution:
     Return *the maximum possible **score** of a **good** subarray.*
     """
 
-    def maximum_score(self, nums: List[int], k: int) -> int:
-        ...
+    def maximum_score(self, nums: list[int], k: int) -> int:
+        """Maximum possible score of a good subarray
+
+        Proposed solution using bisect.
+
+        Args:
+            nums (list of int): Input integer array of nums.
+            k (int): A threshold value between i and j for determining good subarrays.
+
+        Returns:
+            int: The maximum possible score of a good subarray.
+        """
+        # Initialize left and right indices with k
+        left, right = k, k
+        # Initialize min_val with the value at index k
+        min_val = nums[k]
+        # Initialize max_score with min_val
+        max_score = min_val
+
+        while left > 0 or right < len(nums) - 1:
+            if left == 0 or (
+                right < len(nums) - 1 and nums[right + 1] > nums[left - 1]
+            ):
+                # Expand the right side of the subarray
+                right += 1
+            else:
+                # Expand the left side of the subarray
+                left -= 1
+            # Update min_val
+            min_val = min(min_val, nums[left], nums[right])
+            # Update max_score
+            max_score = max(max_score, min_val * (right - left + 1))
+
+        return max_score
 
     maximumScore = maximum_score
