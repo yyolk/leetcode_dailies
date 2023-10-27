b66f3198928ce470c7ab1bbe84ecf52f2730c6c2
Joseph Chiocchi <joe@yolk.cc>
2023-10-27 00:16:58 +0000

    Boilerplate for 20231027
    https://leetcode.com/problems/longest-palindromic-substring/

---
+++ 202310/20231027.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/longest-palindromic-substring/
+
+
+class Solution:
+    """5. Longest Palindromic Substring
+
+    Given a string `s`, return *the longest* *palindromic* *substring* in `s`.
+    """
+
+    def longest_palindrome(self, s: str) -> str:
+        ...
+
+    longestPalindrome = longest_palindrome
--- 202310/README.md
+++ 202310/README.md
@@ -165,7 +165,9 @@
    </a>
   </td>
   <td class="fri">
-   27
+   <a href="20231027.py">
+    27
+   </a>
   </td>
   <td class="sat">
    28
