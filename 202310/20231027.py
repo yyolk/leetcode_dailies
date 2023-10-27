8fae66e053f2e163d3504ec92f23da92232ebdc0
Joseph Chiocchi <input.output@gmail.com>
2023-10-27 03:51:00 +0000

    solution 20231027.py
    - 382ms (beats 83.48%)
    - 16.09mb (beats 99.51%)

--- 202310/20231027.py
+++ 202310/20231027.py
@@ -8,6 +8,42 @@ class Solution:
     """
 
     def longest_palindrome(self, s: str) -> str:
-        ...
+        """The longest palindromic substring from input s.
+
+        Proposed solution using dynamic programming.
+
+        Args:
+            s (str): The input string to search.
+        Returns:
+            str: The palindromic substring that's the longest from s.
+        """
+
+        def expand_around_center(left, right):
+            """Helper function to expand around a center and find a palindrome."""
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            # When the loop ends, s[left + 1 : right] is the palindrome found
+            return s[left + 1 : right]
+
+        # Initialize a variable to store the longest palindrome
+        longest = ""
+
+        # Iterate through the characters in the string
+        for i in range(len(s)):
+            # Find the longest odd length palindrome with the current character as the
+            # center
+            palindrome_odd = expand_around_center(i, i)
+            if len(palindrome_odd) > len(longest):
+                longest = palindrome_odd
+
+            # Find the longest even length palindrome with the current character as the
+            # center
+            palindrome_even = expand_around_center(i, i + 1)
+            if len(palindrome_even) > len(longest):
+                longest = palindrome_even
+
+        # Return the longest palindrome found
+        return longest
 
     longestPalindrome = longest_palindrome
