a937369bb5f20ece82c62c6566db89f5c578f632
Joseph Chiocchi <input.output@gmail.com>
2023-10-28 00:44:12 +0000

    solution 20231028.py
    - 189ms (beats 74.16%)
    - 20.41mb (beats 59.03%)

--- 202310/20231028.py
+++ 202310/20231028.py
@@ -1,4 +1,5 @@
 # https://leetcode.com/problems/count-vowels-permutation/
+MOD = 10**9 + 7
 
 
 class Solution:
@@ -23,6 +24,38 @@ class Solution:
     """
 
     def count_vowel_permutation(self, n: int) -> int:
-        ...
+        """How many strings of length n can be formed under the rules.
+
+        Proposed solution using dynamic programming.
+
+        Args:
+            n (int): The input target length to form strings.
+
+        Returns:
+            int: The maximum amount of strings that can be formed for target length n
+                cutoff at 10^9 + 7.
+        """
+        # Initialize the dp array with 0s.
+        dp = [[0] * 5 for _ in range(n + 1)]
+
+        # Base case: there's 1 valid string of length 1 for each vowel.
+        for j in range(5):
+            dp[1][j] = 1
+
+        for i in range(2, n + 1):
+            # Calculate the number of valid strings of length i for each vowel.
+            # 'a' can only be followed by 'e'
+            dp[i][0] = dp[i - 1][1]
+            # 'e' can only be followed by 'a' or 'i'
+            dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]) % MOD
+            # 'i' cannot be followed by another 'i'
+            dp[i][2] = (dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][3] + dp[i - 1][4]) % MOD
+            # 'o' can be followed by 'i' or 'u'
+            dp[i][3] = (dp[i - 1][2] + dp[i - 1][4]) % MOD
+            # 'u' can only be followed by 'a'
+            dp[i][4] = dp[i - 1][0]
+
+        # Calculate the total count of valid strings of length n, cut to MOD, and return it
+        return sum(dp[n]) % MOD
 
     countVowelPermutation = count_vowel_permutation
