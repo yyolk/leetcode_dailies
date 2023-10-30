077da115b0bf4a43da874909ff312f2036ed4597
Joseph Chiocchi <input.output@gmail.com>
2023-10-30 21:44:37 +0000

    solution 20231030.py
    - 64ms (beats 78.00%)
    - 16.27mb (beats 94.29%)

--- 202310/20231030.py
+++ 202310/20231030.py
@@ -12,7 +12,23 @@ class Solution:
     Return *the array after sorting it*.
     """
 
-    def sort_by_bits(self, arr: List[int]) -> List[int]:
-        ...
+    def sort_by_bits(self, arr: list[int]) -> list[int]:
+        """Sort input array by their binary representation.
+
+        Sorts the array using `list.sort(...)` with the key function counting the
+        number of 1s using `int.bit_count(...)`. If two numbers have the same number
+        of 1s, we'll sort again by their original value.
+
+                lambda num: (num.bit_count(), num)
+                            ^- primary sort   ^
+                                              | secondary sort
+
+        Args:
+            arr (list of int): Input integer array to sort by binary representation.
+        Returns:
+            list of int: Resulting, sorted, integer array.
+        """
+        arr.sort(key=lambda num: (num.bit_count(), num))
+        return arr
 
     sortByBits = sort_by_bits
