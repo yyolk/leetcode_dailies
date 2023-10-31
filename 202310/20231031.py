38b4b1bb651b76862f01f8cd7712bbeffab26da7
Joseph Chiocchi <input.output@gmail.com>
2023-10-31 01:04:05 +0000

    solution 20231031.py
    - 685ms
    - 41.57mb

--- 202310/20231031.py
+++ 202310/20231031.py
@@ -14,7 +14,38 @@ class Solution:
     It can be proven that the answer is **unique**.
     """
 
-    def find_array(self, pref: List[int]) -> List[int]:
-        ...
+    def find_array(self, pref: list[int]) -> list[int]:
+        """Finds and returns the original array `arr` of size `n` that satisfies the given conditions.
+
+        Proposed solution using dyanmic programming.
+
+        Args:
+            pref: An integer list of size `n` representing the prefix XOR values.
+
+        Returns:
+            The original array `arr` of size `n`.
+
+        Example:
+            >>> solution = Solution()
+            >>> solution.find_array([1, 3, 2, 6, 4])
+            [1, 2, 3, 4, 5]
+
+        Note:
+            The answer is guaranteed to be unique.
+        """
+        n = len(pref)
+        # Initialize arr with 0s
+        arr = [0] * (n + 1)
+        # Initialize the result array
+        result = [0] * n
+
+        for i in range(n):
+            # Calculate the current element of the result array
+            result[i] = pref[i] ^ arr[i]
+
+            # Update the arr array for the next iteration
+            arr[i + 1] = arr[i] ^ result[i]
+
+        return result
 
     findArray = find_array
