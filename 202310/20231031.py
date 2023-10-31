dd4c991b1e7e539da1fce8e212baecbb6403ddea
Joseph Chiocchi <joe@yolk.cc>
2023-10-31 00:19:14 +0000

    Boilerplate for 20231031
    https://leetcode.com/problems/find-the-original-array-of-prefix-xor/

---
+++ 202310/20231031.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/find-the-original-array-of-prefix-xor/
+
+
+class Solution:
+    """2433. Find The Original Array of Prefix Xor
+
+    You are given an **integer** array `pref` of size `n`. Find and return *the array*
+    `arr` *of size* `n` *that satisfies*:
+
+    * `pref[i] = arr[0] ^ arr[1] ^ ... ^ arr[i]`.
+
+    Note that `^` denotes the **bitwise-xor** operation.
+
+    It can be proven that the answer is **unique**.
+    """
+
+    def find_array(self, pref: List[int]) -> List[int]:
+        ...
+
+    findArray = find_array
--- 202310/README.md
+++ 202310/README.md
@@ -187,7 +187,9 @@
    </a>
   </td>
   <td class="tue">
-   31
+   <a href="20231031.py">
+    31
+   </a>
   </td>
   <td class="noday">
   </td>
