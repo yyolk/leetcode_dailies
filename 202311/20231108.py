02fd3cbc7deea271d65b497f8f9449e0f4b14ed7
Joseph Chiocchi <input.output@gmail.com>
2023-11-08 00:52:10 +0000

    solution 20231108.py
    - 32ms (beats 93.46%)
    - 16.27mb (beats 73.40%)

--- 202311/20231108.py
+++ 202311/20231108.py
@@ -18,6 +18,32 @@ class Solution:
     """
 
     def is_reachable_at_time(self, sx: int, sy: int, fx: int, fy: int, t: int) -> bool:
-        ...
+        """Can a cell be reached within the alloted time.
+
+        Proposed solution with mathematical observation.
+
+        Args:
+            sx: Starting point x coordinate.
+            sy: Starting point y coordinate.
+            fx: Finish point x coordinate.
+            fy: Finish point y coordinate.
+            t: Time in seconds that are alloted.
+
+        Returns:
+            True if the finished cell can be reached from the starting cell in the
+            alloted time.
+        """
+        # If the starting and finishing cells are the same, check if t is greater than 1
+        if sx == fx and sy == fy:
+            if t == 1:
+                # You can't stay in the same cell for exactly 1 second
+                return False
+            else:
+                # You can stay in the same cell for t seconds
+                return True
+
+        # Check if the Manhattan distance between starting and finishing cells is less
+        # than or equal to t
+        return abs(sx - fx) <= t and abs(sy - fy) <= t
 
     isReachableAtTime = is_reachable_at_time
