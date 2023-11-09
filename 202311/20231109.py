4c6ce7432eff4cedc593b4fdb07c6127c990d85a
Joseph Chiocchi <input.output@gmail.com>
2023-11-09 22:21:39 +0000

    solution 20231109.py
    - 112ms (beats 71.13%)
    - 17.39mb (beats 53.52%)

--- 202311/20231109.py
+++ 202311/20231109.py
@@ -5,7 +5,7 @@ class Solution:
     """1759. Count Number of Homogenous Substrings
 
     Given a string `s`, return *the number of **homogenous** substrings of* `s`*.* Since
-    the answer may be too large, return it **modulo** `109 + 7`.
+    the answer may be too large, return it **modulo** `10^9 + 7`.
 
     A string is **homogenous** if all the characters of the string are the same.
 
@@ -13,6 +13,27 @@ class Solution:
     """
 
     def count_homogenous(self, s: str) -> int:
-        ...
+        """Count the number of homogenous substrings in the given string.
+
+        Args:
+            s: The input string.
+
+        Returns:
+            The number of homogenous substrings modulo 10^9 + 7.
+        """
+        MOD = 10**9 + 7
+        result = 0
+        count = 1
+        for i in range(1, len(s)):
+            if s[i] == s[i - 1]:
+                count += 1
+            else:
+                result = (result + count * (count + 1) // 2) % MOD
+                count = 1
+
+        # Add the count for the last homogenous substring
+        result = (result + count * (count + 1) // 2) % MOD
+
+        return result
 
     countHomogenous = count_homogenous
