d3c74375c22e46dadb3e5319e09fefba79f0bc12
Joseph Chiocchi <input.output@gmail.com>
2023-11-10 23:00:11 +0000

    solution 20231110.py

--- 202311/20231110.py
+++ 202311/20231110.py
@@ -1,4 +1,5 @@
 # https://leetcode.com/problems/restore-the-array-from-adjacent-pairs/
+from collections import defaultdict
 
 
 class Solution:
@@ -21,6 +22,35 @@ class Solution:
     """
 
     def restore_array(self, adjacent_pairs: List[List[int]]) -> List[int]:
-        ...
+        # Build the adjacency list
+        adj_list = defaultdict(list)
+        for pair in adjacent_pairs:
+            u, v = pair
+            adj_list[u].append(v)
+            adj_list[v].append(u)
+
+        # Function to perform DFS
+        def dfs(node):
+            visited.add(node)
+            result.append(node)
+            for neighbor in adj_list[node]:
+                if neighbor not in visited:
+                    dfs(neighbor)
+
+        # Find a starting node
+        start_node = None
+        for node, neighbors in adj_list.items():
+            if len(neighbors) == 1:
+                start_node = node
+                break
+
+        # Initialize data structures
+        visited = set()
+        result = []
+
+        # Perform DFS
+        dfs(start_node)
+
+        return result
 
     restoreArray = restore_array
