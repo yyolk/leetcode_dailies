a601221ff1116b0545c7cb9a89c25fe166f05dcc
Joseph Chiocchi <joe@yolk.cc>
2023-11-16 00:18:02 +0000

    Boilerplate for 20231116
    https://leetcode.com/problems/find-unique-binary-string/

---
+++ 202311/20231116.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/find-unique-binary-string/
+
+
+class Solution:
+    """1980. Find Unique Binary String
+
+    Given an array of strings `nums` containing `n` **unique** binary strings each of
+    length `n`, return *a binary string of length* `n` *that **does not appear** in*
+    `nums`*. If there are multiple answers, you may return **any** of them*.
+    """
+
+    def find_different_binary_string(self, nums: List[str]) -> str:
+        ...
+
+    findDifferentBinaryString = find_different_binary_string
