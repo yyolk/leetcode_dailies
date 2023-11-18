2f5c0cad867044a14002b61040051df284bcd841
Joseph Chiocchi <joe@yolk.cc>
2023-11-18 00:17:41 +0000

    Boilerplate for 20231118
    https://leetcode.com/problems/frequency-of-the-most-frequent-element/

---
+++ 202311/20231118.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/frequency-of-the-most-frequent-element/
+
+
+class Solution:
+    """1838. Frequency of the Most Frequent Element
+
+    The **frequency** of an element is the number of times it occurs in an array.
+
+    You are given an integer array `nums` and an integer `k`. In one operation, you can
+    choose an index of `nums` and increment the element at that index by `1`.
+
+    Return *the **maximum possible frequency** of an element after performing **at
+    most*** `k` *operations*.
+    """
+
+    def max_frequency(self, nums: List[int], k: int) -> int:
+        ...
+
+    maxFrequency = max_frequency
