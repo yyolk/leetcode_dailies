8cff472f7ebd4f36414f497ccb0603c9ceb42d47
Joseph Chiocchi <input.output@gmail.com>
2023-11-18 02:23:56 +0000

    solution 20231118.py
    - 1131ms (beats 79.82%)
    - 31.33mb

--- 202311/20231118.py
+++ 202311/20231118.py
@@ -14,6 +14,37 @@ class Solution:
     """
 
     def max_frequency(self, nums: List[int], k: int) -> int:
-        ...
+        """The maximum possible frequency of an element post, at most, k operations.
+
+        Using a sliding window.
+
+        Args:
+            nums: Input list of integer elements.
+            k: Max operations to perform.
+
+        Returns:
+            Frequency of an element after performing at most k operations.
+        """
+        # Sort the array in non-decreasing order.
+        nums.sort()
+        n = len(nums)
+        max_freq = 0
+        operations = 0
+        left = 0
+
+        # Iterate through the sorted list.
+        for right in range(n):
+            operations += nums[right]
+
+            # While the operations exceed the limit, move the left pointer
+            while (right - left + 1) * nums[right] > operations + k:
+                operations -= nums[left]
+                left += 1
+
+            # Update the maximum frequency.
+            max_freq = max(max_freq, right - left + 1)
+
+        # Return the maximum frequency
+        return max_freq
 
     maxFrequency = max_frequency
