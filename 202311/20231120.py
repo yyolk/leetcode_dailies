8f49cca7a3f140d376c4788620c84cc7c88e2380
Joseph Chiocchi <input.output@gmail.com>
2023-11-20 02:51:08 +0000

    solution 20231120.py
    - 737ms (beats 99.67%)
    - 38.54mb (beats 93.22%)

--- 202311/20231120.py
+++ 202311/20231120.py
@@ -23,7 +23,37 @@ class Solution:
     Return *the **minimum** number of minutes needed to pick up all the garbage.*
     """
 
-    def garbage_collection(self, garbage: List[str], travel: List[int]) -> int:
-        ...
+    def garbage_collection(self, garbage: list[str], travel: list[int]) -> int:
+        """The minimum number of minutes needed to pick up all the garbage.
+
+        Args:
+            garbage: 0-indexed list of strings where the ith element is the ith house.
+            travel: 0-index integer list with the time it takes to go from house i to
+                house i + 1.
+
+        Returns:
+            The minimum number of minutes needed to pick up all the garbage
+        """
+        n = len(garbage)
+        time_g, time_p, time_m = 0, 0, 0
+        time_total = 0
+
+        # Iterate through each house in reverse order.
+        for i in range(n - 1, -1, -1):
+            x = garbage[i]
+            time_total += len(x)
+
+            # Update the last occurrence of each type of garbage.
+            if time_g == 0 and "G" in x:
+                time_g = i
+            if time_p == 0 and "P" in x:
+                time_p = i
+            if time_m == 0 and "M" in x:
+                time_m = i
+
+        # Update the total time with the travel time for each garbage type.
+        time_total += sum(travel[:time_g]) + sum(travel[:time_p]) + sum(travel[:time_m])
+
+        return time_total
 
     garbageCollection = garbage_collection
