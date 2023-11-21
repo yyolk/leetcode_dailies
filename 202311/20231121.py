aacc3ea81e417f32e7ae967a8ac18f642269c596
Joseph Chiocchi <joe@yolk.cc>
2023-11-21 00:18:38 +0000

    Boilerplate for 20231121
    https://leetcode.com/problems/count-nice-pairs-in-an-array/

---
+++ 202311/20231121.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/count-nice-pairs-in-an-array/
+
+
+class Solution:
+    """1814. Count Nice Pairs in an Array
+
+    You are given an array `nums` that consists of non-negative integers. Let us define
+    `rev(x)` as the reverse of the non-negative integer `x`. For example, `rev(123) =
+    321`, and `rev(120) = 21`. A pair of indices `(i, j)` is **nice** if it satisfies
+    all of the following conditions:
+
+    * `0 <= i < j < nums.length`
+
+    * `nums[i] + rev(nums[j]) == nums[j] + rev(nums[i])`
+
+    Return *the number of nice pairs of indices*. Since that number can be too large,
+    return it **modulo** `109 + 7`.
+    """
+
+    def count_nice_pairs(self, nums: List[int]) -> int:
+        ...
+
+    countNicePairs = count_nice_pairs
