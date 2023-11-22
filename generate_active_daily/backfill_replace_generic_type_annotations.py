d60a00d089af8b1564e6642a6692028f5f3e65c8
Joseph Chiocchi <input.output@gmail.com>
2023-11-22 04:01:29 +0000

    init backfill_replace_generic_type_annotations

---
+++ generate_active_daily/backfill_replace_generic_type_annotations.py
@@ -0,0 +1,32 @@
+import os
+import re
+
+
+for dir_ in ["202308", "202309", "202310", "202311"]:
+    for root, _, files in os.walk(dir_):
+        for file_ in files:
+            if file_.endswith(".py"):
+                with open(f"{root}/{file_}", "r") as fp:
+                    content = fp.read()
+                    for word in ["List", "Dict", "Tuple", "Set"]:
+                        # new_content = content.replace(word, word.lower())
+                        content = re.sub(
+                            f"""
+                            (?:             # set our negative look behind choices to non-capture
+                                (?<!#\\ )   # negative look behid for line comments like '# Type'
+                                |
+                                (?<!\)\:)   # negative look behind for '):' in the case of redundant google-style docstrings
+                            )               # end our negative look behind choices
+                            ({word})        # our only capture group
+                            (?:             # set our positive lookahead choices
+                                (?=\\[)     # include when it's like 'Type[...].
+                                |
+                                (?= ?of ?)  # include when it's like 'List of List ...'
+                            )
+                            """,
+                            word.lower(),
+                            content,
+                            flags=re.MULTILINE | re.VERBOSE,
+                        )
+                with open(f"{root}/{file_}", "w") as fp:
+                    fp.write(content)
