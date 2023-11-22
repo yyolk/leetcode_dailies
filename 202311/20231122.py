fd01aa7473a781622ae88222236b7fa0d36e05ab
Joseph Chiocchi <input.output@gmail.com>
2023-11-22 02:12:14 +0000

    solution 20231122.py
    - 967ms
    - 37.36mb (beats 78.75%)

--- 202311/20231122.py
+++ 202311/20231122.py
@@ -1,4 +1,5 @@
 # https://leetcode.com/problems/diagonal-traverse-ii/
+import heapq
 
 
 class Solution:
@@ -9,6 +10,31 @@ class Solution:
     """
 
     def find_diagonal_order(self, nums: list[list[int]]) -> list[int]:
-        ...
+        """Returns all elements from nums in diagonal order.
+
+        Args:
+            nums: Input 2D integer array of input elements.
+
+        Returns:
+            Integer array of elements starting from top left to bottom right of
+            elements in diagonal order.
+        """
+        # Create a min heap.
+        heap = []
+        # Create the result list.
+        result = []
+
+        # Push elements into the heap with the tuple:
+        #   (sum of indices, -row index, element).
+        for i in range(len(nums)):
+            for j in range(len(nums[i])):
+                heapq.heappush(heap, (i + j, -i, nums[i][j]))
+
+        # Pop elements from the heap and append them to the result list.
+        while heap:
+            _, _, element = heapq.heappop(heap)
+            result.append(element)
+
+        return result
 
     findDiagonalOrder = find_diagonal_order
