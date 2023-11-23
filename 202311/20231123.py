8461c517841669d1ac778b15e0a08d0307060bcd
Joseph Chiocchi <input.output@gmail.com>
2023-11-23 01:56:06 +0000

    solution 20231123.py
    - 155ms (beats 93.99%)
    - 16.64mb

--- 202311/20231123.py
+++ 202311/20231123.py
@@ -41,6 +41,35 @@ class Solution:
     def check_arithmetic_subarrays(
         self, nums: list[int], l: list[int], r: list[int]
     ) -> list[bool]:
-        ...
+        """Check for arithmetic subarrays.
+
+        Args:
+            nums: A list of integers.
+            l: First list of integers for queries.
+            r: Second list of integers for queries.
+
+        Returns:
+            Boolean list indexed to map to subarray[i] which can be rearranged to form
+            an arithmetic sequence.
+        """
+
+        def is_arithmetic(subarray: list[int]) -> bool:
+            """Determine if subarray is arithmetic."""
+            subarray.sort()
+            diff = subarray[1] - subarray[0]
+            # Check if the difference is the same for all consecutive elements.
+            for i in range(2, len(subarray)):
+                if subarray[i] - subarray[i - 1] != diff:
+                    return False
+            return True
+
+        result = []
+        for i in range(len(l)):
+            # Extract the subarray based on the current query.
+            subarray = nums[l[i] : r[i] + 1]
+            # Check if the subarray can be rearranged to form an arithmetic sequence.
+            result.append(is_arithmetic(subarray))
+
+        return result
 
     checkArithmeticSubarrays = check_arithmetic_subarrays
