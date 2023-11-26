3d46b50e8925b9b9977301ef285d2ddb2ef5452a
Joseph Chiocchi <input.output@gmail.com>
2023-11-26 01:02:05 +0000

    solution 20231126.py
    - 1072ms (beats 86.11%)
    - 42.80mb

--- 202311/20231126.py
+++ 202311/20231126.py
@@ -12,6 +12,30 @@ class Solution:
     """
 
     def largest_submatrix(self, matrix: list[list[int]]) -> int:
-        ...
+        """Find the area of the largest submatrix within the given matrix.
+
+        Args:
+            matrix: The input matrix as a 2D integer list.
+
+        Returns:
+            The area of the largest submatrix within the given input matrix.
+        """
+        m, n = len(matrix), len(matrix[0])
+
+        max_area = 0
+
+        # Calculate the cumulative sum for each column starting from the second row.
+        for j in range(n):
+            for i in range(1, m):
+                matrix[i][j] += matrix[i - 1][j] if matrix[i][j] else 0
+
+        # Sort each row in descending order and calculate the area for each row.
+        for i in range(m):
+            matrix[i].sort(reverse=True)
+            for j in range(n):
+                # Update the maximum area considering the current height and width.
+                max_area = max(max_area, matrix[i][j] * (j + 1))
+
+        return max_area
 
     largestSubmatrix = largest_submatrix
