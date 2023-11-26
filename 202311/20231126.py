ce5f391ad16abd5f702a418cbebdeb826a39f443
Joseph Chiocchi <joe@yolk.cc>
2023-11-26 00:19:43 +0000

    Boilerplate for 20231126
    https://leetcode.com/problems/largest-submatrix-with-rearrangements/

---
+++ 202311/20231126.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/largest-submatrix-with-rearrangements/
+
+
+class Solution:
+    """1727. Largest Submatrix With Rearrangements
+
+    You are given a binary matrix `matrix` of size `m x n`, and you are allowed to
+    rearrange the **columns** of the `matrix` in any order.
+
+    Return *the area of the largest submatrix within* `matrix` *where **every** element
+    of the submatrix is* `1` *after reordering the columns optimally.*
+    """
+
+    def largest_submatrix(self, matrix: list[list[int]]) -> int:
+        ...
+
+    largestSubmatrix = largest_submatrix
