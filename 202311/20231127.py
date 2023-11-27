97d2c339eaf57fa9b0bfa331ead3c89b8ee9dda0
Joseph Chiocchi <input.output@gmail.com>
2023-11-27 02:29:49 +0000

    solution 20231127.py
    - 1233ms (beats 56.41%)
    - 18.20mb (beats 63.64%)

--- 202311/20231127.py
+++ 202311/20231127.py
@@ -1,4 +1,5 @@
 # https://leetcode.com/problems/knight-dialer/
+MOD = 10**9 + 7
 
 
 class Solution:
@@ -29,6 +30,47 @@ class Solution:
     """
 
     def knight_dialer(self, n: int) -> int:
-        ...
+        """How many distinct numbers of length n can be dialed using a Knight's movement.
+
+        10 is the maximum number of digits in a phone number.
+
+        Args:
+            n: The target length of distinct sequence of digits that will form a number.
+
+        Returns:
+            The number of distinct numbers that can be dialed, trimmed to MOD.
+        """
+        max_number_length = 10
+        # Create a moves lookup table, where the knight can move from from starting key.
+        moves = {
+            1: [6, 8],
+            2: [7, 9],
+            3: [4, 8],
+            4: [3, 9, 0],
+            5: [],
+            6: [1, 7, 0],
+            7: [2, 6],
+            8: [1, 3],
+            9: [2, 4],
+            0: [4, 6],
+        }
+
+        # Initialize a 2D array dp where dp[i][j] represents the number of ways to reach
+        # digit j with i moves.
+        dp = [[0] * max_number_length for _ in range(n)]
+
+        # Initialize the base case: with 1 move, there is only 1 way to reach each digit.
+        for i in range(max_number_length):
+            dp[0][i] = 1
+
+        # Populate the dp array using th eknight's moves.
+        for i in range(1, n):
+            for j in range(max_number_length):
+                # The number of ways to reach digit j with i moves is the sum of ways
+                # to reach the next_digit from the moves[j] list with i-1 moves.
+                dp[i][j] = sum(dp[i - 1][next_digit] for next_digit in moves[j]) % MOD
+
+        # Sum up the possibilities for all digits with n moves.
+        return sum(dp[n - 1]) % MOD
 
     knightDialer = knight_dialer
