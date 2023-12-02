7ab1429b952a8f10db9bdc5643201f5a9d9e7f2d
Joseph Chiocchi <input.output@gmail.com>
2023-12-02 00:38:37 +0000

    solution 20231202.py
    - 158ms (beats 52.26%)
    - 16.91mb

--- 202312/20231202.py
+++ 202312/20231202.py
@@ -1,4 +1,5 @@
 # https://leetcode.com/problems/find-words-that-can-be-formed-by-characters/
+from collections import Counter
 
 
 class Solution:
@@ -13,6 +14,30 @@ class Solution:
     """
 
     def count_characters(self, words: list[str], chars: str) -> int:
-        ...
+        """Count all the characters in *good strings*.
+
+        Args:
+            words: Input list of strings that are the words.
+            chars: Predicate characters.
+
+        Returns:
+            Sum of lengths of all good strings in words.
+        """
+        # Count every character in predicate sequence.
+        chars_counter = Counter(chars)
+
+        # Initialize running total
+        total = 0
+
+        for word in words:
+            # Create a Counter object for the characters in the current word.
+            word_counter = Counter(word)
+
+            # Check to see if word_counter is a sub-multiset of chars_counter.
+            if word_counter <= chars_counter:
+                # If it is, add the current length of word to the running total.
+                total += len(word)
+
+        return total
 
     countCharacters = count_characters
