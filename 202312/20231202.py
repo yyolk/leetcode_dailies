02e2babfdcfc0394498ba565936c9b27c555fc5d
Joseph Chiocchi <joe@yolk.cc>
2023-12-02 00:17:28 +0000

    Boilerplate for 20231202
    https://leetcode.com/problems/find-words-that-can-be-formed-by-characters/

---
+++ 202312/20231202.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/find-words-that-can-be-formed-by-characters/
+
+
+class Solution:
+    """1160. Find Words That Can Be Formed by Characters
+
+    You are given an array of strings `words` and a string `chars`.
+
+    A string is **good** if it can be formed by characters from chars (each character
+    can only be used once).
+
+    Return *the sum of lengths of all good strings in words*.
+    """
+
+    def count_characters(self, words: list[str], chars: str) -> int:
+        ...
+
+    countCharacters = count_characters
