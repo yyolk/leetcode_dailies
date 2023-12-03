14b5da738d14b4e6b4a4a41a888526661c8e30c3
Joseph Chiocchi <input.output@gmail.com>
2023-12-03 00:22:02 +0000

    solution 20231203.py
    - 58ms (beats 89.51%)
    - 16.47mb

--- 202312/20231203.py
+++ 202312/20231203.py
@@ -26,6 +26,30 @@ class Solution:
     """
 
     def min_time_to_visit_all_points(self, points: list[list[int]]) -> int:
-        ...
+        """Minimum time for visiting all points.
+
+        Args:
+            points: The input list of points of a 2D plane of coords like x, y to visit
+                in sequence order.
+
+        Returns:
+            The minimum time in seconds to visit all the points in the order given.
+        """
+        total_time = 0
+
+        for i in range(len(points) - 1):
+            x1, y1 = points[i]
+            x2, y2 = points[i + 1]
+
+            dx = abs(x2 - x1)
+            dy = abs(y2 - y1)
+
+            # The minimum time to move from one point to another is the maximum of
+            # dx and dy because you can move vertically, horizontally, or diagonally.
+            min_time = max(dx, dy)
+
+            total_time += min_time
+
+        return total_time
 
     minTimeToVisitAllPoints = min_time_to_visit_all_points
