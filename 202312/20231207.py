b40b4f3f2003f66edf9dc39dc7291d6204c6c829
Joseph Chiocchi <joe@yolk.cc>
2023-12-07 00:18:26 +0000

    Boilerplate for 20231207
    https://leetcode.com/problems/largest-odd-number-in-string/

---
+++ 202312/20231207.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/largest-odd-number-in-string/
+
+
+class Solution:
+    """1903. Largest Odd Number in String
+
+    You are given a string `num`, representing a large integer. Return *the **largest-
+    valued odd** integer (as a string) that is a **non-empty substring** of* `num`*, or
+    an empty string* `""` *if no odd integer exists*.
+
+    A **substring** is a contiguous sequence of characters within a string.
+    """
+
+    def largest_odd_number(self, num: str) -> str:
+        ...
+
+    largestOddNumber = largest_odd_number
