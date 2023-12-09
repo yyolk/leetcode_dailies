260c82f6bf00323748ab545b1d11bfd25785acd4
Joseph Chiocchi <joe@yolk.cc>
2023-12-09 00:18:10 +0000

    Boilerplate for 20231209
    https://leetcode.com/problems/binary-tree-inorder-traversal/

---
+++ 202312/20231209.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/binary-tree-inorder-traversal/
+
+
+class Solution:
+    """94. Binary Tree Inorder Traversal
+
+    Given the `root` of a binary tree, return *the inorder traversal of its nodes'
+    values*.
+    """
+
+    def inorder_traversal(self, root: Optional[TreeNode]) -> list[int]:
+        ...
+
+    inorderTraversal = inorder_traversal
