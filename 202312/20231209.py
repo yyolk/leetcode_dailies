63c6bc16e54d769f057a9c644cb68ee210734712
Joseph Chiocchi <input.output@gmail.com>
2023-12-09 00:46:25 +0000

    solution 20231209.py
    - 30ms (beats 95.33%)
    - 16.26mb (beats 54.15%)

--- 202312/20231209.py
+++ 202312/20231209.py
@@ -6,9 +6,36 @@ class Solution:
 
     Given the `root` of a binary tree, return *the inorder traversal of its nodes'
     values*.
+
+    Definition for a binary tree node.
+
+        class TreeNode:
+            def __init__(self, val=0, left=None, right=None):
+                self.val = val
+                self.left = left
+                self.right = right
     """
 
     def inorder_traversal(self, root: Optional[TreeNode]) -> list[int]:
-        ...
+        """Get the inorder traversal of an input binary tree.
+
+        Args:
+            root: The input binary tree.
+
+        Returns:
+            The inorder traversal of the input its nodes' values.
+        """
+        result: list[int] = []
+
+        def inorder_recursive(node: Optional[TreeNode]):
+            """Helper inner recursive function for traversal."""
+            nonlocal result
+            if node:
+                inorder_recursive(node.left)
+                result.append(node.val)
+                inorder_recursive(node.right)
+
+        inorder_recursive(root)
+        return result
 
     inorderTraversal = inorder_traversal
