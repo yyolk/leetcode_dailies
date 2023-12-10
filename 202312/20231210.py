d07baab19056fcf69d87d446a48d6d3efe7b30cc
Joseph Chiocchi <joe@yolk.cc>
2023-12-10 00:20:13 +0000

    Boilerplate for 20231210
    https://leetcode.com/problems/transpose-matrix/

---
+++ 202312/20231210.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/transpose-matrix/
+
+
+class Solution:
+    """867. Transpose Matrix
+
+    Given a 2D integer array `matrix`, return *the **transpose** of* `matrix`.
+
+    The **transpose** of a matrix is the matrix flipped over its main diagonal,
+    switching the matrix's row and column indices.
+
+    ![](https://assets.leetcode.com/uploads/2021/02/10/hint_transpose.png)
+    """
+
+    def transpose(self, matrix: list[list[int]]) -> list[list[int]]:
+        ...
+
+    transpose = transpose
