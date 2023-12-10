67f4b20c08fc4572f1658967add11cddd8acae5a
Joseph Chiocchi <input.output@gmail.com>
2023-12-10 01:34:34 +0000

    solution 20231210.py
    - 64ms (beats 95.46%)
    - 17.18mb (beats 53.19%)

--- 202312/20231210.py
+++ 202312/20231210.py
@@ -13,6 +13,19 @@ class Solution:
     """
 
     def transpose(self, matrix: list[list[int]]) -> list[list[int]]:
-        ...
+        """Transpose the input matrix.
+
+        Effectively transposes the matrix using `zip(...)`.
+
+        Args:
+            matrix: The input matrix.
+
+        Returns:
+            The transpose of the input matrix, aka the matrix flipped over its main
+            diagonal.
+        """
+        # Use zip to unpack the rows of the matrix and repack those with the same index
+        # into tuples. This effectively transposes the matrix.
+        return list(map(list, zip(*matrix)))
 
     transpose = transpose
