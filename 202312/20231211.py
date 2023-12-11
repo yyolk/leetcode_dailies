9369005a4d4c287a027e543723a83dc47c7b9370
Joseph Chiocchi <input.output@gmail.com>
2023-12-11 02:24:00 +0000

    solution 20231211.py
    - 81ms (beats 94.15%)
    - 18.24mb

--- 202312/20231211.py
+++ 202312/20231211.py
@@ -1,4 +1,5 @@
 # https://leetcode.com/problems/element-appearing-more-than-25-in-sorted-array/
+from collections import Counter
 
 
 class Solution:
@@ -9,6 +10,19 @@ class Solution:
     """
 
     def find_special_integer(self, arr: list[int]) -> int:
-        ...
+        """Find the special integer.
+
+        Args:
+            arr: The input list of integers to search.
+
+        Returns:
+            The one integer in the input that occurs more than 25% of the time.
+        """
+        # Create a counter instance of all the input integers.
+        c = Counter(arr)
+        # Get the most_common results list from the counter, and pick off the first
+        # element and its first item, which is the integer that occured the most and
+        # 25% of the time.
+        return c.most_common()[0][0]
 
     findSpecialInteger = find_special_integer
