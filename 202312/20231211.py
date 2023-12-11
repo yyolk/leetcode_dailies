9777aa1d21d49ce3e02e45876e8d0e4f778476e5
Joseph Chiocchi <joe@yolk.cc>
2023-12-11 00:19:18 +0000

    Boilerplate for 20231211
    https://leetcode.com/problems/element-appearing-more-than-25-in-sorted-array/

---
+++ 202312/20231211.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/element-appearing-more-than-25-in-sorted-array/
+
+
+class Solution:
+    """1287. Element Appearing More Than 25% In Sorted Array
+
+    Given an integer array **sorted** in non-decreasing order, there is exactly one
+    integer in the array that occurs more than 25% of the time, return that integer.
+    """
+
+    def find_special_integer(self, arr: list[int]) -> int:
+        ...
+
+    findSpecialInteger = find_special_integer
