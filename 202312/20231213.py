35cbb97d6d401fd332cf99d9da8cdb9506bace14
Joseph Chiocchi <input.output@gmail.com>
2023-12-13 01:51:31 +0000

    solution 20231213.py
    - 143ms (beats 95.72%)
    - 16.77mb

--- 202312/20231213.py
+++ 202312/20231213.py
@@ -12,6 +12,32 @@ class Solution:
     """
 
     def num_special(self, mat: list[list[int]]) -> int:
-        ...
+        """Number of special position in input matrix.
+
+        Args:
+            mat: The input matrix to search.
+
+        Returns:
+            The number of special positions in the input matrix.
+        """
+        m, n = len(mat), len(mat[0])
+        row_count = [0] * m
+        col_count = [0] * n
+        special_count = 0
+
+        # Count the number of 1s in each row and column.
+        for i in range(m):
+            for j in range(n):
+                if mat[i][j] == 1:
+                    row_count[i] += 1
+                    col_count[j] += 1
+
+        # Check for special_count.
+        for i in range(m):
+            for j in range(n):
+                if mat[i][j] == 1 and row_count[i] == 1 and col_count[j] == 1:
+                    special_count += 1
+
+        return special_count
 
     numSpecial = num_special
