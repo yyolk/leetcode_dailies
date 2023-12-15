c9be970559353c045aaa102477ee7a15cb2de2ec
Joseph Chiocchi <joe@yolk.cc>
2023-12-15 00:18:49 +0000

    Boilerplate for 20231215
    https://leetcode.com/problems/destination-city/

---
+++ 202312/20231215.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/destination-city/
+
+
+class Solution:
+    """1436. Destination City
+
+    You are given the array `paths`, where `paths[i] = [cityAi, cityBi]` means there
+    exists a direct path going from `cityAi` to `cityBi`. *Return the destination city,
+    that is, the city without any path outgoing to another city.*
+
+    It is guaranteed that the graph of paths forms a line without any loop, therefore,
+    there will be exactly one destination city.
+    """
+
+    def dest_city(self, paths: list[list[str]]) -> str:
+        ...
+
+    destCity = dest_city
