efb9c0977d5f16f7bad2387096f0d06827130a7c
Joseph Chiocchi <joe@yolk.cc>
2023-12-19 00:18:39 +0000

    Boilerplate for 20231219
    https://leetcode.com/problems/image-smoother/

---
+++ 202312/20231219.py
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/image-smoother/
+
+
+class Solution:
+    """661. Image Smoother
+
+    An **image smoother** is a filter of the size `3 x 3` that can be applied to each
+    cell of an image by rounding down the average of the cell and the eight surrounding
+    cells (i.e., the average of the nine cells in the blue smoother). If one or more of
+    the surrounding cells of a cell is not present, we do not consider it in the average
+    (i.e., the average of the four cells in the red smoother).
+
+    ![](https://assets.leetcode.com/uploads/2021/05/03/smoother-grid.jpg)
+
+    Given an `m x n` integer matrix `img` representing the grayscale of an image, return
+    *the image after applying the smoother on each cell of it*.
+    """
+
+    def image_smoother(self, img: list[list[int]]) -> list[list[int]]:
+        ...
+
+    imageSmoother = image_smoother
--- 202312/README.md
+++ 202312/README.md
@@ -135,7 +135,9 @@
    </a>
   </td>
   <td class="tue">
-   19
+   <a href="20231219.py">
+    19
+   </a>
   </td>
   <td class="wed">
    20
