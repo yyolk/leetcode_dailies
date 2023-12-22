d84a275bb2b65f3520fddd876d982a8440ce3c6d
Joseph Chiocchi <input.output@gmail.com>
2023-12-22 01:52:45 +0000

    solution 20231222.py
    - 35ms (beats 91.68%)
    - 17.34mb

--- 202312/20231222.py
+++ 202312/20231222.py
@@ -13,6 +13,26 @@ class Solution:
     """
 
     def max_score(self, s: str) -> int:
-        ...
+        max_score = 0
+        zeros_count = 0
+        # Count the number of ones in the whole string.
+        ones_count = s.count("1")
+
+        # Iterate through the string
+        for i in range(len(s) - 1):
+            if s[i] == "0":
+                # Increment zeros count for left substring.
+                zeros_count += 1
+            else:
+                # Decrement ones count for right substring.
+                ones_count -= 1
+
+            # Calculate current score for the split.
+            current_score = zeros_count + ones_count
+
+            # Update max_score if the current score is higher.
+            max_score = max(max_score, current_score)
+
+        return max_score
 
     maxScore = max_score
