3d8d2527177142fb11d3c698ee1d00133ccc2dd4
Joseph Chiocchi <input.output@gmail.com>
2023-12-23 02:02:06 +0000

    solution 20231223.py
    - 32ms (beats 82.83%)
    - 17.48mb

--- 202312/20231223.py
+++ 202312/20231223.py
@@ -13,6 +13,32 @@ class Solution:
     """
 
     def is_path_crossing(self, path: str) -> bool:
-        ...
+        # Initialize tracking coords.
+        x, y = 0, 0
+        # Initialize visited coords set().
+        visited = set()
+        # Add origin (starting point) to visited.
+        visited.add((x, y))
+
+        # Each character in path is a cardinal direction.
+        for move in path:
+            match move:
+                case "N":
+                    y += 1
+                case "S":
+                    y -= 1
+                case "E":
+                    x += 1
+                case "W":
+                    x -= 1
+            # Check if the current position is already visited.
+            if (x, y) in visited:
+                return True
+            
+            # Add the current position, (x, y) coords, to visited.
+            visited.add((x, y))
+
+        # We never crossed our path.
+        return False
 
     isPathCrossing = is_path_crossing
