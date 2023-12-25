ee07b8ad79162cc591529727925409a7904a5225
Joseph Chiocchi <input.output@gmail.com>
2023-12-25 06:12:45 +0000

    solution 20231225.py 🎄
    - 33ms (beats 90.49%)
    - 17.42mb

--- 202312/20231225.py
+++ 202312/20231225.py
@@ -37,6 +37,35 @@ class Solution:
     """
 
     def num_decodings(self, s: str) -> int:
-        ...
+        # Check for empty string or leading zero, which indicates an invalid encoding.
+        if not s or s[0] == "0":
+            return 0
+
+        n = len(s)
+        # Initialize variables to store the previous two results in the dynamic
+        # programming approach.
+        prev, curr = 1, 1
+
+        for i in range(2, n + 1):
+            # Extract the current one-digit and two-digit numbers.
+            one_digit = int(s[i - 1])
+            two_digits = int(s[i - 2 : i])
+
+            # Temporary variable to store the current result.
+            temp = 0
+
+            # Check if the one-digit number is valid (1-9).
+            if 1 <= one_digit <= 9:
+                temp += curr
+
+            # Check if the two-digit number is valid (10-26).
+            if 10 <= two_digits <= 26:
+                temp += prev
+
+            # Update the previous two results for the next iteration.
+            prev, curr = curr, temp
+
+        # When exiting the loop, curr is the number of ways to decode.
+        return curr
 
     numDecodings = num_decodings
