dfd27d11ff0983cb76251eeacff1d4603fce3a69
Joseph Chiocchi <input.output@gmail.com>
2023-12-26 01:27:45 +0000

    solution 20231226.py
    - 520ms
    - 17.72mb (beats 50.35%)

--- 202312/20231226.py
+++ 202312/20231226.py
@@ -1,4 +1,5 @@
 # https://leetcode.com/problems/number-of-dice-rolls-with-target-sum/
+MOD = 10**9 + 7
 
 
 class Solution:
@@ -12,6 +13,18 @@ class Solution:
     """
 
     def num_rolls_to_target(self, n: int, k: int, target: int) -> int:
-        ...
+        # Use dynamic programming - dp[i][j]: number of ways to get sum j using i dice.
+        dp = [[0] * (target + 1) for _ in range(n + 1)]
+        dp[0][0] = 1
+
+        for i in range(1, n + 1):
+            for j in range(1, target + 1):
+                for face in range(1, k + 1):
+                    # Check if the current face value is valid for the current target sum.
+                    if j - face >= 0:
+                        # Accumulate the ways to get the current sum using the current face value.
+                        dp[i][j] = (dp[i][j] + dp[i - 1][j - face]) % MOD
+
+        return dp[n][target]
 
     numRollsToTarget = num_rolls_to_target
