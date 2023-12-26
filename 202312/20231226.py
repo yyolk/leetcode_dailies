0e419799d76263eee381ad6bcb6f207e3fbec9a1
Joseph Chiocchi <joe@yolk.cc>
2023-12-26 00:17:53 +0000

    Boilerplate for 20231226
    https://leetcode.com/problems/number-of-dice-rolls-with-target-sum/

---
+++ 202312/20231226.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/number-of-dice-rolls-with-target-sum/
+
+
+class Solution:
+    """1155. Number of Dice Rolls With Target Sum
+
+    You have `n` dice, and each die has `k` faces numbered from `1` to `k`.
+
+    Given three integers `n`, `k`, and `target`, return *the number of possible ways
+    (out of the* `kn` *total ways)* *to roll the dice, so the sum of the face-up numbers
+    equals* `target`. Since the answer may be too large, return it **modulo** `109 + 7`.
+    """
+
+    def num_rolls_to_target(self, n: int, k: int, target: int) -> int:
+        ...
+
+    numRollsToTarget = num_rolls_to_target
