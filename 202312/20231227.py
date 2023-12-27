a55c35202e0893c5b9ef5dfef007dffc58fa0020
Joseph Chiocchi <input.output@gmail.com>
2023-12-27 01:04:13 +0000

    solution 20231227.py
    - 794ms (beats 98.20%)
    - 28.27mb

--- 202312/20231227.py
+++ 202312/20231227.py
@@ -17,6 +17,34 @@ class Solution:
     """
 
     def min_cost(self, colors: str, needed_time: list[int]) -> int:
-        ...
+        start, consecutive_count, total = 0, 1, 0
+        current_sum = needed_time[0]
+        max_time = needed_time[0]
+
+        # Iterate through the balloons.
+        for i in range(1, len(colors)):
+            # Check if the current balloon has the same color as the previous one.
+            if colors[i] == colors[i - 1]:
+                # If consecutive balloons have the same color, update variables.
+                consecutive_count += 1
+                current_sum += needed_time[i]
+                max_time = max(max_time, needed_time[i])
+            else:
+                # If a different color is encountered, calculate the total time for
+                # consecutive balloons with the same color.
+                if consecutive_count > 1:
+                    total += (current_sum - max_time)
+                
+                # Reset variables for the new color.
+                consecutive_count = 1
+                start = i
+                current_sum = needed_time[i]
+                max_time = needed_time[i]
+
+        # Calculate the total time for the last set of consecutive balloons, if any.
+        if consecutive_count > 1:
+            total += (current_sum - max_time)
+
+        return total
 
     minCost = min_cost
