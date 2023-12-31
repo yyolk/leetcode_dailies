e12756a355e8b159c4d57885095b3de039d8884e
Joseph Chiocchi <joe@yolk.cc>
2023-12-31 00:20:09 +0000

    Boilerplate for 20231231
    https://leetcode.com/problems/largest-substring-between-two-equal-characters/

---
+++ 202312/20231231.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/largest-substring-between-two-equal-characters/
+
+
+class Solution:
+    """1624. Largest Substring Between Two Equal Characters
+
+    Given a string `s`, return *the length of the longest substring between two equal
+    characters, excluding the two characters.* If there is no such substring return
+    `-1`.
+
+    A **substring** is a contiguous sequence of characters within a string.
+    """
+
+    def max_length_between_equal_characters(self, s: str) -> int:
+        ...
+
+    maxLengthBetweenEqualCharacters = max_length_between_equal_characters
