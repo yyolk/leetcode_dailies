21605fc6691d9d7140ed2cdb301f714ec3d8f774
Joseph Chiocchi <joe@yolk.cc>
2024-01-02 00:18:19 +0000

    Boilerplate for 20240102
    https://leetcode.com/problems/convert-an-array-into-a-2d-array-with-conditions/

---
+++ 202401/20240102.py
@@ -0,0 +1,24 @@
+# https://leetcode.com/problems/convert-an-array-into-a-2d-array-with-conditions/
+
+
+class Solution:
+    """2610. Convert an Array Into a 2D Array With Conditions
+
+    You are given an integer array `nums`. You need to create a 2D array from `nums`
+    satisfying the following conditions:
+
+    * The 2D array should contain **only** the elements of the array `nums`.
+
+    * Each row in the 2D array contains **distinct** integers.
+
+    * The number of rows in the 2D array should be **minimal**.
+
+    Return *the resulting array*. If there are multiple answers, return any of them.
+
+    **Note** that the 2D array can have a different number of elements on each row.
+    """
+
+    def find_matrix(self, nums: list[int]) -> list[list[int]]:
+        ...
+
+    findMatrix = find_matrix
