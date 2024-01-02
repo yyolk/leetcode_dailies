3d9a4d1a6c269e5f0eebbd234175d1daccc6bfc9
Joseph Chiocchi <input.output@gmail.com>
2024-01-02 04:51:02 +0000

    solution 20240102.py
    - 50ms (90.15%)
    - 17.37mb

--- 202401/20240102.py
+++ 202401/20240102.py
@@ -19,6 +19,27 @@ class Solution:
     """
 
     def find_matrix(self, nums: list[int]) -> list[list[int]]:
-        ...
+        # Create a frequency array to keep track of the count of each element
+        freq = [0] * (len(nums) + 1)
+        
+        # Initialize the result matrix
+        ans = []
+
+        # Iterate through each element in the input array
+        for c in nums:
+            # Check if the current frequency of the element is greater than or equal to
+            # the number of rows in the result matrix.
+            if freq[c] >= len(ans):
+                # If so, add a new row to the result matrix
+                ans.append([])
+            
+            # Add the current element to the appropriate row in the result matrix
+            ans[freq[c]].append(c)
+            
+            # Update the frequency count of the current element
+            freq[c] += 1
+
+        # Return the resulting matrix
+        return ans
 
     findMatrix = find_matrix
