cac632d976d57058566ce56411a87cc2f4fdd20f
Joseph Chiocchi <joe@yolk.cc>
2024-01-04 00:18:43 +0000

    Boilerplate for 20240104
    https://leetcode.com/problems/minimum-number-of-operations-to-make-array-empty/

---
+++ 202401/20240104.py
@@ -0,0 +1,24 @@
+# https://leetcode.com/problems/minimum-number-of-operations-to-make-array-empty/
+
+
+class Solution:
+    """2870. Minimum Number of Operations to Make Array Empty
+
+    You are given a **0-indexed** array `nums` consisting of positive integers.
+
+    There are two types of operations that you can apply on the array **any** number of
+    times:
+
+    * Choose **two** elements with **equal** values and **delete** them from the array.
+
+    * Choose **three** elements with **equal** values and **delete** them from the
+    array.
+
+    Return *the **minimum** number of operations required to make the array empty, or*
+    `-1` *if it is not possible*.
+    """
+
+    def min_operations(self, nums: list[int]) -> int:
+        ...
+
+    minOperations = min_operations
