6ce49f30f9913d5dd59b1fb99c592a6f9608885e
Joseph Chiocchi <joe@yolk.cc>
2024-01-07 00:20:32 +0000

    Boilerplate for 20240107
    https://leetcode.com/problems/arithmetic-slices-ii-subsequence/

---
+++ 202401/20240107.py
@@ -0,0 +1,29 @@
+# https://leetcode.com/problems/arithmetic-slices-ii-subsequence/
+
+
+class Solution:
+    """446. Arithmetic Slices II - Subsequence
+
+    Given an integer array `nums`, return *the number of all the **arithmetic
+    subsequences** of* `nums`.
+
+    A sequence of numbers is called arithmetic if it consists of **at least three
+    elements** and if the difference between any two consecutive elements is the same.
+
+    * For example, `[1, 3, 5, 7, 9]`, `[7, 7, 7, 7]`, and `[3, -1, -5, -9]` are
+    arithmetic sequences.
+
+    * For example, `[1, 1, 2, 5, 7]` is not an arithmetic sequence.
+
+    A **subsequence** of an array is a sequence that can be formed by removing some
+    elements (possibly none) of the array.
+
+    * For example, `[2,5,10]` is a subsequence of `[1,2,1,**2**,4,1,**5**,**10**]`.
+
+    The test cases are generated so that the answer fits in **32-bit** integer.
+    """
+
+    def number_of_arithmetic_slices(self, nums: list[int]) -> int:
+        ...
+
+    numberOfArithmeticSlices = number_of_arithmetic_slices
