934880de6d73012c8fbfd124d7122b1fd62d28c0
Joseph Chiocchi <input.output@gmail.com>
2024-01-10 20:52:27 -0600

    solution 20240111.py
    - 32ms (beats 97.55%)
    - 18.78mb (beats 94.62%)

--- 202401/20240111.py
+++ 202401/20240111.py
@@ -10,9 +10,33 @@ class Solution:
 
     A node `a` is an ancestor of `b` if either: any child of `a` is equal to `b` or any
     child of `a` is an ancestor of `b`.
+
+    Definition for a binary tree node:
+        class TreeNode:
+            def __init__(self, val=0, left=None, right=None):
+                self.val = val
+                self.left = left
+                self.right = right
     """
 
     def max_ancestor_diff(self, root: Optional[TreeNode]) -> int:
-        ...
+        def dfs(node, min_val, max_val):
+            # Base case: if the node is None, return the difference between max and min
+            if not node:
+                return max_val - min_val
+
+            # Update min and max values encountered so far
+            min_val = min(min_val, node.val)
+            max_val = max(max_val, node.val)
+
+            # Recursively calculate the maximum ancestor difference for left and right subtrees
+            left_diff = dfs(node.left, min_val, max_val)
+            right_diff = dfs(node.right, min_val, max_val)
+
+            # Return the maximum difference between left and right subtrees
+            return max(left_diff, right_diff)
+
+        # Start DFS from the root with initial min and max values set to root's value
+        return dfs(root, root.val, root.val)
 
     maxAncestorDiff = max_ancestor_diff
