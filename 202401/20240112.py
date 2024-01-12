f8f124444e42c680ee8a9cc911ba9f6acd9d695c
Joseph Chiocchi <joe@yolk.cc>
2024-01-12 00:18:38 +0000

    Boilerplate for 20240112
    https://leetcode.com/problems/determine-if-string-halves-are-alike/

---
+++ 202401/20240112.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/determine-if-string-halves-are-alike/
+
+
+class Solution:
+    """1704. Determine if String Halves Are Alike
+
+    You are given a string `s` of even length. Split this string into two halves of
+    equal lengths, and let `a` be the first half and `b` be the second half.
+
+    Two strings are **alike** if they have the same number of vowels (`'a'`, `'e'`,
+    `'i'`, `'o'`, `'u'`, `'A'`, `'E'`, `'I'`, `'O'`, `'U'`). Notice that `s` contains
+    uppercase and lowercase letters.
+
+    Return `true` *if* `a` *and* `b` *are **alike***. Otherwise, return `false`.
+    """
+
+    def halves_are_alike(self, s: str) -> bool:
+        ...
+
+    halvesAreAlike = halves_are_alike
