4e066c0ae608e4692cb94ae8a5e30ad3436ce5df
Joseph Chiocchi <joe@yolk.cc>
2024-01-23 00:19:13 +0000

    Boilerplate for 20240123
    https://leetcode.com/problems/maximum-length-of-a-concatenated-string-with-unique-characters/

---
+++ 202401/20240123.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/maximum-length-of-a-concatenated-string-with-unique-characters/
+
+
+class Solution:
+    """1239. Maximum Length of a Concatenated String with Unique Characters
+
+    You are given an array of strings `arr`. A string `s` is formed by the
+    **concatenation** of a **subsequence** of `arr` that has **unique characters**.
+
+    Return *the **maximum** possible length* of `s`.
+
+    A **subsequence** is an array that can be derived from another array by deleting
+    some or no elements without changing the order of the remaining elements.
+    """
+
+    def max_length(self, arr: list[str]) -> int:
+        ...
+
+    maxLength = max_length
