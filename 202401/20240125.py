ab2ea3f8c704ada09179c1c460e4c9b15114f318
Joseph Chiocchi <input.output@gmail.com>
2024-01-24 18:55:08 -0600

    solution 20240125.py
    - 467ms (beats 87.67%)
    - 41.76mb (beats 74.31%)

--- 202401/20240125.py
+++ 202401/20240125.py
@@ -18,6 +18,20 @@ class Solution:
     """
 
     def longest_common_subsequence(self, text1: str, text2: str) -> int:
-        ...
+        m, n = len(text1), len(text2)
+        
+        # Initialize a 2D array to store the lengths of common subsequences
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        
+        # Build the DP array
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if text1[i - 1] == text2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + 1
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+        
+        # The result is stored in the bottom-right cell of the DP array
+        return dp[m][n]
 
     longestCommonSubsequence = longest_common_subsequence
