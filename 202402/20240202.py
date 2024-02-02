6e802ffade0f47d5e1aa2f21d280424cd5faf2c6
Joseph Chiocchi <joe@yolk.cc>
2024-02-02 00:17:32 +0000

    Boilerplate for 20240202
    https://leetcode.com/problems/sequential-digits/

---
+++ 202402/20240202.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/sequential-digits/
+
+
+class Solution:
+    """1291. Sequential Digits
+
+    An integer has *sequential digits* if and only if each digit in the number is one
+    more than the previous digit.
+
+    Return a **sorted** list of all the integers in the range `[low, high]` inclusive
+    that have sequential digits.
+
+    """
+
+    def sequential_digits(self, low: int, high: int) -> list[int]: ...
+
+    sequentialDigits = sequential_digits
