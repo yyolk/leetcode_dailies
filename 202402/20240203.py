7478aed825668f6ab0537484922f2801533b7d1a
Joseph Chiocchi <joe@yolk.cc>
2024-02-04 00:19:24 +0000

    Boilerplate for 20240204
    https://leetcode.com/problems/minimum-window-substring/

--- 202402/20240203.py
+++ 202402/20240203.py
@@ -15,19 +15,19 @@ class Solution:
 
     def max_sum_after_partitioning(self, arr: list[int], k: int) -> int:
         n = len(arr)
-        
+
         # Initialize an array to store the maximum sum for each position
         dp = [0] * (n + 1)
 
         # Iterate through the array
         for i in range(1, n + 1):
-            max_val = float('-inf')
-            
+            max_val = float("-inf")
+
             # Consider subarrays of length at most k
             for j in range(1, min(i, k) + 1):
                 # Update the maximum value in the current subarray
                 max_val = max(max_val, arr[i - j])
-                
+
                 # Update the maximum sum for the current position
                 dp[i] = max(dp[i], dp[i - j] + max_val * j)
 
