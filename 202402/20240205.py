806be4686032ec6478b707debf1ad409ce0f24e9
Joseph Chiocchi <input.output@gmail.com>
2024-02-04 18:54:31 -0600

    solution 20240205.py
    - 75ms (beats 88.69%)
    - 16.86mb (beats 59.20%)

--- 202402/20240205.py
+++ 202402/20240205.py
@@ -1,4 +1,5 @@
 # https://leetcode.com/problems/first-unique-character-in-a-string/
+from collections import Counter
 
 
 class Solution:
@@ -9,6 +10,15 @@ class Solution:
 
     """
 
-    def first_uniq_char(self, s: str) -> int: ...
+    def first_uniq_char(self, s: str) -> int:
+        char_count = Counter(s)
+
+        # Iterate through the string to find the first non-repeating character
+        for i, char in enumerate(s):
+            if char_count[char] == 1:
+                return i
+
+        # If no non-repeating character is found, return -1
+        return -1
 
     firstUniqChar = first_uniq_char
