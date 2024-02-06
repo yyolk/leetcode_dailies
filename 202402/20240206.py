2f8c527e9f190bd2574b5f5891ed12eb4a63af8f
Joseph Chiocchi <joe@yolk.cc>
2024-02-06 00:17:24 +0000

    Boilerplate for 20240206
    https://leetcode.com/problems/group-anagrams/

---
+++ 202402/20240206.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/group-anagrams/
+
+
+class Solution:
+    """49. Group Anagrams
+
+    Given an array of strings `strs`, group **the anagrams** together. You can return
+    the answer in **any order**.
+
+    An **Anagram** is a word or phrase formed by rearranging the letters of a different
+    word or phrase, typically using all the original letters exactly once.
+
+    """
+
+    def group_anagrams(self, strs: list[str]) -> list[list[str]]: ...
+
+    groupAnagrams = group_anagrams
