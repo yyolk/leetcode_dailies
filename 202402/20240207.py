29e58ac4ceb53d1245c4e10b7b800e2ec9fc5d8a
Joseph Chiocchi <input.output@gmail.com>
2024-02-06 18:39:57 -0600

    solution 20240207.py
    - 44ms (beats 73.73%)
    - 17.75mb (beats 83.41%)

--- 202402/20240207.py
+++ 202402/20240207.py
@@ -1,4 +1,5 @@
 # https://leetcode.com/problems/sort-characters-by-frequency/
+from collections import Counter
 
 
 class Solution:
@@ -12,6 +13,16 @@ class Solution:
 
     """
 
-    def frequency_sort(self, s: str) -> str: ...
+    def frequency_sort(self, s: str) -> str:
+        # Count the frequency of each character in the string
+        char_freq = Counter(s)
+
+        # Sort the characters based on their frequency in decreasing order
+        sorted_chars = sorted(char_freq, key=lambda x: char_freq[x], reverse=True)
+
+        # Build the sorted string by repeating each character according to its frequency
+        result = ''.join(char * char_freq[char] for char in sorted_chars)
+
+        return result
 
     frequencySort = frequency_sort
