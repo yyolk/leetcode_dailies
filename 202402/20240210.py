1b7cf76a5bbe57f7796ee067b8ebfaa74283d68f
Joseph Chiocchi <joe@yolk.cc>
2024-02-10 00:17:04 +0000

    Boilerplate for 20240210
    https://leetcode.com/problems/palindromic-substrings/

---
+++ 202402/20240210.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/palindromic-substrings/
+
+
+class Solution:
+    """647. Palindromic Substrings
+
+    Given a string `s`, return *the number of **palindromic substrings** in it*.
+
+    A string is a **palindrome** when it reads the same backward as forward.
+
+    A **substring** is a contiguous sequence of characters within the string.
+
+    """
+
+    def count_substrings(self, s: str) -> int: ...
+
+    countSubstrings = count_substrings
