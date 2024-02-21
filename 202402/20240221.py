07a8aebd9cd372e9b0f8eade02cdc885376fb060
Joseph Chiocchi <input.output@gmail.com>
2024-02-20 20:49:58 -0600

    solution 20240221.py
    - 43ms (beats 90.76%)
    - 16.51mb (beats 81.69%)

--- 202402/20240221.py
+++ 202402/20240221.py
@@ -9,6 +9,15 @@ class Solution:
 
     """
 
-    def range_bitwise_and(self, left: int, right: int) -> int: ...
+    def range_bitwise_and(self, left: int, right: int) -> int:
+        shift = 0
+        # Find the common prefix of the binary representations of left and right
+        while left < right:
+            left >>= 1
+            right >>= 1
+            shift += 1
+
+        # Left shift back to obtain the final result
+        return left << shift
 
     rangeBitwiseAnd = range_bitwise_and
