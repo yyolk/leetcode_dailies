49ce7289928696fa966253c5241ee15f44a14b40
Joseph Chiocchi <input.output@gmail.com>
2024-02-22 19:49:54 -0600

    solution 20240223.py
    - 103ms (beats 57.44%)
    - 18.29mb (beats 52.50%)

--- 202402/20240223.py
+++ 202402/20240223.py
@@ -1,4 +1,5 @@
 # https://leetcode.com/problems/cheapest-flights-within-k-stops/
+import heapq
 
 
 class Solution:
@@ -16,6 +17,34 @@ class Solution:
 
     def find_cheapest_price(
         self, n: int, flights: list[list[int]], src: int, dst: int, k: int
-    ) -> int: ...
+    ) -> int:
+        # Create an adjacency list to represent the graph
+        graph = {i: [] for i in range(n)}
+        for flight in flights:
+            graph[flight[0]].append((flight[1], flight[2]))
+
+        # Priority queue to store (cost, current_city, stops_left)
+        heap = [(0, src, k + 1)]
+        
+        # Dictionary to memoize the minimum cost for each (city, stops_left) pair
+        memo = {}
+
+        while heap:
+            cost, current_city, stops_left = heapq.heappop(heap)
+
+            # If the destination is reached, return the cost
+            if current_city == dst:
+                return cost
+
+            # If stops are left, explore neighbors
+            if stops_left > 0:
+                for neighbor, price in graph[current_city]:
+                    # Use memoization to avoid redundant computations
+                    if (neighbor, stops_left - 1) not in memo or cost + price < memo[(neighbor, stops_left - 1)]:
+                        heapq.heappush(heap, (cost + price, neighbor, stops_left - 1))
+                        memo[(neighbor, stops_left - 1)] = cost + price
+
+        # If destination is not reached within the given stops
+        return -1
 
     findCheapestPrice = find_cheapest_price
