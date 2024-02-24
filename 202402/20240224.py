e0e75ce1834265934e102116365168d75ea1e1cc
Joseph Chiocchi <input.output@gmail.com>
2024-02-23 20:24:03 -0600

    solution 20240224.py
    - 1593ms (beats 71.52%)
    - 59.21mb

--- 202402/20240224.py
+++ 202402/20240224.py
@@ -1,4 +1,5 @@
 # https://leetcode.com/problems/find-all-people-with-secret/
+from collections import deque
 
 
 class Solution:
@@ -26,6 +27,49 @@ class Solution:
 
     def find_all_people(
         self, n: int, meetings: list[list[int]], first_person: int
-    ) -> list[int]: ...
+    ) -> list[int]:
+        # Initialize the set with people who initially know the secret
+        known_set = set([0, first_person])
+        
+        # Sort meetings by time
+        sorted_meetings = []
+        meetings.sort(key=lambda x: x[2])
+
+        seen_time = set()
+
+        # Group meetings by time
+        for meeting in meetings:
+            if meeting[2] not in seen_time:
+                seen_time.add(meeting[2])
+                sorted_meetings.append([])
+            sorted_meetings[-1].append((meeting[0], meeting[1]))
+
+        # Process meetings in sorted order
+        for meeting_group in sorted_meetings:
+            people_know_secret = set()
+            graph = defaultdict(list)
+
+            # Build graph and identify people who already know the secret
+            for p1, p2 in meeting_group:
+                graph[p1].append(p2)
+                graph[p2].append(p1)
+                if p1 in known_set:
+                    people_know_secret.add(p1)
+                if p2 in known_set:
+                    people_know_secret.add(p2)
+
+            # Use BFS to propagate the secret to connected people
+            queue = deque(people_know_secret)
+
+            while queue:
+                curr = queue.popleft()
+                known_set.add(curr)
+                for neighbor in graph[curr]:
+                    if neighbor not in known_set:
+                        known_set.add(neighbor)
+                        queue.append(neighbor)
+
+        # Convert the final set to a list and return
+        return list(known_set)
 
     findAllPeople = find_all_people
