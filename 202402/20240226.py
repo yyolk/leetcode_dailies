4fea09e3f70acfb0b769977ef8f2d04325c2284f
Joseph Chiocchi <input.output@gmail.com>
2024-02-25 19:32:06 -0600

    solution 20240226.py
    - 34ms (beats 70.24%)
    - 16.48mn (beats 98.07%)

--- 202402/20240226.py
+++ 202402/20240226.py
@@ -10,8 +10,30 @@ class Solution:
     Two binary trees are considered the same if they are structurally identical, and the
     nodes have the same value.
 
+    Definition for a binary tree node:
+    
+        class TreeNode:
+            def __init__(self, val=0, left=None, right=None):
+                self.val = val
+                self.left = left
+                self.right = right
+
     """
 
-    def is_same_tree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool: ...
+    def is_same_tree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        # Base case: If both trees are None, they are the same
+        if not p and not q:
+            return True
+        # If one of the trees is None and the other is not, they are not the same
+        elif not p or not q:
+            return False
+        # Check if current nodes have the same value and recursively check their left and right subtrees
+        else:
+            return (
+                p.val == q.val and
+                self.is_same_tree(p.left, q.left) and
+                self.is_same_tree(p.right, q.right)
+            )
 
     isSameTree = is_same_tree
+        
