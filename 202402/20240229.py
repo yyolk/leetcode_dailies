4e273c8558e34a91cd1b05050d223d4faf692c52
Joseph Chiocchi <input.output@gmail.com>
2024-02-28 20:10:14 -0600

    solution 20240229.py
    - 203ms (beats 95.06%)
    - 37.09mb (beats 56.17%)

--- 202402/20240229.py
+++ 202402/20240229.py
@@ -1,4 +1,6 @@
 # https://leetcode.com/problems/even-odd-tree/
+from collections import deque
+from typing import Optional
 
 
 class Solution:
@@ -18,8 +20,47 @@ class Solution:
     Given the `root` of a binary tree, *return* `true` *if the binary tree is **Even-
     Odd**, otherwise return* `false`*.*
 
+    Definition for a binary tree node:
+
+        class TreeNode:
+            def __init__(self, val=0, left=None, right=None):
+                self.val = val
+                self.left = left
+                self.right = right
+
     """
 
-    def is_even_odd_tree(self, root: Optional[TreeNode]) -> bool: ...
+    def is_even_odd_tree(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return True
+
+        queue = deque([root])
+        level = 0
+
+        while queue:
+            level_size = len(queue)
+            prev_value = None  # To track the previous node's value within the current level
+
+            for _ in range(level_size):
+                node = queue.popleft()
+
+                # Check the validity of the current node's value based on the level
+                if (level % 2 == 0 and (node.val % 2 == 0 or (prev_value is not None and node.val <= prev_value))) or \
+                        (level % 2 == 1 and (node.val % 2 == 1 or (prev_value is not None and node.val >= prev_value))):
+                    return False
+
+                # Update the previous value for the next iteration
+                prev_value = node.val
+
+                # Add the children to the queue if they exist
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+            # Move to the next level
+            level += 1
+
+        return True
 
     isEvenOddTree = is_even_odd_tree
