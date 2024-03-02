d555aa089ae6ec0928085ec151d194f0b6140442
Joseph Chiocchi <input.output@gmail.com>
2024-03-01 20:45:56 -0600

    solution 20240302.py
    - 157ms (beats 55.14%)
    - 18.70mb (beats 72.31%)

--- 202403/20240302.py
+++ 202403/20240302.py
@@ -9,6 +9,13 @@ class Solution:
 
     """
 
-    def sorted_squares(self, nums: list[int]) -> list[int]: ...
+    def sorted_squares(self, nums: list[int]) -> list[int]:
+        # Square each element in the array
+        squares = [num ** 2 for num in nums]
+
+        # Sort the squared values in non-decreasing order
+        sorted_squares = sorted(squares)
+
+        return sorted_squares
 
     sortedSquares = sorted_squares
