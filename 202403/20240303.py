1324f9de5c09676191b5f53c6b9a8d75a7136595
Joseph Chiocchi <input.output@gmail.com>
2024-03-02 18:38:35 -0600

    solution 20240303.py
    - 27ms (beats 97.59%)
    - 16.57mb (beats 61.49%)

--- 202403/20240303.py
+++ 202403/20240303.py
@@ -1,4 +1,5 @@
 # https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+from typing import Optional
 
 
 class Solution:
@@ -11,6 +12,24 @@ class Solution:
 
     def remove_nth_from_end(
         self, head: Optional[ListNode], n: int
-    ) -> Optional[ListNode]: ...
+    ) -> Optional[ListNode]:
+        # Create a dummy node to simplify edge cases
+        dummy = ListNode(0)
+        dummy.next = head
+        fast = slow = dummy
+
+        # Move fast pointer n + 1 steps ahead
+        for _ in range(n + 1):
+            fast = fast.next
+
+        # Move both pointers until the fast pointer reaches the end
+        while fast is not None:
+            fast = fast.next
+            slow = slow.next
+
+        # Remove the nth node from the end
+        slow.next = slow.next.next
+
+        return dummy.next
 
     removeNthFromEnd = remove_nth_from_end
