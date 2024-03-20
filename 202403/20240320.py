fc814e4c6214326794b82af265b1f351679e3531
Joseph Chiocchi <input.output@gmail.com>
2024-03-19 19:46:26 -0500

    solution 20240320.py
    - 179ms (beats 94.25%)
    - 21.11mb

--- 202403/20240320.py
+++ 202403/20240320.py
@@ -16,10 +16,37 @@ class Solution:
 
     *Build the result list and return its head.*
 
+    Definition for singly-linked list::
+
+        class ListNode:
+            def __init__(self, val=0, next=None):
+                self.val = val
+                self.next = next
+
     """
 
     def merge_in_between(
         self, list1: ListNode, a: int, b: int, list2: ListNode
-    ) -> ListNode: ...
+    ) -> ListNode:
+        # Initialize pointers
+        ptr = list1  # Pointer to ath node
+        for _ in range(a - 1):
+            ptr = ptr.next  # Move ptr to the node before ath
+        
+        qtr = ptr.next  # Pointer to node after bth
+        for _ in range(b - a + 1):
+            qtr = qtr.next  # Move qtr to the node after bth
+        
+        # Connect list1 from ath to list2
+        ptr.next = list2
+        
+        # Traverse list2 to find its tail node
+        while list2.next:
+            list2 = list2.next
+        
+        # Connect the tail node of list2 to qtr in list1
+        list2.next = qtr
+        
+        return list1
 
     mergeInBetween = merge_in_between
