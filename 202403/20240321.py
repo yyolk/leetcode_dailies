948d37f7d6ab567edc7b095a4992cf704de62b43
Joseph Chiocchi <joe@yolk.cc>
2024-03-22 00:17:27 +0000

    Boilerplate for 20240322
    https://leetcode.com/problems/palindrome-linked-list/

--- 202403/20240321.py
+++ 202403/20240321.py
@@ -13,7 +13,7 @@ class Solution:
         class ListNode:
             def __init__(self, val=0, next=None):
                 self.val = val
-                self.next = next    
+                self.next = next
 
     """
 
---
+++ 202403/20240322.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/palindrome-linked-list/
+
+
+class Solution:
+    """234. Palindrome Linked List
+
+    Given the `head` of a singly linked list, return `true` *if it is a* *palindrome*
+    *or* `false` *otherwise*.
+
+    """
+
+    def is_palindrome(self, head: Optional[ListNode]) -> bool: ...
+
+    isPalindrome = is_palindrome
