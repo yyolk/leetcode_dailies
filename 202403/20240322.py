457341c28dfba32031d5b9da4190269f4760d0c7
Joseph Chiocchi <input.output@gmail.com>
2024-03-21 21:18:08 -0500

    solution 20240322.py
    - 266ms (beats 95.13%)
    - 31.81mb (beats 93.07%)

--- 202403/20240322.py
+++ 202403/20240322.py
@@ -1,4 +1,5 @@
 # https://leetcode.com/problems/palindrome-linked-list/
+from typing import Optional
 
 
 class Solution:
@@ -7,8 +8,45 @@ class Solution:
     Given the `head` of a singly linked list, return `true` *if it is a* *palindrome*
     *or* `false` *otherwise*.
 
+    Definition for singly-linked list::
+        class ListNode:
+            def __init__(self, val=0, next=None):
+                self.val = val
+                self.next = next
     """
 
-    def is_palindrome(self, head: Optional[ListNode]) -> bool: ...
+    def is_palindrome(self, head: Optional[ListNode]) -> bool:
+        # Helper function to reverse a linked list
+        def reverse_linked_list(node):
+            prev = None
+            current = node
+            while current:
+                next_node = current.next
+                current.next = prev
+                prev = current
+                current = next_node
+            return prev
+
+        if not head or not head.next:
+            return True
+
+        # Find the middle of the linked list using slow and fast pointers
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        # Reverse the second half of the linked list
+        second_half_reversed = reverse_linked_list(slow)
+
+        # Compare the first half with the reversed second half
+        while second_half_reversed:
+            if head.val != second_half_reversed.val:
+                return False
+            head = head.next
+            second_half_reversed = second_half_reversed.next
+
+        return True
 
     isPalindrome = is_palindrome
+        
