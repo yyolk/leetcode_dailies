a59e03008add8a69017062670ba42ede6340a107
Joseph Chiocchi <joe@yolk.cc>
2024-03-26 00:17:34 +0000

    Boilerplate for 20240326
    https://leetcode.com/problems/first-missing-positive/

---
+++ 202403/20240326.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/first-missing-positive/
+
+
+class Solution:
+    """41. First Missing Positive
+
+    Given an unsorted integer array `nums`. Return the *smallest positive integer* that
+    is *not present* in `nums`.
+
+    You must implement an algorithm that runs in `O(n)` time and uses `O(1)` auxiliary
+    space.
+
+    """
+
+    def first_missing_positive(self, nums: list[int]) -> int: ...
+
+    firstMissingPositive = first_missing_positive
--- 202403/README.md
+++ 202403/README.md
@@ -172,7 +172,9 @@
    </a>
   </td>
   <td class="tue">
-   26
+   <a href="20240326.py">
+    26
+   </a>
   </td>
   <td class="wed">
    27
