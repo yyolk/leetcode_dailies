a69a7ab67944c4da118c30cb7fc50fabf57adc39
Joseph Chiocchi <joe@yolk.cc>
2024-03-27 00:17:20 +0000

    Boilerplate for 20240327
    https://leetcode.com/problems/subarray-product-less-than-k/

---
+++ 202403/20240327.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/subarray-product-less-than-k/
+
+
+class Solution:
+    """713. Subarray Product Less Than K
+
+    Given an array of integers `nums` and an integer `k`, return *the number of
+    contiguous subarrays where the product of all the elements in the subarray is
+    strictly less than* `k`.
+
+    """
+
+    def num_subarray_product_less_than_k(self, nums: list[int], k: int) -> int: ...
+
+    numSubarrayProductLessThanK = num_subarray_product_less_than_k
