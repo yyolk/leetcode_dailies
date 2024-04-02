47bc53bc12f560a44b47924999c8b694a5a0d27e
Joseph Chiocchi <joe@yolk.cc>
2024-04-02 00:17:58 +0000

    Boilerplate for 20240402
    https://leetcode.com/problems/isomorphic-strings/

---
+++ 202404/20240402.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/isomorphic-strings/
+
+
+class Solution:
+    """205. Isomorphic Strings
+
+    Given two strings `s` and `t`, *determine if they are isomorphic*.
+
+    Two strings `s` and `t` are isomorphic if the characters in `s` can be replaced to
+    get `t`.
+
+    All occurrences of a character must be replaced with another character while
+    preserving the order of characters. No two characters may map to the same character,
+    but a character may map to itself.
+
+    """
+
+    def is_isomorphic(self, s: str, t: str) -> bool: ...
+
+    isIsomorphic = is_isomorphic
