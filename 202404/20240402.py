940c10e7e21eda94090bbe96b0d9e8d8c453ced3
Joseph Chiocchi <input.output@gmail.com>
2024-04-02 19:04:32 -0500

    solution 20240402.py
    - 39ms (beats 77.42%)
    - 16.86mb

--- 202404/20240402.py
+++ 202404/20240402.py
@@ -15,6 +15,29 @@ class Solution:
 
     """
 
-    def is_isomorphic(self, s: str, t: str) -> bool: ...
+    def is_isomorphic(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        mapping_s_t = {}
+        mapping_t_s = {}
+
+        for i in range(len(s)):
+            char_s = s[i]
+            char_t = t[i]
+
+            if char_s in mapping_s_t:
+                if mapping_s_t[char_s] != char_t:
+                    return False
+            else:
+                mapping_s_t[char_s] = char_t
+
+            if char_t in mapping_t_s:
+                if mapping_t_s[char_t] != char_s:
+                    return False
+            else:
+                mapping_t_s[char_t] = char_s
+
+        return True
 
     isIsomorphic = is_isomorphic
