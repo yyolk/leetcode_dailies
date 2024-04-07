e1935449818f46bbdd50dbba3444d3c6544ba329
Joseph Chiocchi <input.output@gmail.com>
2024-04-06 19:35:29 -0500

    solution 20240407.py
    - 30ms (beats 87.55%)
    - 16.50mb (beats 50.32%)
    
    nit: format

--- 202404/20240407.py
+++ 202404/20240407.py
@@ -20,6 +20,35 @@ class Solution:
 
     """
 
-    def check_valid_string(self, s: str) -> bool: ...
+    def check_valid_string(self, s: str) -> bool:
+        # Stacks to keep track of indices of left parentheses and asterisks
+        left_stack = []
+        star_stack = []
+
+        for i, char in enumerate(s):
+            if char == '(':
+                left_stack.append(i)
+            elif char == '*':
+                star_stack.append(i)
+            else:  # char == ')'
+                # Pop from left_stack if a matching left parenthesis is found
+                if left_stack:
+                    left_stack.pop()
+                # If no matching left parenthesis, use an asterisk as one
+                elif star_stack:
+                    star_stack.pop()
+                else:
+                    return False
+
+        # Match remaining left parentheses with asterisks if possible
+        while left_stack and star_stack:
+            if left_stack[-1] < star_stack[-1]:
+                left_stack.pop()
+                star_stack.pop()
+            else:
+                break
+
+        # If all left parentheses are matched, the string is valid
+        return not left_stack
 
     checkValidString = check_valid_string
