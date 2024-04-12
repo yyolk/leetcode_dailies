c5bc6db7bf26ecd3abc9bd7bc8c8f8cbe7eeb01b
Joseph Chiocchi <joe@yolk.cc>
2024-04-12 00:17:56 +0000

    Boilerplate for 20240412
    https://leetcode.com/problems/trapping-rain-water/

---
+++ 202404/20240412.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/trapping-rain-water/
+
+
+class Solution:
+    """42. Trapping Rain Water
+
+    Given `n` non-negative integers representing an elevation map where the width of
+    each bar is `1`, compute how much water it can trap after raining.
+
+    """
+
+    def trap(self, height: list[int]) -> int: ...
+
+    trap = trap
