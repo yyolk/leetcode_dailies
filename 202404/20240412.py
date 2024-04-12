704912666bca8dc8afb17ad5327a16a7049c3905
Joseph Chiocchi <input.output@gmail.com>
2024-04-11 19:25:29 -0500

    solution 20240412.py
    - 79ms (beats 99.50%)
    - 18.58mb

--- 202404/20240412.py
+++ 202404/20240412.py
@@ -9,6 +9,29 @@ class Solution:
 
     """
 
-    def trap(self, height: list[int]) -> int: ...
+    def trap(self, height: list[int]) -> int:
+        if not height:
+            return 0
 
-    trap = trap
+        # Initialize two pointers
+        left, right = 0, len(height) - 1
+        # Initialize max heights
+        left_max, right_max = height[left], height[right]
+
+        total_water = 0
+
+        while left < right:
+            if height[left] < height[right]:
+                if height[left] >= left_max:
+                    left_max = height[left]
+                else:
+                    total_water += left_max - height[left]
+                left += 1
+            else:
+                if height[right] >= right_max:
+                    right_max = height[right]
+                else:
+                    total_water += right_max - height[right]
+                right -= 1
+
+        return total_water
