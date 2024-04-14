3e729efc3186f7f3712e7d7969dde785acb1a83f
Joseph Chiocchi <input.output@gmail.com>
2024-04-13 20:26:22 -0500

    solution 20240414.py
    - 27ms (beats 96.78%)
    - 16.70mb (beats 93.56%)

--- 202404/20240414.py
+++ 202404/20240414.py
@@ -1,4 +1,5 @@
 # https://leetcode.com/problems/sum-of-left-leaves/
+from typing import Optional
 
 
 class Solution:
@@ -9,8 +10,35 @@ class Solution:
     A **leaf** is a node with no children. A **left leaf** is a leaf that is the left
     child of another node.
 
+    Definition for a binary tree node:
+
+        class TreeNode:
+            def __init__(self, val=0, left=None, right=None):
+                self.val = val
+                self.left = left
+                self.right = right
+
     """
 
-    def sum_of_left_leaves(self, root: Optional[TreeNode]) -> int: ...
+    def sum_of_left_leaves(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        
+        def is_leaf(node):
+            return node and not node.left and not node.right
+        
+        def dfs(node):
+            if not node:
+                return 0
+            
+            left_sum = dfs(node.left) if node.left else 0
+            right_sum = dfs(node.right) if node.right else 0
+            
+            if is_leaf(node.left):
+                return node.left.val + right_sum
+            else:
+                return left_sum + right_sum
+        
+        return dfs(root)
 
     sumOfLeftLeaves = sum_of_left_leaves
