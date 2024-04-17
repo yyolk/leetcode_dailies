328bb13c5f0494b02ecd8c4e50b758143e8db894
Joseph Chiocchi <input.output@gmail.com>
2024-04-16 21:01:03 -0500

    solution 20240417.py
    - 33ms (beats 96.67%)
    - 18.24mb

--- 202404/20240417.py
+++ 202404/20240417.py
@@ -1,4 +1,6 @@
 # https://leetcode.com/problems/smallest-string-starting-from-leaf/
+from collections import deque
+from typing import Optional
 
 
 class Solution:
@@ -16,8 +18,45 @@ class Solution:
 
     A leaf of a node is a node that has no children.
 
-    """
+    Definition for a binary tree node::
 
-    def smallest_from_leaf(self, root: Optional[TreeNode]) -> str: ...
+        class TreeNode:
+            def __init__(self, val=0, left=None, right=None):
+                self.val = val
+                self.left = left
+                self.right = right
+
+    """
+    def num_to_char(self, number):
+        # Converts a number to its corresponding character (in the range 'a' to 'z')
+        return chr(ord("a") + number)
+
+    def smallest_from_leaf(self, root: Optional[TreeNode]) -> str:
+        # Initialize a queue and add the root node with its path (just the root's value)
+        queue = deque()
+        queue.append((root, self.num_to_char(root.val)))
+        results = []
+
+        # Traverse the tree using BFS
+        while queue:
+            # Get the current node and its path from the queue
+            cur, path = queue.popleft()
+
+            # If the current node is a leaf, add its path to the results list
+            if not cur.left and not cur.right:
+                # Reverse the path to get the correct order
+                results.append(path[::-1])
+
+            # If the current node has a left child, add it to the queue
+            if cur.left:
+                queue.append((cur.left, path + self.num_to_char(cur.left.val)))
+
+            # If the current node has a right child, add it to the queue
+            if cur.right:
+                queue.append((cur.right, path + self.num_to_char(cur.right.val)))
+
+        # Sort the results and return the first (smallest) string
+        results.sort()
+        return results[0]
 
     smallestFromLeaf = smallest_from_leaf
