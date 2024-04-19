2ba36fd1e07a463ad5dd0604043aa42ff1c3a856
Joseph Chiocchi <input.output@gmail.com>
2024-04-19 17:55:07 -0500

    solution 20240419.py
    - 225ms (beats 90.97%)
    - 18.83mb (beats 83.21%)

--- 202404/20240419.py
+++ 202404/20240419.py
@@ -13,6 +13,28 @@ class Solution:
 
     """
 
-    def num_islands(self, grid: list[list[str]]) -> int: ...
+    def num_islands(self, grid: list[list[str]]) -> int:
+        if not grid:
+            return 0
+
+        rows, cols = len(grid), len(grid[0])
+        count = 0
+
+        def dfs(i, j):
+            if i < 0 or i >= rows or j < 0 or j >= cols or grid[i][j] == '0':
+                return
+            grid[i][j] = '0'  # Mark the current cell as visited
+            dfs(i + 1, j)  # Check down
+            dfs(i - 1, j)  # Check up
+            dfs(i, j + 1)  # Check right
+            dfs(i, j - 1)  # Check left
+
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == '1':
+                    count += 1
+                    dfs(i, j)
+
+        return count
 
     numIslands = num_islands
