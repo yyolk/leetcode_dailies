e9f4427c42f8136ecf7616c71bb7c71cb219b1a7
Joseph Chiocchi <joe@yolk.cc>
2024-05-06 00:19:02 +0000

    Boilerplate for 20240506
    https://leetcode.com/problems/remove-nodes-from-linked-list/

---
+++ 202405/20240506.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/remove-nodes-from-linked-list/
+
+
+class Solution:
+    """2487. Remove Nodes From Linked List
+
+    You are given the `head` of a linked list.
+
+    Remove every node which has a node with a greater value anywhere to the right side
+    of it.
+
+    Return *the* `head` *of the modified linked list.*
+
+    """
+
+    def remove_nodes(self, head: Optional[ListNode]) -> Optional[ListNode]: ...
+
+    removeNodes = remove_nodes
--- 202405/README.md
+++ 202405/README.md
@@ -57,10 +57,14 @@
  </tr>
  <tr>
   <td class="sun">
-   5
+   <a href="20240505.py">
+    5
+   </a>
   </td>
   <td class="mon">
-   6
+   <a href="20240506.py">
+    6
+   </a>
   </td>
   <td class="tue">
    7
