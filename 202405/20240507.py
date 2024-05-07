688ea5ad09c1bd4c55c93e38d2be408504b3b37d
Joseph Chiocchi <input.output@gmail.com>
2024-05-06 19:37:19 -0500

    solution 20240507.py
    - 222ms (beats 81.22%)
    - 20.6mb

--- 202405/20240507.py
+++ 202405/20240507.py
@@ -9,8 +9,31 @@ class Solution:
 
     Return *the* `head` *of the linked list after **doubling** it*.
 
+    Definition for singly-linked list::
+        class ListNode:
+            def __init__(self, val=0, next=None):
+                self.val = val
+                self.next = next
     """
 
-    def double_it(self, head: Optional[ListNode]) -> Optional[ListNode]: ...
+    def double_it(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        def get_carry(node: Optional[ListNode]) -> Optional[ListNode]:
+            """A helper function to calculate carry and update node values."""
+            # Base case: node is None, return 0
+            if node is None:
+                return 0
+            
+            # Recursively get carry from the next node
+            val = node.val * 2 + get_carry(node.next)
+            node.val = val % 10  # Update current node value
+            return val // 10  # Return carry
+
+        # Check if there is a carry from the most significant digit
+        if get_carry(head) == 1:
+            # If there is a carry, create a new node for the carry
+            return ListNode(1, head)
+        
+        # Return the modified head of the list
+        return head
 
     doubleIt = double_it
