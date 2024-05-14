480e84825a788209138defe5378872246fd9ed09
Joseph Chiocchi <input.output@gmail.com>
2024-05-13 21:00:31 -0500

    solution 20240514.py
    - 3296ms
    - 16.50mb (beats 71.32%)

--- 202405/20240514.py
+++ 202405/20240514.py
@@ -22,6 +22,28 @@ class Solution:
 
     """
 
-    def get_maximum_gold(self, grid: list[list[int]]) -> int: ...
+    def get_maximum_gold(self, grid: list[list[int]]) -> int:
+        def dfs(i, j):
+            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] == 0:
+                return 0
+
+            temp = grid[i][j]
+            # Mark as visited
+            grid[i][j] = 0
+            max_gold = 0
+
+            for di, dj in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                max_gold = max(max_gold, temp + dfs(i + di, j + dj))
+
+            # Revert back after exploration
+            grid[i][j] = temp
+            return max_gold
+
+        max_gold = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] != 0:
+                    max_gold = max(max_gold, dfs(i, j))
+        return max_gold
 
     getMaximumGold = get_maximum_gold
