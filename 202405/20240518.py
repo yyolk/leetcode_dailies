f582f30fc88b62e3cb8f93fb9aeeb7c9f5e25dd6
Joseph Chiocchi <input.output@gmail.com>
2024-05-17 20:36:07 -0400

    solution 20240518.py
    - 32ms (beats 90.18%)
    - 16.54mb

--- 202405/20240518.py
+++ 202405/20240518.py
@@ -13,8 +13,33 @@ class Solution:
     Return *the **minimum** number of moves required to make every node have **exactly**
     one coin*.
 
+    Definition for a binary tree node::
+        class TreeNode:
+            def __init__(self, val=0, left=None, right=None):
+                self.val = val
+                self.left = left
+                self.right = right
     """
 
-    def distribute_coins(self, root: Optional[TreeNode]) -> int: ...
+    def distribute_coins(self, root: Optional[TreeNode]) -> int:
+        self.moves = 0
+        
+        def dfs(node):
+            if not node:
+                return 0
+            
+            # Process left and right subtrees
+            left_excess = dfs(node.left)
+            right_excess = dfs(node.right)
+            
+            # Total moves is the absolute value of excess coins moved in both subtrees
+            self.moves += abs(left_excess) + abs(right_excess)
+            
+            # Return the number of excess coins in this subtree
+            # node.val - 1 is the excess/deficit of coins at the current node
+            return node.val + left_excess + right_excess - 1
+        
+        dfs(root)
+        return self.moves
 
     distributeCoins = distribute_coins
