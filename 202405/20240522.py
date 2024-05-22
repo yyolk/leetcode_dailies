ed66e51ca077ca7678959e2ec938adbd242f52ed
Joseph Chiocchi <input.output@gmail.com>
2024-05-21 20:22:01 -0400

    solution 20240522.py
    - 444ms (beats 95.32%)
    - 35.29mb

--- 202405/20240522.py
+++ 202405/20240522.py
@@ -9,6 +9,28 @@ class Solution:
 
     """
 
-    def partition(self, s: str) -> list[list[str]]: ...
-
-    partition = partition
+    def partition(self, s: str) -> list[list[str]]:
+        # Helper function to check if a substring is a palindrome
+        def is_palindrome(sub: str) -> bool:
+            return sub == sub[::-1]
+        
+        # Backtracking function to find all partitions
+        def backtrack(start: int, path: list):
+            # If we have reached the end of the string, add the current path to results
+            if start == len(s):
+                result.append(path[:])
+                return
+            
+            # Explore all possible partitions starting from 'start'
+            for end in range(start + 1, len(s) + 1):
+                # Get the substring
+                substring = s[start:end]
+                # If the substring is a palindrome, recurse with the new start position
+                if is_palindrome(substring):
+                    path.append(substring)
+                    backtrack(end, path)
+                    path.pop()
+        
+        result = []
+        backtrack(0, [])
+        return result
