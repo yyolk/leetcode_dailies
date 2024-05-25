479b86e7f15b1b86e66057ddb65fe2c6e6f88083
Joseph Chiocchi <joe@yolk.cc>
2024-05-25 00:18:11 +0000

    Boilerplate for 20240525
    https://leetcode.com/problems/word-break-ii/

--- 202405/20240524.py
+++ 202405/20240524.py
@@ -23,7 +23,7 @@ class Solution:
         def count_letters(word):
             count = [0] * 26
             for char in word:
-                count[ord(char) - ord('a')] += 1
+                count[ord(char) - ord("a")] += 1
             return count
 
         # Function to calculate the score of a subset of words
@@ -31,35 +31,37 @@ class Solution:
             total_score = 0
             for word in words:
                 for char in word:
-                    total_score += score[ord(char) - ord('a')]
+                    total_score += score[ord(char) - ord("a")]
             return total_score
-        
+
         # Backtracking function to find the maximum score
         def backtrack(index, current_words, current_count):
             if index == len(words):
                 return calculate_score(current_words)
-            
+
             # Skip the current word
             max_score = backtrack(index + 1, current_words, current_count)
-            
+
             # Include the current word if possible
             word_count = count_letters(words[index])
             if all(current_count[i] >= word_count[i] for i in range(26)):
                 for i in range(26):
                     current_count[i] -= word_count[i]
                 current_words.append(words[index])
-                max_score = max(max_score, backtrack(index + 1, current_words, current_count))
+                max_score = max(
+                    max_score, backtrack(index + 1, current_words, current_count)
+                )
                 current_words.pop()
                 for i in range(26):
                     current_count[i] += word_count[i]
-            
+
             return max_score
-        
+
         # Initial count of available letters
         letter_count = [0] * 26
         for char in letters:
-            letter_count[ord(char) - ord('a')] += 1
-        
+            letter_count[ord(char) - ord("a")] += 1
+
         return backtrack(0, [], letter_count)
 
     maxScoreWords = max_score_words
---
+++ 202405/20240525.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/word-break-ii/
+
+
+class Solution:
+    """140. Word Break II
+
+    Given a string `s` and a dictionary of strings `word_dict`, add spaces in `s` to
+    construct a sentence where each word is a valid dictionary word. Return all such
+    possible sentences in **any order**.
+
+    **Note** that the same word in the dictionary may be reused multiple times in the
+    segmentation.
+
+    """
+
+    def word_break(self, s: str, word_dict: list[str]) -> list[str]: ...
+
+    wordBreak = word_break
