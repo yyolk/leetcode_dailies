73e426982f1d168bd8815f9eff535f88933c735c
Joseph Chiocchi <input.output@gmail.com>
2024-05-25 20:35:57 -0400

    solution 20240526.py
    - 4409ms
    - 64.27mb

--- 202405/20240526.py
+++ 202405/20240526.py
@@ -1,4 +1,5 @@
 # https://leetcode.com/problems/student-attendance-record-ii/
+MOD = 10**9 + 7
 
 
 class Solution:
@@ -27,6 +28,31 @@ class Solution:
 
     """
 
-    def check_record(self, n: int) -> int: ...
+    def check_record(self, n: int) -> int:
+        # Initialize DP array
+        dp = [[[0] * 3 for _ in range(2)] for _ in range(n + 1)]
+        dp[0][0][0] = 1  # Base case: an empty sequence
+        
+        for i in range(1, n + 1):
+            for j in range(2):
+                for k in range(3):
+                    # Add 'P' to the sequence
+                    dp[i][j][0] = (dp[i][j][0] + dp[i-1][j][k]) % MOD
+                    
+                    # Add 'A' to the sequence
+                    if j > 0:
+                        dp[i][j][0] = (dp[i][j][0] + dp[i-1][j-1][k]) % MOD
+                    
+                    # Add 'L' to the sequence
+                    if k > 0:
+                        dp[i][j][k] = (dp[i][j][k] + dp[i-1][j][k-1]) % MOD
+        
+        # Sum up all valid sequences of length n
+        result = 0
+        for j in range(2):
+            for k in range(3):
+                result = (result + dp[n][j][k]) % MOD
+        
+        return result
 
     checkRecord = check_record
