0c77bbc594ae49285ec6e40dc852fc5e16f26318
Joseph Chiocchi <joe@yolk.cc>
2024-05-27 00:19:39 +0000

    Boilerplate for 20240527
    https://leetcode.com/problems/special-array-with-x-elements-greater-than-or-equal-x/

--- 202405/20240526.py
+++ 202405/20240526.py
@@ -32,27 +32,27 @@ class Solution:
         # Initialize DP array
         dp = [[[0] * 3 for _ in range(2)] for _ in range(n + 1)]
         dp[0][0][0] = 1  # Base case: an empty sequence
-        
+
         for i in range(1, n + 1):
             for j in range(2):
                 for k in range(3):
                     # Add 'P' to the sequence
-                    dp[i][j][0] = (dp[i][j][0] + dp[i-1][j][k]) % MOD
-                    
+                    dp[i][j][0] = (dp[i][j][0] + dp[i - 1][j][k]) % MOD
+
                     # Add 'A' to the sequence
                     if j > 0:
-                        dp[i][j][0] = (dp[i][j][0] + dp[i-1][j-1][k]) % MOD
-                    
+                        dp[i][j][0] = (dp[i][j][0] + dp[i - 1][j - 1][k]) % MOD
+
                     # Add 'L' to the sequence
                     if k > 0:
-                        dp[i][j][k] = (dp[i][j][k] + dp[i-1][j][k-1]) % MOD
-        
+                        dp[i][j][k] = (dp[i][j][k] + dp[i - 1][j][k - 1]) % MOD
+
         # Sum up all valid sequences of length n
         result = 0
         for j in range(2):
             for k in range(3):
                 result = (result + dp[n][j][k]) % MOD
-        
+
         return result
 
     checkRecord = check_record
