3d3e06e6ee1000d2e70e482d278070b7fd2d36bc
Joseph Chiocchi <input.output@gmail.com>
2024-05-28 19:42:24 -0500

    solution 20240529.py
    - 30ms (beats 89.77%)
    - 16.58mb (beats 62.56%)

--- 202405/20240529.py
+++ 202405/20240529.py
@@ -15,6 +15,22 @@ class Solution:
 
     """
 
-    def num_steps(self, s: str) -> int: ...
+    def num_steps(self, s: str) -> int:
+        # Convert the binary string to an integer
+        num = int(s, 2)
+        steps = 0
+
+        # Continue until the number is reduced to 1
+        while num > 1:
+            if num % 2 == 0:
+                # If the number is even, divide by 2
+                num //= 2
+            else:
+                # If the number is odd, add 1
+                num += 1
+            # Increment the step counter
+            steps += 1
+
+        return steps
 
     numSteps = num_steps
