b87f8932a4e9231edb51ffc8b5f20695d537c221
Joseph Chiocchi <input.output@gmail.com>
2024-05-29 19:51:43 -0500

    solution 20230802.py
    - 34ms (beats 91.34%)
    - 16.67mb (beats 75.13%)

---
+++ 202308/20230802.py
@@ -0,0 +1,32 @@
+# https://leetcode.com/problems/permutations/
+# TODO: Submit with timetravel ticket
+
+
+class Solution:
+    """46. Permutations
+
+    Given an array `nums` of distinct integers, return *all the possible permutations*.
+    You can return the answer in **any order**.
+
+    """
+
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        # Helper function for backtracking
+        def backtrack(first):
+            # If all elements are used, add the permutation to the output
+            if first == n:
+                output.append(nums[:])
+            # Generate permutations recursively
+            for i in range(first, n):
+                # Swap elements to generate different permutations
+                nums[first], nums[i] = nums[i], nums[first]
+                # Recursively call backtrack for the next index
+                backtrack(first + 1)
+                # Undo the swap for backtracking
+                nums[first], nums[i] = nums[i], nums[first]
+
+        n = len(nums)
+        output = []
+        # Start backtracking from index 0
+        backtrack(0)
+        return output
