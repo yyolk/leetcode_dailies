8987a18f8d9ee0e4ffc83eda116e8834b873ef87
Joseph Chiocchi <input.output@gmail.com>
2024-05-30 20:36:23 -0500

    solution 20240531.py
    - 51ms (beats 89.89%)
    - 18.28mb (beats 96.72%)

--- 202405/20240531.py
+++ 202405/20240531.py
@@ -13,6 +13,26 @@ class Solution:
 
     """
 
-    def single_number(self, nums: list[int]) -> list[int]: ...
+    def single_number(self, nums: list[int]) -> list[int]:
+        # Step 1: XOR all the numbers to get the XOR of the two unique numbers
+        xor_result = 0
+        for num in nums:
+            xor_result ^= num
+        
+        # Step 2: Find a set bit (rightmost set bit) in xor_result
+        # This bit will be different in the two unique numbers
+        diff_bit = xor_result & -xor_result
+        
+        # Step 3: Divide the numbers into two groups based on the diff_bit
+        # and XOR the numbers in each group to find the unique numbers
+        num1, num2 = 0, 0
+        for num in nums:
+            if num & diff_bit:
+                num1 ^= num
+            else:
+                num2 ^= num
+        
+        # Step 4: Return the two unique numbers
+        return [num1, num2]
 
     singleNumber = single_number
