c0b563c691eb3eb4a3256f138c70602a8da38685
Joseph Chiocchi <input.output@gmail.com>
2024-06-04 21:21:22 -0500

    solution 20240605.py
    - 47ms (beats 70.15%)
    - 16.54mb (beats 93.72%)

--- 202406/20240605.py
+++ 202406/20240605.py
@@ -1,4 +1,5 @@
 # https://leetcode.com/problems/find-common-characters/
+from collections import Counter
 
 
 class Solution:
@@ -10,6 +11,24 @@ class Solution:
 
     """
 
-    def common_chars(self, words: list[str]) -> list[str]: ...
+    def common_chars(self, words: list[str]) -> list[str]:
+        # Initialize the counter with the first word of the stack
+        common_count = Counter(words.pop())
+        
+        # Intersect the counter with all the words that are left
+        for word in words:
+            word_count = Counter(word)
+            for char in common_count:
+                if char in word_count:
+                    common_count[char] = min(common_count[char], word_count[char])
+                else:
+                    common_count[char] = 0
+        
+        # Create the result list based on the common_count
+        result = []
+        for char, count in common_count.items():
+            result.extend([char] * count)
+        
+        return result
 
     commonChars = common_chars
