b29c113ca4dd5bb8b162c223661866395980691e
Joseph Chiocchi <input.output@gmail.com>
2024-06-05 20:23:50 -0500

    solution 20240606.py
    - 159ms (beats 71.12%)
    - 18.27mb (beats 74.24%)

--- 202406/20240606.py
+++ 202406/20240606.py
@@ -1,4 +1,6 @@
 # https://leetcode.com/problems/hand-of-straights/
+from collections import Counter
+import heapq
 
 
 class Solution:
@@ -14,6 +16,32 @@ class Solution:
 
     """
 
-    def is_n_straight_hand(self, hand: list[int], group_size: int) -> bool: ...
+    def is_n_straight_hand(self, hand: list[int], group_size: int) -> bool:
+        # If the total number of cards is not divisible by the group size, it's impossible to form groups
+        if len(hand) % group_size != 0:
+            return False
+        
+        # Count the frequency of each card
+        count = Counter(hand)
+        
+        # Create a min-heap from the unique card values
+        min_heap = list(count.keys())
+        heapq.heapify(min_heap)
+        
+        while min_heap:
+            # Get the smallest card value
+            first = min_heap[0]
+            
+            # Try to form a group starting from the smallest card value
+            for i in range(first, first + group_size):
+                if i not in count:
+                    return False
+                count[i] -= 1
+                if count[i] == 0:
+                    if i != min_heap[0]:
+                        return False
+                    heapq.heappop(min_heap)
+        
+        return True
 
     isNStraightHand = is_n_straight_hand
