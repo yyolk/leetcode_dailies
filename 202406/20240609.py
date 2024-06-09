5fa6696c71a9cb40fa28a1aa0b81d6d9ae4126a0
Joseph Chiocchi <input.output@gmail.com>
2024-06-08 19:42:01 -0500

    solution 20240609.py
    - 224ms (beats 78.34%)
    - 21.34mb (beats 87.31%)

--- 202406/20240609.py
+++ 202406/20240609.py
@@ -11,6 +11,32 @@ class Solution:
 
     """
 
-    def subarrays_div_by_k(self, nums: list[int], k: int) -> int: ...
+    def subarrays_div_by_k(self, nums: list[int], k: int) -> int:
+        # Dictionary to store the count of remainders
+        remainder_count = {0: 1}
+        current_sum = 0
+        count = 0
+
+        for num in nums:
+            # Add current number to the cumulative sum
+            current_sum += num
+            # Calculate remainder of the current sum divided by k
+            remainder = current_sum % k
+            # Adjust remainder to be positive
+            if remainder < 0:
+                remainder += k
+
+            # If remainder is already in the dictionary, it means there are
+            # some subarrays which summed up to a multiple of k
+            if remainder in remainder_count:
+                count += remainder_count[remainder]
+
+            # Update the count of this remainder in the dictionary
+            if remainder in remainder_count:
+                remainder_count[remainder] += 1
+            else:
+                remainder_count[remainder] = 1
+
+        return count
 
     subarraysDivByK = subarrays_div_by_k
