e1234d0405b84c0255cb676c58e1fb625623bf02
Joseph Chiocchi <joe@yolk.cc>
2024-06-16 00:21:35 +0000

    Boilerplate for 20240616
    https://leetcode.com/problems/patching-array/

---
+++ 202406/20240616.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/patching-array/
+
+
+class Solution:
+    """330. Patching Array
+
+    Given a sorted integer array `nums` and an integer `n`, add/patch elements to the
+    array such that any number in the range `[1, n]` inclusive can be formed by the sum
+    of some elements in the array.
+
+    Return *the minimum number of patches required*.
+
+    """
+
+    def min_patches(self, nums: list[int], n: int) -> int: ...
+
+    minPatches = min_patches
