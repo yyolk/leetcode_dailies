f44e5b49d09d9bd6d1ec05961808f57ccb5f04a9
Joseph Chiocchi <input.output@gmail.com>
2024-06-15 19:47:37 -0500

    solution 20240616.py
    - 55ms (beats 71.33%)
    - 16.72mb

--- 202406/20240616.py
+++ 202406/20240616.py
@@ -12,6 +12,23 @@ class Solution:
 
     """
 
-    def min_patches(self, nums: list[int], n: int) -> int: ...
+    def min_patches(self, nums: list[int], n: int) -> int:
+        patches = 0
+        i = 0
+        current_sum = 1
+        
+        # Loop until we can form all numbers in the range [1, n]
+        while current_sum <= n:
+            if i < len(nums) and nums[i] <= current_sum:
+                # If the current number in nums can extend the range
+                current_sum += nums[i]
+                i += 1
+            else:
+                # If the current number in nums cannot extend the range
+                # Patch the array by adding current_sum
+                current_sum += current_sum
+                patches += 1
+        
+        return patches
 
     minPatches = min_patches
