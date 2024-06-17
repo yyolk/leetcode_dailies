11ff880365862336e42d84eb0bbe330db7a5e544
Joseph Chiocchi <input.output@gmail.com>
2024-06-17 02:37:32 -0500

    add pushover notification

---
+++ notify_pushover/__main__.py
@@ -0,0 +1,20 @@
+import os
+
+import requests
+
+
+PUSHOVER_TOKEN = os.environ["PUSHOVER_TOKEN"]
+PUSHOVER_USER = os.environ["PUSHOVER_USER"]
+
+payload = dict(
+    token = PUSHOVER_TOKEN,
+    user = PUSHOVER_USER,
+    message = "Workflow finished.",
+    ttl = 3600,
+)
+headers = {
+    "Content-Type": "application/x-www-form-urlencoded"
+}
+
+response = requests.post("https://api.pushover.net/1/messages.json", headers=headers, data=payload)
+response.raise_for_status()
