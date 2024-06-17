cd4b15d2df0595bd226e029c077b7655bd38b273
Joseph Chiocchi <input.output@gmail.com>
2024-06-16 19:23:44 -0500

    solution 20240617.py
    - 70ms (beats 98.08%)
    - 16.48mb (beats 79.87%)

--- 202406/20240617.py
+++ 202406/20240617.py
@@ -9,6 +9,26 @@ class Solution:
 
     """
 
-    def judge_square_sum(self, c: int) -> bool: ...
+    def judge_square_sum(self, c: int) -> bool:
+        # Initialize two pointers, one starting from 0 and the other from the square root of c
+        a = 0
+        b = int(c**0.5)
+        
+        # Use a while loop to check the sum of squares of the two pointers
+        while a <= b:
+            current_sum = a * a + b * b
+            
+            # If the current sum equals c, we have found such integers a and b
+            if current_sum == c:
+                return True
+            # If the current sum is less than c, increment the lower pointer
+            elif current_sum < c:
+                a += 1
+            # If the current sum is greater than c, decrement the upper pointer
+            else:
+                b -= 1
+        
+        # If no such pair is found, return False
+        return False
 
     judgeSquareSum = judge_square_sum
