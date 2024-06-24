1b673c6c516d735917deb95ecbaa42131d71d9ef
Joseph Chiocchi <input.output@gmail.com>
2024-06-23 19:24:34 -0500

    solution 20240624.py
    - 770ms (beats 97.18%)
    - 19.48mb (beats 92.96%)

--- 202406/20240624.py
+++ 202406/20240624.py
@@ -17,6 +17,29 @@ class Solution:
 
     """
 
-    def min_k_bit_flips(self, nums: list[int], k: int) -> int: ...
+    def min_k_bit_flips(self, nums: list[int], k: int) -> int:
+        n = len(nums)
+        flip_count = 0
+        # To keep track of flip operations
+        flips = [0] * n
+        # Current flip state
+        flip = 0
+
+        for i in range(n):
+            # Update current flip state based on previous flips
+            if i >= k:
+                flip ^= flips[i - k]
+
+            # If nums[i] is 0 and the current flip state is 0, we need to flip
+            if nums[i] == flip:
+                # If flipping is not possible
+                if i + k > n:
+                    return -1
+                # Mark the flip
+                flips[i] = 1
+                flip ^= 1
+                flip_count += 1
+
+        return flip_count
 
     minKBitFlips = min_k_bit_flips
