bacd12d19f98c15e52ff060fe92f09af7a9bf0bd
Joseph Chiocchi <joe@yolk.cc>
2024-06-24 00:20:57 +0000

    Boilerplate for 20240624
    https://leetcode.com/problems/minimum-number-of-k-consecutive-bit-flips/

--- 202406/20240623.py
+++ 202406/20240623.py
@@ -16,18 +16,18 @@ class Solution:
         min_deque = deque()  # Will store elements in increasing order
         left = 0
         max_length = 0
-        
+
         for right in range(len(nums)):
             # Maintain the decreasing order in max_deque
             while max_deque and nums[right] > max_deque[-1]:
                 max_deque.pop()
             max_deque.append(nums[right])
-            
+
             # Maintain the increasing order in min_deque
             while min_deque and nums[right] < min_deque[-1]:
                 min_deque.pop()
             min_deque.append(nums[right])
-            
+
             # If the difference between the max and min in the current window exceeds the limit
             while max_deque[0] - min_deque[0] > limit:
                 # Adjust the left pointer of the window
@@ -36,10 +36,10 @@ class Solution:
                 if nums[left] == min_deque[0]:
                     min_deque.popleft()
                 left += 1
-            
+
             # Update the maximum length of the valid window
             max_length = max(max_length, right - left + 1)
-        
+
         return max_length
 
     longestSubarray = longest_subarray
