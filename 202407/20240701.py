725054aa6071eb8e08795d6d22ef2cfb3d4de28c
Joseph Chiocchi <input.output@gmail.com>
2024-06-30 19:25:21 -0500

    solution 20240701.py
    - 38ms (beats 96.38%)
    - 16.67mb (beats 67.76%)

--- 202407/20240701.py
+++ 202407/20240701.py
@@ -9,6 +9,12 @@ class Solution:
 
     """
 
-    def three_consecutive_odds(self, arr: list[int]) -> bool: ...
+    def three_consecutive_odds(self, arr: list[int]) -> bool:
+        # Iterate through the array
+        for i in range(len(arr) - 2):
+            # Check if the current number and the next two numbers are odd
+            if arr[i] % 2 != 0 and arr[i + 1] % 2 != 0 and arr[i + 2] % 2 != 0:
+                return True
+        return False
 
     threeConsecutiveOdds = three_consecutive_odds
