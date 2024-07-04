f7d4a93d5b6e6065b42a7486b34ecdff7e667ac3
Joseph Chiocchi <input.output@gmail.com>
2024-07-03 19:24:37 -0500

    solution 20240704.py
    - 959ms (beats 52.40%)
    - 74.81mb

--- 202407/20240704.py
+++ 202407/20240704.py
@@ -14,8 +14,42 @@ class Solution:
 
     Return *the* `head` *of the modified linked list*.
 
+    Definition for singly-linked list:
+
+        class ListNode:
+            def __init__(self, val=0, next=None):
+                self.val = val
+                self.next = next
+
     """
 
-    def merge_nodes(self, head: Optional[ListNode]) -> Optional[ListNode]: ...
+    def merge_nodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # Skip the initial zero node
+        current = head.next
+        # This will be the new head of the resulting list
+        dummy = ListNode(0)
+        new_list_tail = dummy
+        # Sum of values between zeros
+        current_sum = 0
+        
+        while current:
+            if current.val != 0:
+                # Add value to the current sum
+                current_sum += current.val
+            else:
+                if current_sum > 0:
+                    # Create a new node with the current sum
+                    new_node = ListNode(current_sum)
+                    # Attach it to the new list
+                    new_list_tail.next = new_node
+                    # Move the tail to the new node
+                    new_list_tail = new_node
+                    # Reset the sum
+                    current_sum = 0
+            # Move to the next node in the original list
+            current = current.next
+        
+        # Return the next of dummy since the first node is dummy
+        return dummy.next
 
     mergeNodes = merge_nodes
