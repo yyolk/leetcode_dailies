991cb55580044017f07c662786ba337810265862
Joseph Chiocchi <input.output@gmail.com>
2024-07-04 20:23:36 -0500

    solution 20240705.py
    - 328ms (beats 93.92%)
    - 44.54mb

--- 202407/20240705.py
+++ 202407/20240705.py
@@ -22,8 +22,44 @@ class Solution:
     **any two distinct** critical points. If there are **fewer** than two critical
     points, return* `[-1, -1]`.
 
+    Definition for singly-linked list.
+
+        class ListNode:
+            def __init__(self, val=0, next=None):
+                self.val = val
+                self.next = next
+
     """
 
-    def nodes_between_critical_points(self, head: Optional[ListNode]) -> list[int]: ...
+    def nodes_between_critical_points(self, head: Optional[ListNode]) -> list[int]:
+        if not head or not head.next or not head.next.next:
+            # If the list has fewer than 3 nodes, there can't be any critical points
+            return [-1, -1]
+
+        critical_points = []
+        prev, curr, next = head, head.next, head.next.next
+        index = 1  # Start with the second node
+
+        while next:
+            # Check if the current node is a local minima or maxima
+            if (curr.val > prev.val and curr.val > next.val) or (curr.val < prev.val and curr.val < next.val):
+                critical_points.append(index)
+            
+            # Move to the next set of nodes
+            prev, curr, next = curr, next, next.next
+            index += 1
+
+        if len(critical_points) < 2:
+            # If there are fewer than 2 critical points
+            return [-1, -1]
+
+        # Calculate minimum and maximum distances between critical points
+        min_distance = float('inf')
+        max_distance = critical_points[-1] - critical_points[0]
+
+        for i in range(1, len(critical_points)):
+            min_distance = min(min_distance, critical_points[i] - critical_points[i - 1])
+
+        return [min_distance, max_distance]
 
     nodesBetweenCriticalPoints = nodes_between_critical_points
