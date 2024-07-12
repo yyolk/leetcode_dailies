92de9b52eec51d5228adf40fa075c0eb62870659
Joseph Chiocchi <input.output@gmail.com>
2024-07-11 22:10:08 -0400

    solution 20240712.py
    - 246ms (beats 67.53%)
    - 18.25mb

--- 202407/20240712.py
+++ 202407/20240712.py
@@ -19,6 +19,25 @@ class Solution:
 
     """
 
-    def maximum_gain(self, s: str, x: int, y: int) -> int: ...
+    def maximum_gain(self, s: str, x: int, y: int) -> int:
+        def remove_substring(s, first, second, score):
+            stack = []
+            total_score = 0
+            for char in s:
+                if stack and stack[-1] == first and char == second:
+                    stack.pop()
+                    total_score += score
+                else:
+                    stack.append(char)
+            return "".join(stack), total_score
+
+        if x > y:
+            s, score1 = remove_substring(s, 'a', 'b', x)
+            _, score2 = remove_substring(s, 'b', 'a', y)
+        else:
+            s, score1 = remove_substring(s, 'b', 'a', y)
+            _, score2 = remove_substring(s, 'a', 'b', x)
+        
+        return score1 + score2
 
     maximumGain = maximum_gain
