e9aa54594c2d163622e2d7e4b73aef08c524e76e
Joseph Chiocchi <input.output@gmail.com>
2024-07-24 20:28:13 -0500

    solution 20240725.py
    - 1189ms
    - 23.84mb (beats 84.59%)

--- 202407/20240725.py
+++ 202407/20240725.py
@@ -11,6 +11,43 @@ class Solution:
 
     """
 
-    def sort_array(self, nums: list[int]) -> list[int]: ...
+    def sort_array(self, nums: list[int]) -> list[int]:
+        if len(nums) <= 1:
+            return nums
+        
+        def merge_sort(arr):
+            if len(arr) > 1:
+                mid = len(arr) // 2  # Finding the mid of the array
+                left_half = arr[:mid]  # Dividing the elements into 2 halves
+                right_half = arr[mid:]
+
+                merge_sort(left_half)  # Sorting the first half
+                merge_sort(right_half)  # Sorting the second half
+
+                i = j = k = 0
+
+                # Copy data to temp arrays left_half[] and right_half[]
+                while i < len(left_half) and j < len(right_half):
+                    if left_half[i] < right_half[j]:
+                        arr[k] = left_half[i]
+                        i += 1
+                    else:
+                        arr[k] = right_half[j]
+                        j += 1
+                    k += 1
+
+                # Checking if any element was left
+                while i < len(left_half):
+                    arr[k] = left_half[i]
+                    i += 1
+                    k += 1
+
+                while j < len(right_half):
+                    arr[k] = right_half[j]
+                    j += 1
+                    k += 1
+
+        merge_sort(nums)
+        return nums
 
     sortArray = sort_array
