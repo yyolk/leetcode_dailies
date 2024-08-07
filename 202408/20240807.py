abadc27be723dab867acc1b29b8bb3708699dc77
Joseph Chiocchi <input.output@gmail.com>
2024-08-06 19:27:42 -0500

    solution 20240807.py
    - 36ms (beats 61.63%)
    - 16.59mb (beats 75.27%)

--- 202408/20240807.py
+++ 202408/20240807.py
@@ -8,6 +8,64 @@ class Solution:
 
     """
 
-    def number_to_words(self, num: int) -> str: ...
+    def number_to_words(self, num: int) -> str:
+        if num == 0:
+            return "Zero"
+
+        below_20 = [
+            "",
+            "One",
+            "Two",
+            "Three",
+            "Four",
+            "Five",
+            "Six",
+            "Seven",
+            "Eight",
+            "Nine",
+            "Ten",
+            "Eleven",
+            "Twelve",
+            "Thirteen",
+            "Fourteen",
+            "Fifteen",
+            "Sixteen",
+            "Seventeen",
+            "Eighteen",
+            "Nineteen",
+        ]
+
+        tens = [
+            "",
+            "",
+            "Twenty",
+            "Thirty",
+            "Forty",
+            "Fifty",
+            "Sixty",
+            "Seventy",
+            "Eighty",
+            "Ninety",
+        ]
+
+        thousands = ["", "Thousand", "Million", "Billion"]
+
+        def helper(n: int) -> str:
+            if n == 0:
+                return ""
+            elif n < 20:
+                return below_20[n] + " "
+            elif n < 100:
+                return tens[n // 10] + " " + helper(n % 10)
+            else:
+                return below_20[n // 100] + " Hundred " + helper(n % 100)
+
+        res = ""
+        for i in range(len(thousands)):
+            if num % 1000 != 0:
+                res = helper(num % 1000) + thousands[i] + " " + res
+            num //= 1000
+
+        return res.strip()
 
     numberToWords = number_to_words
