828866a81723f748637a109e03889ad15087e7e6
Joseph Chiocchi <joe@yolk.cc>
2024-08-07 00:20:26 +0000

    Boilerplate for 20240807
    https://leetcode.com/problems/integer-to-english-words/

--- 202408/20240806.py
+++ 202408/20240806.py
@@ -30,17 +30,17 @@ class Solution:
     def minimum_pushes(self, word: str) -> int:
         # Count the frequency of each character in the word
         char_freq = Counter(word)
-        
+
         # Sort characters by frequency in descending order
         sorted_chars = sorted(char_freq.items(), key=lambda x: x[1], reverse=True)
-        
+
         total_pushes = 0
         for i, (char, freq) in enumerate(sorted_chars):
             # Calculate the number of pushes needed for this character
             # The first 8 characters need 1 push, the next 8 need 2 pushes, and so on
             pushes = (i // 8) + 1
             total_pushes += pushes * freq
-        
+
         return total_pushes
 
     minimumPushes = minimum_pushes
---
+++ 202408/20240807.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/integer-to-english-words/
+
+
+class Solution:
+    """273. Integer to English Words
+
+    Convert a non\\-negative integer `num` to its English words representation.
+
+    """
+
+    def number_to_words(self, num: int) -> str: ...
+
+    numberToWords = number_to_words
