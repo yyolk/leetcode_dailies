ffa200028a52d260052db01bf50a8468c8994142
Joseph Chiocchi <joe@yolk.cc>
2024-08-16 06:00:20 +0000

    Boilerplate for 20240816
    https://leetcode.com/problems/maximum-distance-in-arrays/

---
+++ 202408/20240816.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/maximum-distance-in-arrays/
+
+
+class Solution:
+    """624. Maximum Distance in Arrays
+
+    You are given `m` `arrays`, where each array is sorted in **ascending order**.
+
+    You can pick up two integers from two different arrays (each array picks one) and
+    calculate the distance. We define the distance between two integers `a` and `b` to
+    be their absolute difference `|a - b|`.
+
+    Return *the maximum distance*.
+
+    """
+
+    def max_distance(self, arrays: list[list[int]]) -> int: ...
+
+    maxDistance = max_distance
