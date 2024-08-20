313bf9a10a046a1a43e85139609cc65b8b631a30
Joseph Chiocchi <input.output@gmail.com>
2024-08-19 19:28:23 -0500

    solution 20240820.py
    - 1482ms
    - 17.11mb (beats 79.59%)

--- 202408/20240820.py
+++ 202408/20240820.py
@@ -20,6 +20,26 @@ class Solution:
 
     """
 
-    def stone_game_i_i(self, piles: list[int]) -> int: ...
+    def stone_game_i_i(self, piles: list[int]) -> int:
+        n = len(piles)
+        # dp[i][M] will store the maximum number of stones Alice can get starting from index i with M as the current value
+        dp = [[0] * (n + 1) for _ in range(n + 1)]
+        # suffix_sum[i] will store the total number of stones from index i to the end
+        suffix_sum = [0] * (n + 1)
+
+        # Fill suffix_sum from the end to the beginning
+        for i in range(n - 1, -1, -1):
+            suffix_sum[i] = suffix_sum[i + 1] + piles[i]
+
+        # Fill the dp table
+        for i in range(n - 1, -1, -1):
+            for M in range(1, n + 1):
+                # Try taking X piles where 1 <= X <= 2 * M
+                for X in range(1, min(2 * M, n - i) + 1):
+                    # We want to maximize Alice's stones, so we subtract the optimal choice for Bob
+                    dp[i][M] = max(dp[i][M], suffix_sum[i] - dp[i + X][max(M, X)])
+
+        # The result is the maximum stones Alice can get starting from index 0 with M = 1
+        return dp[0][1]
 
     stoneGameII = stone_game_i_i
