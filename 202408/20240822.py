5f0ace8f6a5473b05ab14008508b9957649f693a
Joseph Chiocchi <input.output@gmail.com>
2024-08-21 19:40:55 -0500

    solution 20240822.py
    - 28ms (beats 90.35%)
    - 16.34mb (beats 89.59%)

--- 202408/20240822.py
+++ 202408/20240822.py
@@ -14,6 +14,14 @@ class Solution:
 
     """
 
-    def find_complement(self, num: int) -> int: ...
+    def find_complement(self, num: int) -> int:
+        # Step 1: Calculate the bit length of num
+        bit_length = num.bit_length()
+        
+        # Step 2: Create a mask with all bits set to 1 of the same length as num
+        mask = (1 << bit_length) - 1
+        
+        # Step 3: XOR num with the mask to get the complement
+        return num ^ mask
 
     findComplement = find_complement
