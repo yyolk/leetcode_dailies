69e29fc0e58df5f28ba7fb41d9256b89118a1bca
Joseph Chiocchi <input.output@gmail.com>
2024-08-25 19:26:46 -0500

    solution 20240826.py
    - 40ms (beats 86.64%)
    - 18.27mb

--- 202408/20240826.py
+++ 202408/20240826.py
@@ -1,15 +1,6 @@
 # https://leetcode.com/problems/n-ary-tree-postorder-traversal/
 
 
-"""
-# Definition for a Node.
-class Node:
-    def __init__(self, val=None, children=None):
-        self.val = val
-        self.children = children
-"""
-
-
 class Solution:
     """590. N-ary Tree Postorder Traversal
 
@@ -19,8 +10,26 @@ class Solution:
     Nary\\-Tree input serialization is represented in their level order traversal. Each
     group of children is separated by the null value (See examples)
 
+    Definition for a Node::
+        class Node:
+            def __init__(self, val=None, children=None):
+                self.val = val
+                self.children = children
     """
 
-    def postorder(self, root: "Node") -> list[int]: ...
-
-    postorder = postorder
+    def postorder(self, root: "Node") -> list[int]:
+        def traverse(node):
+            # If the current node is None, return an empty list
+            if not node:
+                return []
+            # Initialize an empty list to store the result
+            result = []
+            # Recursively traverse each child and extend the result list
+            for child in node.children:
+                result.extend(traverse(child))
+            # Append the current node's value after traversing children
+            result.append(node.val)
+            return result
+        
+        # Start the traversal from the root
+        return traverse(root)
