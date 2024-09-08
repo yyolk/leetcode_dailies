385d3c29793dc2410a9afa3f82cab5a7eb5efa75
Joseph Chiocchi <input.output@gmail.com>
2024-09-07 19:35:21 -0500

    solution 20240908.py
    - 32ms (beats 89.46%)
    - 16.91mb

--- 202409/20240908.py
+++ 202409/20240908.py
@@ -16,10 +16,61 @@ class Solution:
 
     Return *an array of the* `k` *parts*.
 
+    Definition for singly-linked list::
+
+        class ListNode:
+            def __init__(self, val=0, next=None):
+                self.val = val
+                self.next = next
     """
 
     def split_list_to_parts(
         self, head: Optional[ListNode], k: int
-    ) -> list[Optional[ListNode]]: ...
+    ) -> list[Optional[ListNode]]:
+        """Split input linked list into parts
+
+        Proposed solution using nested iteration and divmod(...)
+
+        Args:
+            head (Optional ListNode): the input, the first ListNode of a singly linked list
+            k (int): the requested number of parts to split the linked list into
+
+        Returns:
+            List of Optional ListNode: the resulting list containing the parts the
+                input was split into
+        """
+        # Calculate the length of the linked list
+        length = 0
+        current = head
+        while current:
+            length += 1
+            current = current.next
+
+        # Calculate the size of each part and the number of larger parts
+        part_size, extra_parts = divmod(length, k)
+
+        result = []
+        # Rewind our walk to calculate the length
+        current = head
+        for i in range(k):
+            # Keep a reference to the head to append to our results
+            part_head = current
+            # This part size is 1 bigger unless we've hit the threshold of our divmod remainder
+            part_size_temp = part_size + (1 if i < extra_parts else 0)
+
+            # Walk the ListNode the length of our part_size_temp, until there isn't any
+            for j in range(part_size_temp - 1):
+                if current:
+                    current = current.next
+
+            # Split the ListNode here, if not None.
+            # It will be the new part_head on next(...)
+            if current:
+                current.next, current = None, current.next
+
+            # Append this ListNode's head to the result List
+            result.append(part_head)
+
+        return result
 
     splitListToParts = split_list_to_parts
