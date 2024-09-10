c8b48cb57b4c786d2f26092ced1fba48eef019f9
Joseph Chiocchi <input.output@gmail.com>
2024-09-09 19:36:34 -0500

    solution 20240910.py
    - 60ms (beats 92.34%)
    - 19.53 (beats 79.95%)

--- 202409/20240910.py
+++ 202409/20240910.py
@@ -1,4 +1,5 @@
 # https://leetcode.com/problems/insert-greatest-common-divisors-in-linked-list/
+from math import gcd
 
 
 class Solution:
@@ -15,10 +16,42 @@ class Solution:
     The **greatest common divisor** of two numbers is the largest positive integer that
     evenly divides both numbers.
 
+    Definition for singly-linked list::
+
+        class ListNode:
+            def __init__(self, val=0, next=None):
+                self.val = val
+                self.next = next
+
     """
 
     def insert_greatest_common_divisors(
         self, head: Optional[ListNode]
-    ) -> Optional[ListNode]: ...
+    ) -> Optional[ListNode]:
+        # If the list is empty or has only one node, no insertion is needed
+        if not head or not head.next:
+            return head
+        
+        # Start with the head of the list
+        current = head
+        
+        while current and current.next:
+            # Find the next node
+            next_node = current.next
+            
+            # Calculate GCD of current node and next node
+            gcd_value = gcd(current.val, next_node.val)
+            
+            # Create a new node with the GCD value
+            gcd_node = ListNode(gcd_value)
+            
+            # Insert the new GCD node between current and next_node
+            current.next = gcd_node
+            gcd_node.next = next_node
+            
+            # Move to the original next node to continue the process
+            current = next_node
+        
+        return head
 
     insertGreatestCommonDivisors = insert_greatest_common_divisors
