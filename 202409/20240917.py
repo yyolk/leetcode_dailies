a2420a849f0c5d85e56954f3cd1acc151d7bc122
Joseph Chiocchi <input.output@gmail.com>
2024-09-16 19:47:02 -0500

    solution 20240917.py
    - 35ms (beats 59.61%)
    - 16.35mb (beats 96.74%)

--- 202409/20240917.py
+++ 202409/20240917.py
@@ -1,4 +1,5 @@
 # https://leetcode.com/problems/uncommon-words-from-two-sentences/
+from collections import Counter
 
 
 class Solution:
@@ -15,6 +16,16 @@ class Solution:
 
     """
 
-    def uncommon_from_sentences(self, s1: str, s2: str) -> list[str]: ...
+    def uncommon_from_sentences(self, s1: str, s2: str) -> list[str]:
+        # Combine both sentences and split into words
+        words = (s1 + ' ' + s2).split()
+        
+        # Count occurrences of each word
+        word_count = Counter(words)
+        
+        # Filter words that appear exactly once
+        uncommon_words = [word for word, count in word_count.items() if count == 1]
+        
+        return uncommon_words
 
     uncommonFromSentences = uncommon_from_sentences
