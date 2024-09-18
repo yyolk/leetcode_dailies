e6ab0e7692c5743f13f6e2ab7c0221f09c22c98d
Joseph Chiocchi <input.output@gmail.com>
2024-09-17 20:33:52 -0500

    solution 20240918.py
    - 33ms (beats 92.96%)
    - 16.52mb

--- 202409/20240918.py
+++ 202409/20240918.py
@@ -1,4 +1,5 @@
 # https://leetcode.com/problems/largest-number/
+import functools
 
 
 class Solution:
@@ -12,6 +13,28 @@ class Solution:
 
     """
 
-    def largest_number(self, nums: list[int]) -> str: ...
+    def largest_number(self, nums: list[int]) -> str:
+        # Convert all numbers to strings for easier comparison and concatenation
+        nums = [str(num) for num in nums]
+
+        # Define a key function for sorting
+        def compare(a, b):
+            # If ab > ba, then a should come before b in our sorting
+            if a + b > b + a:
+                return -1
+            elif a + b < b + a:
+                return 1
+            else:
+                return 0
+
+        # Sort nums using the compare function
+        nums.sort(key=functools.cmp_to_key(compare))
+
+        # After sorting, if the first number is "0", then all numbers were "0"
+        if nums[0] == "0":
+            return "0"
+
+        # Join all numbers to form the largest number
+        return "".join(nums)
 
     largestNumber = largest_number
