2f5a0f406871eff9c2024dad53b715d7861b9c6d
Joseph Chiocchi <input.output@gmail.com>
2024-09-18 20:03:32 -0500

    solution 20240919.py
    - 32ms (beats 86.74%)
    - 16.53mb (beats 90.68%)

--- 202409/20240919.py
+++ 202409/20240919.py
@@ -13,6 +13,31 @@ class Solution:
 
     """
 
-    def diff_ways_to_compute(self, expression: str) -> list[int]: ...
+    def diff_ways_to_compute(self, expression: str) -> list[int]:
+        def compute(left, right, op):
+            if op == "+":
+                return left + right
+            elif op == "-":
+                return left - right
+            elif op == "*":
+                return left * right
+        
+        # If the expression is just a number, return it as a single-element list
+        if expression.isdigit():
+            return [int(expression)]
+        
+        results = []
+        for i, char in enumerate(expression):
+            if char in ["+", "-", "*"]:
+                # Recursively compute results for left and right subexpressions
+                left_results = self.diff_ways_to_compute(expression[:i])
+                right_results = self.diff_ways_to_compute(expression[i+1:])
+                
+                # Combine all possible results
+                for left in left_results:
+                    for right in right_results:
+                        results.append(compute(left, right, char))
+        
+        return results
 
     diffWaysToCompute = diff_ways_to_compute
