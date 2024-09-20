464779c89a3d7b6eef5684d4e4209de70d2f4efa
Joseph Chiocchi <joe@yolk.cc>
2024-09-20 00:22:20 +0000

    Boilerplate for 20240920
    https://leetcode.com/problems/shortest-palindrome/

---
+++ 202409/20240920.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/shortest-palindrome/
+
+
+class Solution:
+    """214. Shortest Palindrome
+
+    You are given a string `s`. You can convert `s` to a palindrome by adding characters
+    in front of it.
+
+    Return *the shortest palindrome you can find by performing this transformation*.
+
+    """
+
+    def shortest_palindrome(self, s: str) -> str: ...
+
+    shortestPalindrome = shortest_palindrome
