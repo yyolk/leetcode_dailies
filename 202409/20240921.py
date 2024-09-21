985235b0bbbfbad6e2c2998fe67b7b32c2710a3d
Joseph Chiocchi <input.output@gmail.com>
2024-09-20 20:02:24 -0500

    solution 20240921.py
    - 69ms (beats 81.34%)
    - 22.51mb (beats 66.63%)

--- 202409/20240921.py
+++ 202409/20240921.py
@@ -11,6 +11,29 @@ class Solution:
 
     """
 
-    def lexical_order(self, n: int) -> list[int]: ...
+    def lexical_order(self, n: int) -> list[int]:
+        result = []
+        
+        # Current number we are checking
+        current = 1
+        
+        # Loop until we've found all numbers
+        for _ in range(n):
+            result.append(current)
+            
+            # If 10*current is within bounds, we go 'down' in lexicographical order
+            if current * 10 <= n:
+                current *= 10
+            # If current+1 is within bounds, we go 'right'
+            elif current % 10 != 9 and current + 1 <= n:
+                current += 1
+            # If we've hit a 9 or gone out of bounds, we go 'up'
+            else:
+                # Keep dividing by 10 until we can move right
+                while (current // 10) % 10 == 9:
+                    current //= 10
+                current = current // 10 + 1
+        
+        return result
 
     lexicalOrder = lexical_order
