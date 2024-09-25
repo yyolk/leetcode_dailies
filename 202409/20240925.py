c14b54d77466b57260ae69c83d31d108bb459d8b
Joseph Chiocchi <input.output@gmail.com>
2024-09-25 00:43:26 -0500

    solution 20240925.py
    - 1966ms
    - 305.35mb (beats 81.38%)

--- 202409/20240925.py
+++ 202409/20240925.py
@@ -1,6 +1,13 @@
 # https://leetcode.com/problems/sum-of-prefix-scores-of-strings/
 
 
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        # Count of words ending here or passing through this node
+        self.count = 0
+
+
 class Solution:
     """2416. Sum of Prefix Scores of Strings
 
@@ -18,7 +25,32 @@ class Solution:
     **Note** that a string is considered as a prefix of itself.
 
     """
+    def __init__(self):
+        self.root = TrieNode()
 
-    def sum_prefix_scores(self, words: list[str]) -> list[int]: ...
+    def sum_prefix_scores(self, words: list[str]) -> list[int]:
+        # Insert all words into Trie
+        for word in words:
+            node = self.root
+            for char in word:
+                if char not in node.children:
+                    node.children[char] = TrieNode()
+                node = node.children[char]
+                node.count += 1  # Increment count for each character
+
+        # Calculate prefix scores for each word
+        result = []
+        for word in words:
+            score = 0
+            node = self.root
+            for char in word:
+                if char not in node.children:
+                    break  # If char not in Trie, no need to continue for this word
+                node = node.children[char]
+                # Add the count of this prefix
+                score += node.count
+            result.append(score)
+
+        return result
 
     sumPrefixScores = sum_prefix_scores
