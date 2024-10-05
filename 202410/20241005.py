5b3e13df5da5a635dfc70e469fdeaf30c684c31e
Joseph Chiocchi <joe@yolk.cc>
2024-10-05 00:22:39 +0000

    Boilerplate for 20241005
    https://leetcode.com/problems/permutation-in-string/

---
+++ 202410/20241005.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/permutation-in-string/
+
+
+class Solution:
+    """567. Permutation in String
+
+    Given two strings `s1` and `s2`, return `true` if `s2` contains a permutation of
+    `s1`, or `false` otherwise.
+
+    In other words, return `true` if one of `s1`'s permutations is the substring of
+    `s2`.
+
+    """
+
+    def check_inclusion(self, s1: str, s2: str) -> bool: ...
+
+    checkInclusion = check_inclusion
