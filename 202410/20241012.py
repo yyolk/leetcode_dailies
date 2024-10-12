2edde08825c6b71e8f3c72ac070bbe166b0152fc
Joseph Chiocchi <input.output@gmail.com>
2024-10-11 20:26:55 -0500

    solution 20241012.py
    - 1039ms (beats 89.24%)
    - 56.41mb

--- 202410/20241012.py
+++ 202410/20241012.py
@@ -1,4 +1,5 @@
 # https://leetcode.com/problems/divide-intervals-into-minimum-number-of-groups/
+from heapq import heappush, heappop
 
 
 class Solution:
@@ -18,6 +19,22 @@ class Solution:
 
     """
 
-    def min_groups(self, intervals: list[list[int]]) -> int: ...
+    def min_groups(self, intervals: list[list[int]]) -> int:
+        # Sort intervals by their start times
+        intervals.sort(key=lambda x: x[0])
+        
+        # Use a min heap to keep track of the end times of the groups
+        heap = []
+        for start, end in intervals:
+            if heap and heap[0] < start:
+                # If the smallest end time in the heap is before the start of the current interval,
+                # we can use the same group, so remove that end time from heap
+                heappop(heap)
+            
+            # Add the current interval's end time to the heap
+            heappush(heap, end)
+        
+        # The size of the heap at the end represents the number of groups needed
+        return len(heap)
 
     minGroups = min_groups
