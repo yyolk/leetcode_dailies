cd4de8a9c0ca7b967d726ac928936ba9749898a4
Joseph Chiocchi <joe@yolk.cc>
2024-10-14 00:24:24 +0000

    Boilerplate for 20241014
    https://leetcode.com/problems/maximal-score-after-applying-k-operations/

--- 202410/20241013.py
+++ 202410/20241013.py
@@ -17,32 +17,32 @@ class Solution:
         # Heap to store {value, list_index, value_index}
         pq = [(row[0], i, 0) for i, row in enumerate(nums)]
         heapq.heapify(pq)
-        
+
         # To keep track of the maximum value in the current range
         max_val = max(row[0] for row in nums)
-        
-        # Initial result 
+
+        # Initial result
         result = [pq[0][0], max_val]
-        
+
         while pq:
             # Pop the smallest element
             min_val, list_index, value_index = heapq.heappop(pq)
-            
+
             # Check if this range is smaller than our current best
             if max_val - min_val < result[1] - result[0]:
                 result = [min_val, max_val]
-            
+
             # If we have exhausted a list, we cannot extend our range anymore
             if value_index + 1 == len(nums[list_index]):
                 return result
-            
+
             # Push the next number from the same list into the heap
             next_val = nums[list_index][value_index + 1]
             heapq.heappush(pq, (next_val, list_index, value_index + 1))
             # Update max_val if necessary
             if next_val > max_val:
                 max_val = next_val
-        
+
         # This line should not be reached if there's a valid solution
         return result
 
--- 202410/README.md
+++ 202410/README.md
@@ -102,7 +102,9 @@
    </a>
   </td>
   <td class="mon">
-   14
+   <a href="20241014.py">
+    14
+   </a>
   </td>
   <td class="tue">
    15
