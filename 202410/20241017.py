b8980f0c00a097a9a473b68ff9321b0363009584
Joseph Chiocchi <input.output@gmail.com>
2024-10-16 19:58:50 -0500

    solution 20241017.py
    - 30ms (beats 84.92%)
    - 16.60mb

--- 202410/20241017.py
+++ 202410/20241017.py
@@ -11,6 +11,23 @@ class Solution:
 
     """
 
-    def maximum_swap(self, num: int) -> int: ...
+    def maximum_swap(self, num: int) -> int:
+        # Convert number to list of digits for easy manipulation
+        num_list = list(str(num))
+        last_occurrence = {int(d): i for i, d in enumerate(num_list)}
+
+        # Iterate through the digits from left to right
+        for i, d in enumerate(num_list):
+            # We want to swap with the largest digit that is greater than the current digit
+            # and appears later in the number
+            for j in range(9, int(d), -1):
+                if last_occurrence.get(j, -1) > i:
+                    # Perform the swap
+                    num_list[i], num_list[last_occurrence[j]] = num_list[last_occurrence[j]], num_list[i]
+                    # Convert back to integer and return since we can only swap once
+                    return int("".join(num_list))
+    
+        # If no swap occurred, return the original number
+        return num
 
     maximumSwap = maximum_swap
