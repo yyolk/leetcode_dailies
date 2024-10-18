979ba3fb17a9ff59319f733d15ebe806ca18ad46
Joseph Chiocchi <input.output@gmail.com>
2024-10-17 19:36:35 -0500

    solution 20241018.py
    - 62ms (beats 86.10%)
    - 16.58mb (beats 66.10%)

--- 202410/20241018.py
+++ 202410/20241018.py
@@ -13,10 +13,33 @@ class Solution:
     **different** if the indices of the elements chosen are different.
 
     The bitwise OR of an array `a` is equal to `a[0] OR a[1] OR ... OR a[a.length - 1]`
-    (**0\\-indexed**).
+    (**0-indexed**).
 
     """
 
-    def count_max_or_subsets(self, nums: list[int]) -> int: ...
+    def count_max_or_subsets(self, nums: list[int]) -> int:
+        max_or = 0
+        for num in nums:
+            max_or |= num
+
+        self.count = 0
+
+        def backtrack(index: int, current_or: int):
+            if current_or == max_or:
+                # Count all subsets from this point forward
+                self.count += 2 ** (len(nums) - index)
+                return
+
+            if index >= len(nums):
+                return
+
+            # Include the current number
+            backtrack(index + 1, current_or | nums[index])
+
+            # Exclude the current number
+            backtrack(index + 1, current_or)
+
+        backtrack(0, 0)
+        return self.count
 
     countMaxOrSubsets = count_max_or_subsets
