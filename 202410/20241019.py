8bd08ca9b587913a2c5fbbee0452620aa9a04732
Joseph Chiocchi <input.output@gmail.com>
2024-10-18 20:28:06 -0500

    solution 20241019.py
    - 407ms
    - 27.72mb

--- 202410/20241019.py
+++ 202410/20241019.py
@@ -30,6 +30,36 @@ class Solution:
 
     """
 
-    def find_kth_bit(self, n: int, k: int) -> str: ...
+    def invert(self, s: str) -> str:
+        """Inverts the binary string."""
+        return "".join("1" if bit == "0" else "0" for bit in s)
+    
+    def find_kth_bit(self, n: int, k: int) -> str:
+        """
+        Finds the kth bit in the nth binary string Sn.
+
+        Args:
+        n: The index of the binary string sequence.
+        k: The position of the bit to find (1-indexed).
+
+        Returns:
+        The kth bit in Sn, "0" or "1".
+        """
+        # Base case
+        if n == 1:
+            return "0"
+        
+        # Recursive behavior: 
+        # The kth bit of Sn can be determined from Sn-1
+        # If k is in the first half of Sn, it"s the same as in Sn-1
+        # If k is in the second half, it"s the inverse of the mirrored position in Sn-1
+        s = "0"  # Starting with S1
+        
+        for i in range(2, n + 1):
+            # Build Si from Si-1
+            s = s + "1" + self.invert(s)[::-1]
+        
+        return s[k - 1]
 
     findKthBit = find_kth_bit
+
