79428438a1a099365d46c67055e606ef58d6e973
Joseph Chiocchi <input.output@gmail.com>
2024-10-20 20:48:40 -0500

    solution 20241021.py
    - 147ms (beats 92.38%)
    - 16.65mb

--- 202410/20241021.py
+++ 202410/20241021.py
@@ -15,6 +15,31 @@ class Solution:
 
     """
 
-    def max_unique_split(self, s: str) -> int: ...
+    def max_unique_split(self, s: str) -> int:
+        def backtrack(index: int, seen: set) -> int:
+            # If we've reached the end of the string,
+            # we've successfully split the string into unique substrings
+            if index == len(s):
+                return len(seen)
+
+            max_splits = 0
+
+            # Try all possible splits from the current index
+            for i in range(index, len(s)):
+                substring = s[index:i+1]
+                if substring not in seen:
+                    # Add this substring to our set of seen substrings
+                    seen.add(substring)
+                    # Recursively try to split the rest of the string
+                    current_splits = backtrack(i + 1, seen)
+                    # Update max_splits if we found a better split
+                    max_splits = max(max_splits, current_splits)
+                    # Backtrack by removing the substring for other possibilities
+                    seen.remove(substring)
+
+            return max_splits
+
+        # Start the backtracking from index 0 with an empty set of seen substrings
+        return backtrack(0, set())
 
     maxUniqueSplit = max_unique_split
