0b390225b1baf1f124503889f0a6d2ad2966872c
Joseph Chiocchi <input.output@gmail.com>
2024-10-26 21:07:40 -0500

    solution 20241027.py
    - 59ms (beats 72.46%)
    - 19.42mb

--- 202410/20241027.py
+++ 202410/20241027.py
@@ -9,6 +9,30 @@ class Solution:
 
     """
 
-    def count_squares(self, matrix: list[list[int]]) -> int: ...
+    def count_squares(self, matrix: list[list[int]]) -> int:
+        if not matrix or not matrix[0]:
+            return 0
+        
+        m, n = len(matrix), len(matrix[0])
+        # dp[i][j] represents the side length of the largest square 
+        # whose bottom-right corner is at (i, j)
+        dp = [[0] * n for _ in range(m)]
+        count = 0
+
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j] == 1:
+                    # If it's on the edge or if it's 1, determine the size:
+                    # - If it's on the first row or column, it can only be 1
+                    # - Otherwise, it's the min of the three adjacent squares plus one
+                    if i == 0 or j == 0:
+                        dp[i][j] = 1
+                    else:
+                        dp[i][j] = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + 1
+                    
+                    # Add the size of this square to our count
+                    count += dp[i][j]
+
+        return count
 
     countSquares = count_squares
