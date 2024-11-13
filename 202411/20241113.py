dee995761a064174e16196a6ee16f7fb0bce9a46
Joseph Chiocchi <input.output@gmail.com>
2024-11-12 18:25:10 -0600

    solution 20241113.py
    - 211ms (beats 63.10%)
    - 30.53mb (beats 91.73%)

--- 202411/20241113.py
+++ 202411/20241113.py
@@ -1,4 +1,5 @@
 # https://leetcode.com/problems/count-the-number-of-fair-pairs/
+from bisect import bisect_left, bisect_right
 
 
 class Solution:
@@ -15,6 +16,22 @@ class Solution:
 
     """
 
-    def count_fair_pairs(self, nums: list[int], lower: int, upper: int) -> int: ...
+    def count_fair_pairs(self, nums: list[int], lower: int, upper: int) -> int:
+        # Sort the array for binary search
+        nums.sort()
+        n = len(nums)
+        count = 0
+        
+        # Iterate over each number
+        for i in range(n):
+            # Find the index where the sum would be at least `lower`
+            left = bisect_left(nums, lower - nums[i], i + 1)
+            # Find the index where the sum would exceed `upper`
+            right = bisect_right(nums, upper - nums[i], i + 1)
+            
+            # Add the number of valid pairs for this number
+            count += right - left
+
+        return count
 
     countFairPairs = count_fair_pairs
