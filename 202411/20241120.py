64083a3b1f067d165cda98708c1a09c68033c45e
Joseph Chiocchi <joe@yolk.cc>
2024-11-20 00:24:14 +0000

    Boilerplate for 20241120
    https://leetcode.com/problems/take-k-of-each-character-from-left-and-right/

---
+++ 202411/20241120.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/take-k-of-each-character-from-left-and-right/
+
+
+class Solution:
+    """2516. Take K of Each Character From Left and Right
+
+    You are given a string `s` consisting of the characters `'a'`, `'b'`, and `'c'` and
+    a non\\-negative integer `k`. Each minute, you may take either the **leftmost**
+    character of `s`, or the **rightmost** character of `s`.
+
+    Return *the **minimum** number of minutes needed for you to take **at least*** `k`
+    *of each character, or return* `-1` *if it is not possible to take* `k` *of each
+    character.*
+
+    """
+
+    def take_characters(self, s: str, k: int) -> int: ...
+
+    takeCharacters = take_characters
