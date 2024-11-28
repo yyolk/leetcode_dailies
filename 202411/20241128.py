445f675385bffd93b23c1f4c8559c901e6646c33
Joseph Chiocchi <input.output@gmail.com>
2024-11-28 16:57:10 -0500

    solution 20241128.py
    - 1046ms (beats 83.94%)
    - 47.06mb

--- 202411/20241128.py
+++ 202411/20241128.py
@@ -1,4 +1,5 @@
 # https://leetcode.com/problems/minimum-obstacle-removal-to-reach-corner/
+from collections import deque
 
 
 class Solution:
@@ -16,6 +17,31 @@ class Solution:
     Return *the **minimum** number of **obstacles** to **remove** so you can move from
     the upper left corner* `(0, 0)` *to the lower right corner* `(m - 1, n - 1)`."""
 
-    def minimum_obstacles(self, grid: list[list[int]]) -> int: ...
+    def minimum_obstacles(self, grid: list[list[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        # right, left, down, up
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        visited = set()
+        # (x, y, obstacles_removed)
+        queue = deque([(0, 0, 0)])
+
+        while queue:
+            x, y, obstacles = queue.popleft()
+            if x == m - 1 and y == n - 1:
+                return obstacles
+
+            for dx, dy in directions:
+                nx, ny = x + dx, y + dy
+                if 0 <= nx < m and 0 <= ny < n and (nx, ny) not in visited:
+                    visited.add((nx, ny))
+                    # obstacle
+                    if grid[nx][ny] == 1:
+                        queue.append((nx, ny, obstacles + 1))
+                    else:
+                        # no need to remove obstacle
+                        queue.appendleft((nx, ny, obstacles))
+
+        # If there's no path to the destination
+        return -1
 
     minimumObstacles = minimum_obstacles
