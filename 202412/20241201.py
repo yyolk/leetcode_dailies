68acfc7a0d17348282b1770c8d6f67bcfb4f866c
Joseph Chiocchi <joe@yolk.cc>
2024-12-01 00:30:25 +0000

    Boilerplate for 20241201
    https://leetcode.com/problems/check-if-n-and-its-double-exist/

---
+++ 202412/20241201.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/check-if-n-and-its-double-exist/
+
+
+class Solution:
+    """1346. Check If N and Its Double Exist
+
+    Given an array `arr` of integers, check if there exist two indices `i` and `j` such
+    that :
+
+    * `i != j`
+
+    * `0 <= i, j < arr.length`
+
+    * `arr[i] == 2 * arr[j]`"""
+
+    def check_if_exist(self, arr: list[int]) -> bool: ...
+
+    checkIfExist = check_if_exist
