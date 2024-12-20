d0b8ebbb523cba4d22b3758c08054cf4d8b8a6f8
Joseph Chiocchi <joe@yolk.cc>
2024-12-20 00:23:33 +0000

    Boilerplate for 20241220
    https://leetcode.com/problems/reverse-odd-levels-of-binary-tree/

---
+++ 202412/20241220.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/reverse-odd-levels-of-binary-tree/
+
+
+class Solution:
+    """2415. Reverse Odd Levels of Binary Tree
+
+    Given the `root` of a **perfect** binary tree, reverse the node values at each
+    **odd** level of the tree.
+
+    * For example, suppose the node values at level 3 are `[2,1,3,4,7,11,29,18]`, then
+    it should become `[18,29,11,7,4,3,1,2]`.
+
+    Return *the root of the reversed tree*.
+
+    A binary tree is **perfect** if all parent nodes have two children and all leaves
+    are on the same level.
+
+    The **level** of a node is the number of edges along the path between it and the
+    root node."""
+
+    def reverse_odd_levels(self, root: Optional[TreeNode]) -> Optional[TreeNode]: ...
+
+    reverseOddLevels = reverse_odd_levels
