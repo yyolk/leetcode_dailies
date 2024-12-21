1cae3ccfa8a73703a7aaa65ee2fffab6e8392358
Joseph Chiocchi <input.output@gmail.com>
2024-12-20 20:51:22 -0500

    solution 20241221.py
    - 143ms (beats 60.98%)
    - 42.08mb

--- 202412/20241221.py
+++ 202412/20241221.py
@@ -1,4 +1,5 @@
 # https://leetcode.com/problems/maximum-number-of-k-divisible-components/
+from collections import defaultdict
 
 
 class Solution:
@@ -21,6 +22,44 @@ class Solution:
 
     def max_k_divisible_components(
         self, n: int, edges: list[list[int]], values: list[int], k: int
-    ) -> int: ...
+    ) -> int:
+
+        # Build the adjacency list for the tree
+        tree = defaultdict(list)
+        for a, b in edges:
+            tree[a].append(b)
+            tree[b].append(a)
+
+        # Visited set to track visited nodes
+        visited = set()
+
+        # Variable to count the number of components
+        components = 0
+
+        def dfs(node: int) -> int:
+            """Perform DFS and calculate subtree sums."""
+            nonlocal components
+
+            visited.add(node)
+            # Start with the value of the current node
+            subtree_sum = values[node]
+
+            for neighbor in tree[node]:
+                if neighbor not in visited:
+                    # Recursively calculate the sum for the subtree
+                    child_sum = dfs(neighbor)
+                    subtree_sum += child_sum
+
+            # If the subtree sum is divisible by k, it forms a component
+            if subtree_sum % k == 0:
+                components += 1
+                return 0  # Reset the sum for this subtree
+
+            return subtree_sum
+
+        # Start DFS from node 0 (or any arbitrary node)
+        dfs(0)
+
+        return components
 
     maxKDivisibleComponents = max_k_divisible_components
