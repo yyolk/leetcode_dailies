9bea4990689cbc3f47166090fd1b7df26fa520d5
Joseph Chiocchi <input.output@gmail.com>
2024-12-25 20:13:35 -0500

    solution 20241226.py
    - 167ms (beats 59.28%)
    - 17.93mb (beats 63.39%)

--- 202412/20241226.py
+++ 202412/20241226.py
@@ -15,6 +15,31 @@ class Solution:
     Return the number of different **expressions** that you can build, which evaluates
     to `target`."""
 
-    def find_target_sum_ways(self, nums: list[int], target: int) -> int: ...
+    def find_target_sum_ways(self, nums: list[int], target: int) -> int:
+        # We'll use dynamic programming to solve this problem
+        # dp[i][j] will represent the number of ways to reach sum j using the first i numbers
+
+        total_sum = sum(nums)
+        if abs(target) > total_sum:
+            # If target sum is not possible to achieve
+            return 0
+
+        # Since we can have negative numbers, we need to shift everything by total_sum
+        dp = [[0 for _ in range(2 * total_sum + 1)] for _ in range(len(nums) + 1)]
+
+        # Base case: there's one way to achieve sum 0 with 0 numbers
+        dp[0][total_sum] = 1
+
+        for i in range(1, len(nums) + 1):
+            for j in range(2 * total_sum + 1):
+                if dp[i - 1][j] > 0:
+                    # We can either add or subtract the current number
+                    # Adding nums[i-1]
+                    dp[i][j + nums[i - 1]] += dp[i - 1][j]
+                    # Subtracting nums[i-1]
+                    dp[i][j - nums[i - 1]] += dp[i - 1][j]
+
+        # The target shifted to non-negative index
+        return dp[len(nums)][total_sum + target]
 
     findTargetSumWays = find_target_sum_ways
