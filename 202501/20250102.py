30b0a4e47dc04ac2d084698872e94f1e9be407d9
Joseph Chiocchi <joe@yolk.cc>
2025-01-03 00:23:30 +0000

    Boilerplate for 20250103
    https://leetcode.com/problems/number-of-ways-to-split-array/

--- 202501/20250102.py
+++ 202501/20250102.py
@@ -16,26 +16,24 @@ class Solution:
 
     **Note** that the vowel letters are `'a'`, `'e'`, `'i'`, `'o'`, and `'u'`."""
 
-    def vowel_strings(
-        self, words: list[str], queries: list[list[int]]
-    ) -> list[int]:
+    def vowel_strings(self, words: list[str], queries: list[list[int]]) -> list[int]:
         vowels = set("aeiou")
-        
+
         # Helper function to check if a word starts and ends with a vowel
         def is_vowel_string(word):
             return word[0] in vowels and word[-1] in vowels
-        
+
         # Count vowel strings for each index in words
         count = [0] * (len(words) + 1)
         for i, word in enumerate(words, 1):
-            count[i] = count[i-1] + (1 if is_vowel_string(word) else 0)
-        
+            count[i] = count[i - 1] + (1 if is_vowel_string(word) else 0)
+
         # Process queries
         ans = []
         for li, ri in queries:
             # The count at ri + 1 minus count at li gives us the number of vowel strings in the range
             ans.append(count[ri + 1] - count[li])
-        
+
         return ans
 
     vowelStrings = vowel_strings
--- 202501/README.md
+++ 202501/README.md
@@ -45,7 +45,9 @@
    </a>
   </td>
   <td class="fri">
-   3
+   <a href="20250103.py">
+    3
+   </a>
   </td>
   <td class="sat">
    4
