9eea4a5779e8061e9bdadd54de63e1a507ae80c1
Joseph Chiocchi <input.output@gmail.com>
2025-01-03 20:48:20 -0600

    solution 20250104.py
    - 76ms (beats 97.14%)
    - 18.15mb
    
    resubmission of solution 20231114.py

--- 202501/20250104.py
+++ 202501/20250104.py
@@ -1,4 +1,5 @@
 # https://leetcode.com/problems/unique-length-3-palindromic-subsequences/
+from string import ascii_lowercase
 
 
 class Solution:
@@ -16,8 +17,28 @@ class Solution:
     with some characters (can be none) deleted without changing the relative order of
     the remaining characters.
 
-    * For example, `"ace"` is a subsequence of `"abcde"`."""
+    * For example, `"ace"` is a subsequence of `"abcde"`.
+    """
 
-    def count_palindromic_subsequence(self, s: str) -> int: ...
+    def count_palindromic_subsequence(self, s: str) -> int:
+        """
+        Counts unique subsequences of input that are length of 3.
+
+        Args:
+            s: Input string to find subsequences from.
+
+        Returns:
+            The number of unique, palindromic subsequences that are length of 3.
+        """
+        count = 0
+
+        # Find the first and last occurrence of c, every unique character in between
+        # would make a unique palindromic occurrence of length 3.
+        for c in ascii_lowercase:
+            first, last = s.find(c), s.rfind(c)
+            if first > -1:
+                count += len(set(s[first + 1 : last]))
+
+        return count
 
     countPalindromicSubsequence = count_palindromic_subsequence
