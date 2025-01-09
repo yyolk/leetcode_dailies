9c6abad075907e2274e821e465acc6d86b07899b
Joseph Chiocchi <joe@yolk.cc>
2025-01-09 00:23:39 +0000

    Boilerplate for 20250109
    https://leetcode.com/problems/counting-words-with-a-given-prefix/

---
+++ 202501/20250109.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/counting-words-with-a-given-prefix/
+
+
+class Solution:
+    """2185. Counting Words With a Given Prefix
+
+    You are given an array of strings `words` and a string `pref`.
+
+    Return *the number of strings in* `words` *that contain* `pref` *as a **prefix***.
+
+    A **prefix** of a string `s` is any leading contiguous substring of `s`."""
+
+    def prefix_count(self, words: list[str], pref: str) -> int: ...
+
+    prefixCount = prefix_count
--- 202501/README.md
+++ 202501/README.md
@@ -77,7 +77,9 @@
    </a>
   </td>
   <td class="thu">
-   9
+   <a href="20250109.py">
+    9
+   </a>
   </td>
   <td class="fri">
    10
