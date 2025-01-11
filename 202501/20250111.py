7b0c796b0bc18dd9b74550b53b020938828a96dc
Joseph Chiocchi <input.output@gmail.com>
2025-01-10 23:08:47 -0600

    solution 20250111.py
    - 27ms (beats 92.07%)
    - 17.97mb

--- 202501/20250111.py
+++ 202501/20250111.py
@@ -1,4 +1,5 @@
 # https://leetcode.com/problems/construct-k-palindrome-strings/
+from collections import Counter
 
 
 class Solution:
@@ -8,6 +9,18 @@ class Solution:
     characters in* `s` *to construct* `k` *palindrome strings or* `false` *otherwise*.
     """
 
-    def can_construct(self, s: str, k: int) -> bool: ...
+    def can_construct(self, s: str, k: int) -> bool:
+        # Count the frequency of each character in s
+        char_count = Counter(s)
+        
+        # A palindrome can have at most one character with odd count
+        odd_counts = sum(1 for count in char_count.values() if count % 2 != 0)
+        
+        # Check if we can construct k palindromes:
+        # 1. We can have at most k characters with odd counts since each palindrome can 
+        #    use up to one odd-count character.
+        # 2. The number of palindromes (k) cannot exceed the number of characters.
+        
+        return odd_counts <= k <= len(s)
 
     canConstruct = can_construct
