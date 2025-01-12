dc16b167cce57dcd5cd3bacef8a167b6254a2539
Joseph Chiocchi <joe@yolk.cc>
2025-01-12 00:26:20 +0000

    Boilerplate for 20250112
    https://leetcode.com/problems/check-if-a-parentheses-string-can-be-valid/

--- 202501/20250111.py
+++ 202501/20250111.py
@@ -12,15 +12,15 @@ class Solution:
     def can_construct(self, s: str, k: int) -> bool:
         # Count the frequency of each character in s
         char_count = Counter(s)
-        
+
         # A palindrome can have at most one character with odd count
         odd_counts = sum(1 for count in char_count.values() if count % 2 != 0)
-        
+
         # Check if we can construct k palindromes:
-        # 1. We can have at most k characters with odd counts since each palindrome can 
+        # 1. We can have at most k characters with odd counts since each palindrome can
         #    use up to one odd-count character.
         # 2. The number of palindromes (k) cannot exceed the number of characters.
-        
+
         return odd_counts <= k <= len(s)
 
     canConstruct = can_construct
