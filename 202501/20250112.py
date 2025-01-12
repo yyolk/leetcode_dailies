9b1038a104899203b4290060d1a92125585d9f18
Joseph Chiocchi <input.output@gmail.com>
2025-01-11 20:46:24 -0600

    solution 20250112.py
    - 59ms (beats 96.54%)
    - 21.36mb

--- 202501/20250112.py
+++ 202501/20250112.py
@@ -25,6 +25,43 @@ class Solution:
     Return `true` *if you can make `s` a valid parentheses string*. Otherwise, return
     `false`."""
 
-    def can_be_valid(self, s: str, locked: str) -> bool: ...
+    def can_be_valid(self, s: str, locked: str) -> bool:
+        # Get the length of the string
+        string_length = len(s)
+        
+        # Check if the string length is odd, which cannot make a valid parentheses string
+        if string_length % 2 == 1:
+            return False
+
+        # Lists to keep track of open parentheses and unlocked positions
+        open_indices = []
+        unlocked_indices = []
+
+        # Iterate through each character in the string
+        for i in range(string_length):
+            if locked[i] == '0':  # If position is unlocked, add to unlocked_indices
+                unlocked_indices.append(i)
+            elif s[i] == '(':     # If character is '(', add its index to open_indices
+                open_indices.append(i)
+            elif s[i] == ')':     # If character is ')', match or use an unlocked position
+                if open_indices:  # Try to match with an open parenthesis
+                    open_indices.pop()
+                elif unlocked_indices:  # Use an unlocked position if possible
+                    unlocked_indices.pop()
+                else:  # If no match possible, return False
+                    return False
+
+        # Match remaining open parentheses with unlocked positions
+        while open_indices and unlocked_indices and open_indices[-1] < unlocked_indices[-1]:
+            open_indices.pop()
+            unlocked_indices.pop()
+
+        # If no unmatched open parentheses remain:
+        if not open_indices and unlocked_indices:
+            # Check if the number of remaining unlocked positions is even
+            return len(unlocked_indices) % 2 == 0
+
+        # Return true if no unmatched open parentheses left
+        return not open_indices
 
     canBeValid = can_be_valid
