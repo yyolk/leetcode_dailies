83440ac44fae9266f9f9708889b6423378235bfe
Joseph Chiocchi <input.output@gmail.com>
2025-01-15 00:03:12 -0600

    implement solution 20250115.py 💯
    - 0ms (beats 100%) 💯
    - 17.70mb (beats 59.20%)

--- 202501/20250115.py
+++ 202501/20250115.py
@@ -19,6 +19,16 @@ class Solution:
     The number of **set bits** of an integer is the number of `1`'s in its binary
     representation."""
 
-    def minimize_xor(self, num1: int, num2: int) -> int: ...
+    def minimize_xor(self, num1: int, num2: int) -> int:
+        diff_set_bits = num1.bit_count() - num2.bit_count()
+        if diff_set_bits > 0:
+            # Reset the "diff_set_bits" least significant bits of the number "num1"
+            for _ in range(diff_set_bits):
+                num1 &= num1 - 1
+        else:
+            # Set the "diff_set_bits" least insignificant bits of the number "num1"
+            for _ in range(-diff_set_bits):  
+                num1 |= num1 + 1   
+        return num1
 
     minimizeXor = minimize_xor
