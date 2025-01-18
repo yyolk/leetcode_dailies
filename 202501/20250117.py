45c04d6344bd583fbb61cac553afedf9486b581c
Joseph Chiocchi <input.output@gmail.com>
2025-01-17 18:26:06 -0600

    solution 20250117.py
    - 38ms (beats 80.82%)
    - 22.41mb

--- 202501/20250117.py
+++ 202501/20250117.py
@@ -21,6 +21,19 @@ class Solution:
 
     * A binary array is an array containing only **0's** and **1's**"""
 
-    def does_valid_array_exist(self, derived: list[int]) -> bool: ...
+    def does_valid_array_exist(self, derived: list[int]) -> bool:
+        # The key idea here is that the XOR of all elements in 'derived'
+        # should be 0 for a valid 'original' array to exist because:
+        # If we XOR all equations:
+        # derived[0] ^ derived[1] ^ ... ^ derived[n-1] =
+        # (original[0] ^ original[1]) ^ (original[1] ^ original[2]) ^ ... ^ (original[n-1] ^ original[0])
+        # This simplifies to original[0] ^ original[0] ^ ... ^ original[n-1] ^ original[n-1] which should be 0
+
+        xor_result = 0
+        for num in derived:
+            xor_result ^= num
+
+        # If the XOR of all numbers in derived is 0, then a valid original array exists
+        return xor_result == 0
 
     doesValidArrayExist = does_valid_array_exist
