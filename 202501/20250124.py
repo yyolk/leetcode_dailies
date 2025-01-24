822a9bf4824af5427f15a4895436cb6b9ef8cc89
Joseph Chiocchi <input.output@gmail.com>
2025-01-23 20:09:20 -0600

    solution 20250124.py
    - 59ms
    - 23.55mb (beats 65.86%)

--- 202501/20250124.py
+++ 202501/20250124.py
@@ -16,6 +16,32 @@ class Solution:
     Return *an array containing all the **safe nodes** of the graph*. The answer should
     be sorted in **ascending** order."""
 
-    def eventual_safe_nodes(self, graph: list[list[int]]) -> list[int]: ...
+    def eventual_safe_nodes(self, graph: list[list[int]]) -> list[int]:
+        n = len(graph)
+        
+        # Reverse graph
+        reverse_graph = [[] for _ in range(n)]
+        out_degree = [0] * n
+        
+        for i in range(n):
+            for neighbor in graph[i]:
+                reverse_graph[neighbor].append(i)
+                out_degree[i] += 1
+        
+        # Queue for nodes with no outgoing edges
+        queue = [i for i in range(n) if out_degree[i] == 0]
+        safe_nodes = []
+        
+        while queue:
+            node = queue.pop(0)
+            safe_nodes.append(node)
+            
+            for prev_node in reverse_graph[node]:
+                out_degree[prev_node] -= 1
+                if out_degree[prev_node] == 0:
+                    queue.append(prev_node)
+        
+        # Sort the safe nodes in ascending order
+        return sorted(safe_nodes)
 
     eventualSafeNodes = eventual_safe_nodes
