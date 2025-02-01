11a315400f165d474879a1bb6d13279a04bff251
Joseph Chiocchi <joe@yolk.cc>
2025-02-01 00:25:03 +0000

    Boilerplate for 20250201
    https://leetcode.com/problems/special-array-i/

--- 202501/20250131.py
+++ 202501/20250131.py
@@ -16,7 +16,7 @@ class Solution:
         n = len(grid)
         label = 2  # Start labeling from 2 since 0 and 1 are already used
         island_sizes = defaultdict(int)
-        
+
         # Step 1: Label islands and calculate their sizes
         for i in range(n):
             for j in range(n):
@@ -24,10 +24,10 @@ class Solution:
                     size = self.dfs(grid, i, j, label)
                     island_sizes[label] = size
                     label += 1
-        
+
         # Step 2: Calculate the maximum island size after converting one 0 to 1
         max_size = max(island_sizes.values(), default=0)
-        
+
         for i in range(n):
             for j in range(n):
                 if grid[i][j] == 0:
@@ -38,9 +38,9 @@ class Solution:
                             neighbors.add(grid[x][y])
                     current_size = 1 + sum(island_sizes[label] for label in neighbors)
                     max_size = max(max_size, current_size)
-        
+
         return max_size
-    
+
     def dfs(self, grid: List[List[int]], i: int, j: int, label: int) -> int:
         n = len(grid)
         if i < 0 or i >= n or j < 0 or j >= n or grid[i][j] != 1:
---
+++ 202502/20250201.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/special-array-i/
+
+
+class Solution:
+    """3151. Special Array I
+
+    An array is considered **special** if every pair of its adjacent elements contains
+    two numbers with different parity.
+
+    You are given an array of integers `nums`. Return `true` if `nums` is a **special**
+    array, otherwise, return `false`."""
+
+    def is_array_special(self, nums: list[int]) -> bool: ...
+
+    isArraySpecial = is_array_special
