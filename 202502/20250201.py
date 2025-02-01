aba21d921798c3bae1a04ce9ec0e7783a6468477
Joseph Chiocchi <input.output@gmail.com>
2025-01-31 18:52:47 -0600

    solution 20250201.py 💯
    - 0ms (beats 100%) 💯
    - 18.02mb

--- 202502/20250201.py
+++ 202502/20250201.py
@@ -1,4 +1,5 @@
 # https://leetcode.com/problems/special-array-i/
+from itertools import pairwise
 
 
 class Solution:
@@ -10,6 +11,12 @@ class Solution:
     You are given an array of integers `nums`. Return `true` if `nums` is a **special**
     array, otherwise, return `false`."""
 
-    def is_array_special(self, nums: list[int]) -> bool: ...
+    def is_array_special(self, nums: list[int]) -> bool:
+        # Use pairwise to check parity of adjacent elements
+        for a, b in pairwise(nums):
+            # If parity is the same, array is not special
+            if (a % 2) == (b % 2):
+                return False
+        return True
 
     isArraySpecial = is_array_special
