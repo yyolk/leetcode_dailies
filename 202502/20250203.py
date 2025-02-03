7d128f442d4fdec0949be8be6f82f8fa59d7b841
Joseph Chiocchi <input.output@gmail.com>
2025-02-02 18:34:55 -0600

    solution 20250203.py 💯
    - 0ms (beats 100%) 💯
    - 17.75mb

--- 202502/20250203.py
+++ 202502/20250203.py
@@ -8,6 +8,29 @@ class Solution:
     subarray of* `nums` *which is either **strictly increasing** or **strictly
     decreasing***."""
 
-    def longest_monotonic_subarray(self, nums: list[int]) -> int: ...
+    def longest_monotonic_subarray(self, nums: list[int]) -> int:
+        if not nums:
+            return 0
+        
+        max_length = 1
+        current_inc_length = 1
+        current_dec_length = 1
+        
+        for i in range(1, len(nums)):
+            # Use match to compare nums[i] and nums[i - 1]
+            match (nums[i], nums[i - 1]):
+                case (x, y) if x > y:
+                    current_inc_length += 1
+                    current_dec_length = 1
+                case (x, y) if x < y:
+                    current_dec_length += 1
+                    current_inc_length = 1
+                case _:
+                    current_inc_length = 1
+                    current_dec_length = 1
+            
+            max_length = max(max_length, current_inc_length, current_dec_length)
+        
+        return max_length
 
     longestMonotonicSubarray = longest_monotonic_subarray
