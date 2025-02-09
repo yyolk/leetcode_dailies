f0cc910ed4c37e8e9d1ecc91591e0b8ec88b05e9
Joseph Chiocchi <input.output@gmail.com>
2025-02-08 18:35:54 -0600

    solution 20250209.py
    - 71ms (beats 75.48%)
    - 38.88mb (beats 59.29%)

--- 202502/20250209.py
+++ 202502/20250209.py
@@ -1,4 +1,5 @@
 # https://leetcode.com/problems/count-number-of-bad-pairs/
+from collections import defaultdict
 
 
 class Solution:
@@ -9,6 +10,24 @@ class Solution:
 
     Return *the total number of **bad pairs** in* `nums`."""
 
-    def count_bad_pairs(self, nums: list[int]) -> int: ...
+    def count_bad_pairs(self, nums: list[int]) -> int:
+        n = len(nums)
+        # Total number of pairs (i, j) where i < j
+        total_pairs = n * (n - 1) // 2
+
+        # Count the frequency of nums[i] - i
+        freq = defaultdict(int)
+        for i in range(n):
+            freq[nums[i] - i] += 1
+
+        # Calculate the number of good pairs
+        good_pairs = 0
+        for count in freq.values():
+            if count >= 2:
+                good_pairs += count * (count - 1) // 2
+
+        # Bad pairs = Total pairs - Good pairs
+        bad_pairs = total_pairs - good_pairs
+        return bad_pairs
 
     countBadPairs = count_bad_pairs
