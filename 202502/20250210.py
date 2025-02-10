e9c955fd8695506aa553803125b7253686439ca1
Joseph Chiocchi <input.output@gmail.com>
2025-02-09 19:26:31 -0600

    solution 20250210.py 💯
    - 0ms (beats 100%)
    - 17.82mb

--- 202502/20250210.py
+++ 202502/20250210.py
@@ -13,6 +13,17 @@ class Solution:
 
     Return the resulting string after removing all digits."""
 
-    def clear_digits(self, s: str) -> str: ...
+    def clear_digits(self, s: str) -> str:
+        stack = []
+        for char in s:
+            if char.isdigit():
+                # If the current character is a digit, pop the closest non-digit to its left
+                if stack:
+                    # Remove the closest non-digit to the left
+                    stack.pop()
+            else:
+                # If it's not a digit, push it onto the stack
+                stack.append(char)
+        return "".join(stack)
 
     clearDigits = clear_digits
