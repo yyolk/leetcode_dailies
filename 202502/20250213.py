f5fce72be1a5489ef165235ee7bfa4f09ad2ac72
Joseph Chiocchi <joe@yolk.cc>
2025-02-13 00:23:36 +0000

    Boilerplate for 20250213
    https://leetcode.com/problems/minimum-operations-to-exceed-threshold-value-ii/

---
+++ 202502/20250213.py
@@ -0,0 +1,25 @@
+# https://leetcode.com/problems/minimum-operations-to-exceed-threshold-value-ii/
+
+
+class Solution:
+    """3066. Minimum Operations to Exceed Threshold Value II
+
+    You are given a **0-indexed** integer array `nums`, and an integer `k`.
+
+    In one operation, you will:
+
+    * Take the two smallest integers `x` and `y` in `nums`.
+
+    * Remove `x` and `y` from `nums`.
+
+    * Add `min(x, y) * 2 + max(x, y)` anywhere in the array.
+
+    **Note** that you can only apply the described operation if `nums` contains at least
+    two elements.
+
+    Return *the **minimum** number of operations needed so that all elements of the
+    array are greater than or equal to* `k`."""
+
+    def min_operations(self, nums: list[int], k: int) -> int: ...
+
+    minOperations = min_operations
