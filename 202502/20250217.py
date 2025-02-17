81b10a23194803a1952df43af08c28a26193255d
Joseph Chiocchi <joe@yolk.cc>
2025-02-17 00:25:18 +0000

    Boilerplate for 20250217
    https://leetcode.com/problems/letter-tile-possibilities/

---
+++ 202502/20250217.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/letter-tile-possibilities/
+
+
+class Solution:
+    """1079. Letter Tile Possibilities
+
+    You have `n`  `tiles`, where each tile has one letter `tiles[i]` printed on it.
+
+    Return *the number of possible non-empty sequences of letters* you can make using
+    the letters printed on those `tiles`."""
+
+    def num_tile_possibilities(self, tiles: str) -> int: ...
+
+    numTilePossibilities = num_tile_possibilities
