338e7656cb34496f0dc70041ee6faeb16e63b9d1
Joseph Chiocchi <joe@yolk.cc>
2025-03-01 00:26:39 +0000

    Boilerplate for 20250301
    https://leetcode.com/problems/apply-operations-to-an-array/

--- 202502/20250228.py
+++ 202502/20250228.py
@@ -14,10 +14,10 @@ class Solution:
     def shortest_common_supersequence(self, str1: str, str2: str) -> str:
         # Get lengths of input strings
         m, n = len(str1), len(str2)
-        
+
         # Initialize DP table for LCS
         dp = [[0] * (n + 1) for _ in range(m + 1)]
-        
+
         # Fill DP table to compute LCS length
         for i in range(1, m + 1):
             for j in range(1, n + 1):
@@ -25,7 +25,7 @@ class Solution:
                     dp[i][j] = dp[i - 1][j - 1] + 1
                 else:
                     dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
-        
+
         # Build SCS by tracing back through DP table
         res = []
         i, j = m, n
@@ -43,17 +43,17 @@ class Solution:
                 # LCS comes from str2 excluding current char, or equal, include str2[j-1]
                 res.append(str2[j - 1])
                 j -= 1
-        
+
         # Append remaining characters from str1, if any
         while i > 0:
             res.append(str1[i - 1])
             i -= 1
-        
+
         # Append remaining characters from str2, if any
         while j > 0:
             res.append(str2[j - 1])
             j -= 1
-        
+
         # Reverse the result since we built it backwards, then join into a string
         return "".join(reversed(res))
 
