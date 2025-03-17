0cf9062a0d336384bf3baadae480622391f9e041
Joseph Chiocchi <input.output@gmail.com>
2025-03-16 20:16:26 -0500

    solution 20250317.py 💯
    - 0ms (beats 100%) 💯
    - 17.97mb

--- 202503/20250317.py
+++ 202503/20250317.py
@@ -1,4 +1,5 @@
 # https://leetcode.com/problems/divide-array-into-equal-pairs/
+from collections import Counter
 
 
 class Solution:
@@ -15,6 +16,11 @@ class Solution:
     Return `true` *if nums can be divided into* `n` *pairs, otherwise return* `false`.
     """
 
-    def divide_array(self, nums: list[int]) -> bool: ...
+    def divide_array(self, nums: list[int]) -> bool:
+        # Count the frequency of each number in the array
+        counter = Counter(nums)
+        
+        # Check if all frequencies are even
+        return all(count % 2 == 0 for count in counter.values())
 
     divideArray = divide_array
