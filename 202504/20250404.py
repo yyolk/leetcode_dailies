ba9fa4deeb7b3410423aff7e0e905550cdc3c38c
Joseph Chiocchi <input.output@gmail.com>
2025-04-03 20:08:11 -0500

    solution 20250404.py 💯
    - 0ms (beats 100%) 💯
    - 18.27mb

--- 202504/20250404.py
+++ 202504/20250404.py
@@ -1,6 +1,12 @@
 # https://leetcode.com/problems/lowest-common-ancestor-of-deepest-leaves/
 
 
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution:
     """1123. Lowest Common Ancestor of Deepest Leaves
 
@@ -17,6 +23,26 @@ class Solution:
     * The lowest common ancestor of a set `S` of nodes, is the node `A` with the largest
     depth such that every node in `S` is in the subtree with root `A`."""
 
-    def lca_deepest_leaves(self, root: Optional[TreeNode]) -> Optional[TreeNode]: ...
+    def lca_deepest_leaves(self, root: TreeNode | None) -> TreeNode | None:
+        def helper(node: TreeNode | None, depth: int) -> tuple[int, TreeNode | None]:
+            # Base case: null node
+            if not node:
+                return (-1, None)
+            # Base case: leaf node
+            if not node.left and not node.right:
+                return (depth, node)
+            # Recurse on left and right children
+            left_max_depth, left_lca = helper(node.left, depth + 1)
+            right_max_depth, right_lca = helper(node.right, depth + 1)
+            # Compare depths to determine LCA
+            if left_max_depth > right_max_depth:
+                return (left_max_depth, left_lca)
+            elif right_max_depth > left_max_depth:
+                return (right_max_depth, right_lca)
+            else:
+                return (left_max_depth, node)
+        
+        # Start recursion from root at depth 0 and return the LCA
+        return helper(root, 0)[1]
 
     lcaDeepestLeaves = lca_deepest_leaves
