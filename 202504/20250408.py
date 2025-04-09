aa85d24591c97e6e0f848718bca3a6d16e0a7215
Joseph Chiocchi <joe@yolk.cc>
2025-04-09 00:25:26 +0000

    Boilerplate for 20250409
    https://leetcode.com/problems/minimum-operations-to-make-array-values-equal-to-k/

--- 202504/20250408.py
+++ 202504/20250408.py
@@ -23,7 +23,7 @@ class Solution:
         # Loop until we find the smallest number of operations to make the suffix distinct
         while True:
             # Extract the suffix after removing the first 3*k elements
-            suffix = nums[3 * k:]
+            suffix = nums[3 * k :]
             # Check if the suffix has all distinct elements by comparing set length to list length
             if len(set(suffix)) == len(suffix):
                 # If suffix is distinct (or empty), return the number of operations performed
