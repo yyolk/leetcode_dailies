ee2bb2352403b5d48c16daf8cba94cba44609d88
Joseph Chiocchi <joe@yolk.cc>
2025-04-10 00:25:25 +0000

    Boilerplate for 20250410
    https://leetcode.com/problems/count-the-number-of-powerful-integers/

---
+++ 202504/20250410.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/count-the-number-of-powerful-integers/
+
+
+class Solution:
+    """2999. Count the Number of Powerful Integers
+
+    You are given three integers `start`, `finish`, and `limit`. You are also given a
+    **0-indexed** string `s` representing a **positive** integer.
+
+    A **positive** integer `x` is called **powerful** if it ends with `s` (in other
+    words, `s` is a **suffix** of `x`) and each digit in `x` is at most `limit`.
+
+    Return *the **total** number of powerful integers in the range* `[start..finish]`.
+
+    A string `x` is a suffix of a string `y` if and only if `x` is a substring of `y`
+    that starts from some index (**including** `0`) in `y` and extends to the index
+    `y.length - 1`. For example, `25` is a suffix of `5125` whereas `512` is not."""
+
+    def number_of_powerful_int(
+        self, start: int, finish: int, limit: int, s: str
+    ) -> int: ...
+
+    numberOfPowerfulInt = number_of_powerful_int
