26e9829bdccfa0bd5cc376a82998725f8556c8b1
Joseph Chiocchi <joe@yolk.cc>
2025-04-12 00:25:01 +0000

    Boilerplate for 20250412
    https://leetcode.com/problems/find-the-count-of-good-integers/

---
+++ 202504/20250412.py
@@ -0,0 +1,28 @@
+# https://leetcode.com/problems/find-the-count-of-good-integers/
+
+
+class Solution:
+    """3272. Find the Count of Good Integers
+
+    You are given two **positive** integers `n` and `k`.
+
+    An integer `x` is called **k-palindromic** if:
+
+    * `x` is a palindrome.
+
+    * `x` is divisible by `k`.
+
+    An integer is called **good** if its digits can be *rearranged* to form a
+    **k-palindromic** integer. For example, for `k = 2`, 2020 can be rearranged to form
+    the *k-palindromic* integer 2002, whereas 1010 cannot be rearranged to form a
+    *k-palindromic* integer.
+
+    Return the count of **good** integers containing `n` digits.
+
+    **Note** that *any* integer must **not** have leading zeros, **neither** before
+    **nor** after rearrangement. For example, 1010 *cannot* be rearranged to form 101.
+    """
+
+    def count_good_integers(self, n: int, k: int) -> int: ...
+
+    countGoodIntegers = count_good_integers
