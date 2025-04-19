c64b470725fc6101a4a0fab7936c5b08138ce288
Joseph Chiocchi <joe@yolk.cc>
2025-04-19 00:24:42 +0000

    Boilerplate for 20250419
    https://leetcode.com/problems/count-the-number-of-fair-pairs/

---
+++ 202504/20250419.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/count-the-number-of-fair-pairs/
+
+
+class Solution:
+    """2563. Count the Number of Fair Pairs
+
+    Given a **0-indexed** integer array `nums` of size `n` and two integers `lower` and
+    `upper`, return *the number of fair pairs*.
+
+    A pair `(i, j)` is **fair** if:
+
+    * `0 <= i < j < n`, and
+
+    * `lower <= nums[i] + nums[j] <= upper`"""
+
+    def count_fair_pairs(self, nums: list[int], lower: int, upper: int) -> int: ...
+
+    countFairPairs = count_fair_pairs
--- 202504/README.md
+++ 202504/README.md
@@ -127,7 +127,9 @@
    </a>
   </td>
   <td class="sat">
-   19
+   <a href="20250419.py">
+    19
+   </a>
   </td>
  </tr>
  <tr>
