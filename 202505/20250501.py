82f032d0bdd6f1b90278989a1e05015952cea004
Joseph Chiocchi <joe@yolk.cc>
2025-05-01 00:29:46 +0000

    Boilerplate for 20250501
    https://leetcode.com/problems/maximum-number-of-tasks-you-can-assign/

---
+++ 202505/20250501.py
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/maximum-number-of-tasks-you-can-assign/
+
+
+class Solution:
+    """2071. Maximum Number of Tasks You Can Assign
+
+    You have `n` tasks and `m` workers. Each task has a strength requirement stored in a
+    **0-indexed** integer array `tasks`, with the `ith` task requiring `tasks[i]`
+    strength to complete. The strength of each worker is stored in a **0-indexed**
+    integer array `workers`, with the `jth` worker having `workers[j]` strength. Each
+    worker can only be assigned to a **single** task and must have a strength **greater
+    than or equal** to the task's strength requirement (i.e., `workers[j] >= tasks[i]`).
+
+    Additionally, you have `pills` magical pills that will **increase a worker's
+    strength** by `strength`. You can decide which workers receive the magical pills,
+    however, you may only give each worker **at most one** magical pill.
+
+    Given the **0-indexed** integer arrays `tasks` and `workers` and the integers
+    `pills` and `strength`, return *the **maximum** number of tasks that can be
+    completed.*"""
+
+    def max_task_assign(
+        self, tasks: list[int], workers: list[int], pills: int, strength: int
+    ) -> int: ...
+
+    maxTaskAssign = max_task_assign
