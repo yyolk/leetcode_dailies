0351f4d3bfc5b3eeceb7f3a0b4e7c6a3b3b8a464
Joseph Chiocchi <input.output@gmail.com>
2025-05-04 19:58:29 -0500

    solution 20250505.py 💯
    - 0ms (beats 100%) 💯
    - 17.95mb (beats 56.45%)

--- 202505/20250505.py
+++ 202505/20250505.py
@@ -1,4 +1,5 @@
 # https://leetcode.com/problems/domino-and-tromino-tiling/
+MOD = 1_000_000_007
 
 
 class Solution:
@@ -10,12 +11,28 @@ class Solution:
     ![](https://assets.leetcode.com/uploads/2021/07/15/lc-domino.jpg)
 
     Given an integer n, return *the number of ways to tile an* `2 x n` *board*. Since
-    the answer may be very large, return it **modulo** `109 + 7`.
+    the answer may be very large, return it **modulo** `10^9 + 7`.
 
     In a tiling, every square must be covered by a tile. Two tilings are different if
     and only if there are two 4-directionally adjacent cells on the board such that
     exactly one of the tilings has both squares occupied by a tile."""
 
-    def num_tilings(self, n: int) -> int: ...
+    def num_tilings(self, n: int) -> int:
+        if n == 1:
+            return 1
+        elif n == 2:
+            return 2
+        # Initialize for dp[0], dp[1], dp[2]
+        dp_prev3 = 1  # dp[0]
+        dp_prev2 = 1  # dp[1]
+        dp_prev1 = 2  # dp[2]
+        # Compute dp[i] from i=3 to n
+        for i in range(3, n + 1):
+            dp_current = (2 * dp_prev1 + dp_prev3) % MOD
+            # Shift variables
+            dp_prev3 = dp_prev2
+            dp_prev2 = dp_prev1
+            dp_prev1 = dp_current
+        return dp_prev1
 
     numTilings = num_tilings
