b9aa81cb7048caa89467f953f04b0e677f61683b
Joseph Chiocchi <joe@yolk.cc>
2025-05-10 00:25:43 +0000

    Boilerplate for 20250510
    https://leetcode.com/problems/minimum-equal-sum-of-two-arrays-after-replacing-zeros/

---
+++ 202505/20250510.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/minimum-equal-sum-of-two-arrays-after-replacing-zeros/
+
+
+class Solution:
+    """2918. Minimum Equal Sum of Two Arrays After Replacing Zeros
+
+    You are given two arrays `nums1` and `nums2` consisting of positive integers.
+
+    You have to replace **all** the `0`'s in both arrays with **strictly** positive
+    integers such that the sum of elements of both arrays becomes **equal**.
+
+    Return *the **minimum** equal sum you can obtain, or* `-1` *if it is impossible*."""
+
+    def min_sum(self, nums1: list[int], nums2: list[int]) -> int: ...
+
+    minSum = min_sum
--- 202505/README.md
+++ 202505/README.md
@@ -84,7 +84,9 @@
    </a>
   </td>
   <td class="sat">
-   10
+   <a href="20250510.py">
+    10
+   </a>
   </td>
  </tr>
  <tr>
