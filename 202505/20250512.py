106bee670859d6899dd90d754cfaab2291017a78
Joseph Chiocchi <input.output@gmail.com>
2025-05-11 20:19:54 -0500

    solution 20250512.py
    - 2723ms
    - 17.85mb (beats 65.57mb)

--- 202505/20250512.py
+++ 202505/20250512.py
@@ -1,4 +1,5 @@
 # https://leetcode.com/problems/finding-3-digit-even-numbers/
+from itertools import permutations
 
 
 class Solution:
@@ -21,6 +22,21 @@ class Solution:
 
     Return *a **sorted** array of the unique integers.*"""
 
-    def find_even_numbers(self, digits: list[int]) -> list[int]: ...
+    def find_even_numbers(self, digits: list[int]) -> list[int]:
+        # Set to store unique valid numbers
+        result = set()
+        
+        # Generate all permutations of 3 digits
+        for perm in permutations(digits, 3):
+            # Check conditions:
+            # 1. No leading zeros (first digit != 0)
+            # 2. Number is even (last digit % 2 == 0)
+            if perm[0] != 0 and perm[2] % 2 == 0:
+                # Form the three-digit number
+                num = 100 * perm[0] + 10 * perm[1] + perm[2]
+                result.add(num)
+        
+        # Return sorted list of unique numbers
+        return sorted(list(result))
 
     findEvenNumbers = find_even_numbers
