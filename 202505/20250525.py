14a3e6a8333578e9e795ca5a1cf16596b0fd42de
Joseph Chiocchi <joe@yolk.cc>
2025-05-25 00:30:21 +0000

    Boilerplate for 20250525
    https://leetcode.com/problems/longest-palindrome-by-concatenating-two-letter-words/

---
+++ 202505/20250525.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/longest-palindrome-by-concatenating-two-letter-words/
+
+
+class Solution:
+    """2131. Longest Palindrome by Concatenating Two Letter Words
+
+    You are given an array of strings `words`. Each element of `words` consists of
+    **two** lowercase English letters.
+
+    Create the **longest possible palindrome** by selecting some elements from `words`
+    and concatenating them in **any order**. Each element can be selected **at most
+    once**.
+
+    Return *the **length** of the longest palindrome that you can create*. If it is
+    impossible to create any palindrome, return `0`.
+
+    A **palindrome** is a string that reads the same forward and backward."""
+
+    def longest_palindrome(self, words: list[str]) -> int: ...
+
+    longestPalindrome = longest_palindrome
--- 202505/README.md
+++ 202505/README.md
@@ -165,7 +165,9 @@
  </tr>
  <tr>
   <td class="sun">
-   25
+   <a href="20250525.py">
+    25
+   </a>
   </td>
   <td class="mon">
    26
