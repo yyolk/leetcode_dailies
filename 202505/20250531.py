b9903d6a2c9b3df33a35130c343047dcebcee7e7
Joseph Chiocchi <input.output@gmail.com>
2025-05-30 19:39:14 -0500

    solution 20250531.py
    - 31ms
    - 17.82mb (beats 82.91%)

--- 202505/20250531.py
+++ 202505/20250531.py
@@ -1,4 +1,5 @@
 # https://leetcode.com/problems/snakes-and-ladders/
+from collections import deque
 
 
 class Solution:
@@ -38,6 +39,37 @@ class Solution:
     Return *the least number of dice rolls required to reach the square* `n2`*. If it is
     not possible to reach the square, return* `-1`."""
 
-    def snakes_and_ladders(self, board: list[list[int]]) -> int: ...
+    def snakes_and_ladders(self, board: list[list[int]]) -> int:        
+        n = len(board)
+        
+        # Helper function to convert label k to (row, col) position
+        def label_to_pos(k):
+            r = n - 1 - (k - 1) // n
+            if ((k - 1) // n) % 2 == 0:
+                c = (k - 1) % n
+            else:
+                c = n - 1 - (k - 1) % n
+            return r, c
+        
+        # BFS setup
+        queue = deque([(1, 0)])  # (current square, moves)
+        visited = set([1])
+        
+        # BFS loop
+        while queue:
+            curr, moves = queue.popleft()
+            if curr == n * n:
+                return moves
+            
+            # Try all possible dice rolls (1 to 6)
+            for next_label in range(curr + 1, min(curr + 7, n * n + 1)):
+                r, c = label_to_pos(next_label)
+                # If there's a snake or ladder, go to its destination; otherwise, stay at next_label
+                final = board[r][c] if board[r][c] != -1 else next_label
+                if final not in visited:
+                    visited.add(final)
+                    queue.append((final, moves + 1))
+        
+        return -1
 
     snakesAndLadders = snakes_and_ladders
