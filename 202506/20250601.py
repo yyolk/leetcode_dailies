46064c273b5a86a95b7cbc09f1fcf5374685fe20
Joseph Chiocchi <joe@yolk.cc>
2025-06-02 00:29:00 +0000

    Boilerplate for 20250602
    https://leetcode.com/problems/candy/

--- 202506/20250601.py
+++ 202506/20250601.py
@@ -13,11 +13,13 @@ class Solution:
         def comb2(m):
             """Helper function to compute C(m, 2) = m*(m-1)/2 if m >= 2, else 0."""
             return m * (m - 1) // 2 if m >= 2 else 0
-        
+
         # Use inclusion-exclusion to find the number of valid distributions
-        return (comb2(n + 2) - 
-                3 * comb2(n - limit + 1) + 
-                3 * comb2(n - 2 * limit) - 
-                comb2(n - 3 * limit - 1))
+        return (
+            comb2(n + 2)
+            - 3 * comb2(n - limit + 1)
+            + 3 * comb2(n - 2 * limit)
+            - comb2(n - 3 * limit - 1)
+        )
 
     distributeCandies = distribute_candies
---
+++ 202506/20250602.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/candy/
+
+
+class Solution:
+    """135. Candy
+
+    There are `n` children standing in a line. Each child is assigned a rating value
+    given in the integer array `ratings`.
+
+    You are giving candies to these children subjected to the following requirements:
+
+    * Each child must have at least one candy.
+
+    * Children with a higher rating get more candies than their neighbors.
+
+    Return *the minimum number of candies you need to have to distribute the candies to
+    the children*."""
+
+    def candy(self, ratings: list[int]) -> int: ...
+
+    candy = candy
