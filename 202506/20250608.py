e2ddcb541684babb6f36d0b0b7ba560bb1d49992
Joseph Chiocchi <input.output@gmail.com>
2025-06-07 19:58:21 -0500

    solution 20250608.py
    - 7ms (beats 95.08%)
    - 21.25mb (beats 77.82%)

--- 202506/20250608.py
+++ 202506/20250608.py
@@ -9,6 +9,19 @@ class Solution:
 
     You must write an algorithm that runs in `O(n)` time and uses `O(1)` extra space."""
 
-    def lexical_order(self, n: int) -> list[int]: ...
+    def lexical_order(self, n: int) -> list[int]:
+        result = []
+        current = 1
+        for _ in range(n):
+            result.append(current)
+            if current * 10 <= n:
+                current *= 10
+            else:
+                if current >= n:
+                    current //= 10
+                current += 1
+                while current % 10 == 0:
+                    current //= 10
+        return result
 
     lexicalOrder = lexical_order
