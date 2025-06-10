a991a29ff774f6b59459d7942853a346256c19e5
Joseph Chiocchi <joe@yolk.cc>
2025-06-10 00:27:46 +0000

    Boilerplate for 20250610
    https://leetcode.com/problems/maximum-difference-between-even-and-odd-frequency-i/

---
+++ 202506/20250610.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/maximum-difference-between-even-and-odd-frequency-i/
+
+
+class Solution:
+    """3442. Maximum Difference Between Even and Odd Frequency I
+
+    You are given a string `s` consisting of lowercase English letters.
+
+    Your task is to find the **maximum** difference `diff = a1 - a2` between the
+    frequency of characters `a1` and `a2` in the string such that:
+
+    * `a1` has an **odd frequency** in the string.
+
+    * `a2` has an **even frequency** in the string.
+
+    Return this **maximum** difference."""
+
+    def max_difference(self, s: str) -> int: ...
+
+    maxDifference = max_difference
--- 202506/README.md
+++ 202506/README.md
@@ -76,7 +76,9 @@
    </a>
   </td>
   <td class="tue">
-   10
+   <a href="20250610.py">
+    10
+   </a>
   </td>
   <td class="wed">
    11
