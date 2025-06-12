81305f4f3157a6652019fba3bc19aa7fc56edd19
Joseph Chiocchi <input.output@gmail.com>
2025-06-11 20:15:51 -0500

    solution 20250612.py 💯
    - 0ms (beats 100%) 💯
    - 17.62mb (beats 80.05%)

--- 202506/20250612.py
+++ 202506/20250612.py
@@ -1,4 +1,5 @@
 # https://leetcode.com/problems/maximum-difference-between-adjacent-elements-in-a-circular-array/
+from itertools import pairwise
 
 
 class Solution:
@@ -9,6 +10,10 @@ class Solution:
 
     **Note**: In a circular array, the first and last elements are adjacent."""
 
-    def max_adjacent_distance(self, nums: list[int]) -> int: ...
+    def max_adjacent_distance(self, nums: list[int]) -> int:
+        if len(nums) < 2:
+            return 0
+        extended = nums + [nums[0]]
+        return max(abs(a - b) for a, b in pairwise(extended))
 
     maxAdjacentDistance = max_adjacent_distance
