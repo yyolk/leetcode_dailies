944271e4bcd5a571f1280df0dbe6edf149bf8c7d
Joseph Chiocchi <joe@yolk.cc>
2025-06-16 00:29:12 +0000

    Boilerplate for 20250616
    https://leetcode.com/problems/maximum-difference-between-increasing-elements/

---
+++ 202506/20250616.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/maximum-difference-between-increasing-elements/
+
+
+class Solution:
+    """2016. Maximum Difference Between Increasing Elements
+
+    Given a **0-indexed** integer array `nums` of size `n`, find the **maximum
+    difference** between `nums[i]` and `nums[j]` (i.e., `nums[j] - nums[i]`), such that
+    `0 <= i < j < n` and `nums[i] < nums[j]`.
+
+    Return *the **maximum difference**.* If no such `i` and `j` exists, return `-1`."""
+
+    def maximum_difference(self, nums: list[int]) -> int: ...
+
+    maximumDifference = maximum_difference
--- 202506/README.md
+++ 202506/README.md
@@ -108,7 +108,9 @@
    </a>
   </td>
   <td class="mon">
-   16
+   <a href="20250616.py">
+    16
+   </a>
   </td>
   <td class="tue">
    17
