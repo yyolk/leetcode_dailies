16ffd3d7cbce09ecea1208baedfc1abb2f638c8f
Joseph Chiocchi <joe@yolk.cc>
2025-06-28 00:26:54 +0000

    Boilerplate for 20250628
    https://leetcode.com/problems/find-subsequence-of-length-k-with-the-largest-sum/

---
+++ 202506/20250628.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/find-subsequence-of-length-k-with-the-largest-sum/
+
+
+class Solution:
+    """2099. Find Subsequence of Length K With the Largest Sum
+
+    You are given an integer array `nums` and an integer `k`. You want to find a
+    **subsequence** of `nums` of length `k` that has the **largest** sum.
+
+    Return***any** such subsequence as an integer array of length* `k`.
+
+    A **subsequence** is an array that can be derived from another array by deleting
+    some or no elements without changing the order of the remaining elements."""
+
+    def max_subsequence(self, nums: list[int], k: int) -> list[int]: ...
+
+    maxSubsequence = max_subsequence
--- 202506/README.md
+++ 202506/README.md
@@ -170,7 +170,9 @@
    </a>
   </td>
   <td class="sat">
-   28
+   <a href="20250628.py">
+    28
+   </a>
   </td>
  </tr>
  <tr>
