17bf6d55eab36bbcf4646fea4741c133823c4442
Joseph Chiocchi <input.output@gmail.com>
2025-07-03 20:03:32 -0500

    solution 20250704.py 💯
    - 0ms (beats 100%) 💯
    - 17.74mb (beats 81.16%)

--- 202507/20250704.py
+++ 202507/20250704.py
@@ -21,9 +21,21 @@ class Solution:
     Return the value of the `kth` character in `word` after performing all the
     operations.
 
-    **Note** that the character `'z'` can be changed to `'a'` in the second type of
+    **Note** that the character `"z"` can be changed to `"a"` in the second type of
     operation."""
 
-    def kth_character(self, k: int, operations: list[int]) -> str: ...
+    def kth_character(self, k: int, operations: list[int]) -> str:
+        # Initialize a mask to track which operations involve shifting
+        shift_mask = 0
+        # Iterate over bit positions up to the number of bits needed to represent (k-1)
+        for bit_position in range((k - 1).bit_length()):
+            # Update the mask by setting the bit if the operation is a shift (1), else contribute 0
+            shift_mask |= (operations[bit_position] << bit_position)
+        # Compute the total number of shifts by counting set bits in the intersection of (k-1) and shift_mask
+        total_shifts = ((k - 1) & shift_mask).bit_count()
+        # Calculate the final character by shifting 'a' by the total number of shifts, wrapping around after 26
+        final_character = chr(97 + (total_shifts % 26))
+        # Return the resulting character
+        return final_character
 
     kthCharacter = kth_character
