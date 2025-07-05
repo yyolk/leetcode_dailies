41dbee1a7157d9ad9479100723674c05ad1dbcb0
Joseph Chiocchi <input.output@gmail.com>
2025-07-04 19:57:57 -0500

    solution 20250705.py 💯
    - 0ms (beats 100%) 💯
    - 17.96mb

--- 202507/20250705.py
+++ 202507/20250705.py
@@ -10,6 +10,18 @@ class Solution:
     Return *the largest **lucky integer** in the array*. If there is no **lucky
     integer** return `-1`."""
 
-    def find_lucky(self, arr: list[int]) -> int: ...
+    def find_lucky(self, arr: list[int]) -> int:
+        # Count frequency of each number using a dictionary
+        freq = {}
+        for num in arr:
+            freq[num] = freq.get(num, 0) + 1
+        
+        # Find lucky numbers where frequency equals value
+        lucky = -1
+        for num in freq:
+            if num == freq[num]:
+                lucky = max(lucky, num)
+        
+        return lucky
 
     findLucky = find_lucky
