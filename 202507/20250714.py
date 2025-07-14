61ff49f0d3e04ac8e9e7498774d3e242929e0f75
Joseph Chiocchi <input.output@gmail.com>
2025-07-13 19:34:27 -0500

    solution 20250714.py 💯
    - 0ms (beats 100%) 💯
    - 17.94mb

--- 202507/20250714.py
+++ 202507/20250714.py
@@ -1,6 +1,11 @@
 # https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/
 
 
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
 class Solution:
     """1290. Convert Binary Number in a Linked List to Integer
 
@@ -12,6 +17,12 @@ class Solution:
 
     The **most significant bit** is at the head of the linked list."""
 
-    def get_decimal_value(self, head: Optional[ListNode]) -> int: ...
+    def get_decimal_value(self, head: ListNode | None) -> int:
+        result = 0
+        current = head
+        while current:
+            result = result * 2 + current.val
+            current = current.next
+        return result
 
     getDecimalValue = get_decimal_value
