7c6ae3e4d5e9ab1c1a9ec58321f153443dc3349f
Joseph Chiocchi <joe@yolk.cc>
2025-07-31 00:29:50 +0000

    Boilerplate for 20250731
    https://leetcode.com/problems/bitwise-ors-of-subarrays/

---
+++ 202507/20250731.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/bitwise-ors-of-subarrays/
+
+
+class Solution:
+    """898. Bitwise ORs of Subarrays
+
+    Given an integer array `arr`, return *the number of distinct bitwise ORs of all the
+    non-empty subarrays of* `arr`.
+
+    The bitwise OR of a subarray is the bitwise OR of each integer in the subarray. The
+    bitwise OR of a subarray of one integer is that integer.
+
+    A **subarray** is a contiguous non-empty sequence of elements within an array."""
+
+    def subarray_bitwise_o_rs(self, arr: list[int]) -> int: ...
+
+    subarrayBitwiseORs = subarray_bitwise_o_rs
--- 202507/README.md
+++ 202507/README.md
@@ -191,7 +191,9 @@
    </a>
   </td>
   <td class="thu">
-   31
+   <a href="20250731.py">
+    31
+   </a>
   </td>
   <td class="noday">
   </td>
