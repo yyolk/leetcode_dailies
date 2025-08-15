acbee6b5f9ace94260402302d5862abb255114d1
Joseph Chiocchi <joe@yolk.cc>
2025-08-15 00:28:56 +0000

    Boilerplate for 20250815
    https://leetcode.com/problems/power-of-four/

---
+++ 202508/20250815.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/power-of-four/
+
+
+class Solution:
+    """342. Power of Four
+
+    Given an integer `n`, return *`true` if it is a power of four. Otherwise, return
+    `false`*.
+
+    An integer `n` is a power of four, if there exists an integer `x` such that `n ==
+    4x`."""
+
+    def is_power_of_four(self, n: int) -> bool: ...
+
+    isPowerOfFour = is_power_of_four
