693ccf018205b278abcc300f40f5038ba458fb4a
Joseph Chiocchi <joe@yolk.cc>
2025-09-24 00:25:57 +0000

    Boilerplate for 20250924
    https://leetcode.com/problems/fraction-to-recurring-decimal/

---
+++ 202509/20250924.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/fraction-to-recurring-decimal/
+
+
+class Solution:
+    """166. Fraction to Recurring Decimal
+
+    Given two integers representing the `numerator` and `denominator` of a fraction,
+    return *the fraction in string format*.
+
+    If the fractional part is repeating, enclose the repeating part in parentheses.
+
+    If multiple answers are possible, return **any of them**.
+
+    It is **guaranteed** that the length of the answer string is less than `104` for all
+    the given inputs."""
+
+    def fraction_to_decimal(self, numerator: int, denominator: int) -> str: ...
+
+    fractionToDecimal = fraction_to_decimal
