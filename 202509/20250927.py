ef478e3a58b6b572f382b94566bafbfb9e4f5c28
Joseph Chiocchi <joe@yolk.cc>
2025-09-27 00:24:49 +0000

    Boilerplate for 20250927
    https://leetcode.com/problems/largest-triangle-area/

---
+++ 202509/20250927.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/largest-triangle-area/
+
+
+class Solution:
+    """812. Largest Triangle Area
+
+    Given an array of points on the **X-Y** plane `points` where `points[i] = [xi, yi]`,
+    return *the area of the largest triangle that can be formed by any three different
+    points*. Answers within `10-5` of the actual answer will be accepted."""
+
+    def largest_triangle_area(self, points: list[list[int]]) -> float: ...
+
+    largestTriangleArea = largest_triangle_area
