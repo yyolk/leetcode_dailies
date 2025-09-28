f60d4b5e6f1cf2c5764f4102af579235a27ae067
Joseph Chiocchi <joe@yolk.cc>
2025-09-28 00:28:53 +0000

    Boilerplate for 20250928
    https://leetcode.com/problems/largest-perimeter-triangle/

---
+++ 202509/20250928.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/largest-perimeter-triangle/
+
+
+class Solution:
+    """976. Largest Perimeter Triangle
+
+    Given an integer array `nums`, return *the largest perimeter of a triangle with a
+    non-zero area, formed from three of these lengths*. If it is impossible to form any
+    triangle of a non-zero area, return `0`."""
+
+    def largest_perimeter(self, nums: list[int]) -> int: ...
+
+    largestPerimeter = largest_perimeter
