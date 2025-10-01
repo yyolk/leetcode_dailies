7c70f766e45f2f582713c233c479b857935dea70
Joseph Chiocchi <input.output@gmail.com>
2025-09-30 19:33:59 -0500

    solution 20251001.py 💯
    - 0ms (beats 100%) 💯
    - 17.81mb

--- 202510/20251001.py
+++ 202510/20251001.py
@@ -13,6 +13,22 @@ class Solution:
     Given the two integers `num_bottles` and `num_exchange`, return *the **maximum**
     number of water bottles you can drink*."""
 
-    def num_water_bottles(self, num_bottles: int, num_exchange: int) -> int: ...
+    def num_water_bottles(self, num_bottles: int, num_exchange: int) -> int:
+        # Initialize the total number of bottles drunk
+        total_drunk = 0
+        # Track the number of empty bottles
+        empty_bottles = 0
+        # Use a loop to simulate drinking and exchanging until no more full bottles can be obtained
+        while num_bottles > 0:
+            # Add the current full bottles to the total drunk
+            total_drunk += num_bottles
+            # Convert the drunk bottles to empty ones
+            empty_bottles += num_bottles
+            # Calculate how many new full bottles can be obtained by exchanging empties
+            num_bottles = empty_bottles // num_exchange
+            # Update empties to the remainder after exchange
+            empty_bottles = empty_bottles % num_exchange
+        # Return the total bottles drunk
+        return total_drunk
 
     numWaterBottles = num_water_bottles
