1b4bced29c8b2782209fe8db09e8e4a78cf109d2
Joseph Chiocchi <joe@yolk.cc>
2025-10-01 00:29:40 +0000

    Boilerplate for 20251001
    https://leetcode.com/problems/water-bottles/

---
+++ 202510/20251001.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/water-bottles/
+
+
+class Solution:
+    """1518. Water Bottles
+
+    There are `num_bottles` water bottles that are initially full of water. You can
+    exchange `num_exchange` empty water bottles from the market with one full water
+    bottle.
+
+    The operation of drinking a full water bottle turns it into an empty bottle.
+
+    Given the two integers `num_bottles` and `num_exchange`, return *the **maximum**
+    number of water bottles you can drink*."""
+
+    def num_water_bottles(self, num_bottles: int, num_exchange: int) -> int: ...
+
+    numWaterBottles = num_water_bottles
