85c598ae103bb6e589d4a66db09a85b4bc402be8
Joseph Chiocchi <input.output@gmail.com>
2025-10-15 20:08:32 -0500

    solution 20251016.py
    - 175ms
    - 38.30mb (beats 62.77%)

--- 202510/20251016.py
+++ 202510/20251016.py
@@ -1,4 +1,5 @@
 # https://leetcode.com/problems/smallest-missing-non-negative-integer-after-operations/
+from collections import Counter
 
 
 class Solution:
@@ -19,6 +20,22 @@ class Solution:
     Return *the maximum MEX of* `nums` *after applying the mentioned operation **any
     number of times***."""
 
-    def find_smallest_integer(self, nums: list[int], value: int) -> int: ...
+    def find_smallest_integer(self, nums: list[int], value: int) -> int:
+        # Compute frequency of each residue modulo value
+        freq = Counter(num % value for num in nums)
+        # Start from potential MEX 0
+        mex = 0
+        while True:
+            # Compute required residue for current mex
+            r = mex % value
+            # If a worker is available for this residue, use it
+            if freq[r] > 0:
+                # Decrement the count for this residue
+                freq[r] -= 1
+                # Move to next potential MEX
+                mex += 1
+            else:
+                # Cannot cover this mex, return it as the maximum achievable MEX
+                return mex
 
     findSmallestInteger = find_smallest_integer
