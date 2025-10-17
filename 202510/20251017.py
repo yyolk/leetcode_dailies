f322933ae9764ee0876dad4c096cd3ac78c6592a
Joseph Chiocchi <input.output@gmail.com>
2025-10-16 20:10:32 -0500

    solution 20251017.py
    - 987ms
    - 257.66mb

--- 202510/20251017.py
+++ 202510/20251017.py
@@ -1,4 +1,5 @@
 # https://leetcode.com/problems/maximize-the-number-of-partitions-after-operations/
+from functools import cache
 
 
 class Solution:
@@ -20,6 +21,47 @@ class Solution:
     Return an integer denoting the **maximum** number of resulting partitions after the
     operations by optimally choosing at most one index to change."""
 
-    def max_partitions_after_operations(self, s: str, k: int) -> int: ...
+    def max_partitions_after_operations(self, s: str, k: int) -> int:
+        # Memoized recursive function
+        @cache
+        def dfs(index: int, current_chars: int, can_change: int) -> int:
+            # Base case: end of string reached, count the current partition
+            if index >= n:
+                return 1
+
+            # Bit for the current character
+            char_bit = 1 << (ord(s[index]) - ord('a'))
+
+            # Attempt to add current character to the partition
+            next_chars = current_chars | char_bit
+
+            # If adding exceeds k distinct, start a new partition
+            if next_chars.bit_count() > k:
+                result = dfs(index + 1, char_bit, can_change) + 1
+            else:
+                # Continue with the current partition
+                result = dfs(index + 1, next_chars, can_change)
+
+            # If change is still available, try changing the current character
+            if can_change:
+                for letter_index in range(26):
+                    # Bit for the new letter
+                    new_bit = 1 << letter_index
+                    next_chars = current_chars | new_bit
+
+                    # If adding the new letter exceeds k, start a new partition
+                    if next_chars.bit_count() > k:
+                        result = max(result, dfs(index + 1, new_bit, 0) + 1)
+                    else:
+                        # Continue with the current partition
+                        result = max(result, dfs(index + 1, next_chars, 0))
+
+            return result
+
+        # Length of the string
+        n = len(s)
+
+        # Start from index 0 with empty mask and change available
+        return dfs(0, 0, 1)
 
     maxPartitionsAfterOperations = max_partitions_after_operations
