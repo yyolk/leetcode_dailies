ab0e3f632d94bae9e041b4e165a9b7dd3b82f50b
Joseph Chiocchi <joe@yolk.cc>
2025-10-22 00:27:33 +0000

    Boilerplate for 20251022
    https://leetcode.com/problems/maximum-frequency-of-an-element-after-performing-operations-ii/

---
+++ 202510/20251022.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/maximum-frequency-of-an-element-after-performing-operations-ii/
+
+
+class Solution:
+    """3347. Maximum Frequency of an Element After Performing Operations II
+
+    You are given an integer array `nums` and two integers `k` and `num_operations`.
+
+    You must perform an **operation** `num_operations` times on `nums`, where in each
+    operation you:
+
+    * Select an index `i` that was **not** selected in any previous operations.
+
+    * Add an integer in the range `[-k, k]` to `nums[i]`.
+
+    Return the **maximum** possible frequency of any element in `nums` after performing
+    the **operations**."""
+
+    def max_frequency(self, nums: list[int], k: int, num_operations: int) -> int: ...
+
+    maxFrequency = max_frequency
--- 202510/README.md
+++ 202510/README.md
@@ -146,7 +146,9 @@
    </a>
   </td>
   <td class="wed">
-   22
+   <a href="20251022.py">
+    22
+   </a>
   </td>
   <td class="thu">
    23
