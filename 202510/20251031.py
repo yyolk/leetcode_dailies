5e40b4f47636724f03435daa301ff62fb26be305
Joseph Chiocchi <joe@yolk.cc>
2025-10-31 01:12:24 +0000

    Boilerplate for 20251031
    https://leetcode.com/problems/the-two-sneaky-numbers-of-digitville/

---
+++ 202510/20251031.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/the-two-sneaky-numbers-of-digitville/
+
+
+class Solution:
+    """3289. The Two Sneaky Numbers of Digitville
+
+    In the town of Digitville, there was a list of numbers called `nums` containing
+    integers from `0` to `n - 1`. Each number was supposed to appear **exactly once** in
+    the list, however, **two** mischievous numbers sneaked in an *additional time*,
+    making the list longer than usual.
+
+    As the town detective, your task is to find these two sneaky numbers. Return an
+    array of size **two** containing the two numbers (in *any order*), so peace can
+    return to Digitville."""
+
+    def get_sneaky_numbers(self, nums: list[int]) -> list[int]: ...
+
+    getSneakyNumbers = get_sneaky_numbers
--- 202510/README.md
+++ 202510/README.md
@@ -193,7 +193,9 @@
    </a>
   </td>
   <td class="fri">
-   31
+   <a href="20251031.py">
+    31
+   </a>
   </td>
   <td class="noday">
   </td>
