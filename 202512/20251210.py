46d6c28c4864b88b1bbf3e7d5983c46b2c9d8533
Joseph Chiocchi <input.output@gmail.com>
2025-12-09 20:25:46 -0600

    solution 20251210.py
    - 18ms (beats 69.44%)
    - 31.88mb (beats 82.78%)

---
+++ 202512/20251210.py
@@ -0,0 +1,46 @@
+# https://leetcode.com/problems/count-the-number-of-computer-unlocking-permutations
+
+
+class Solution:
+    """3577. Count the Number of Computer Unlocking Permutations
+
+    You are given an array complexity of length n.
+
+    There are n locked computers in a room with labels from 0 to n - 1,
+    each with its own unique password. The password of the computer i has a
+    complexity complexity[i].
+
+    The password for the computer labeled 0 is already decrypted and serves
+    as the root. All other computers must be unlocked using it or another
+    previously unlocked computer, following this information:
+
+    You can decrypt the password for the computer i using the password for
+    computer j, where j is any integer less than i with a lower complexity.
+    (i.e. j < i and complexity[j] < complexity[i])
+    To decrypt the password for computer i, you must have already unlocked a
+    computer j such that j < i and complexity[j] < complexity[i].
+    Find the number of permutations of [0, 1, 2, ..., (n - 1)] that represent
+    a valid order in which the computers can be unlocked, starting from
+    computer 0 as the only initially unlocked one.
+
+    Since the answer may be large, return it modulo 109 + 7.
+
+    Note that the password for the computer with label 0 is decrypted, and
+    not the computer with the first position in the permutation.
+    """
+    def count_permutations(self, complexity: list[int]) -> int:
+        # MOD for the result
+        MOD = 10**9 + 7
+        n = len(complexity)
+        # Check if complexity[0] is strictly less than all complexity[i] for i >= 1
+        # If not, impossible to unlock all, return 0
+        for i in range(1, n):
+            if complexity[i] <= complexity[0]:
+                return 0
+        # Otherwise, all can be unlocked in any order after 0, so (n-1)! % MOD
+        res = 1
+        for i in range(1, n):
+            res = (res * i) % MOD
+        return res
+
+    countPermutations = count_permutations
