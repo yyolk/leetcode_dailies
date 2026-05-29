0badce654c497f9bd9d5cd55e71d081b264949db
Joseph Chiocchi <joe@yolk.cc>
2026-05-29 01:03:07 +0000

    Boilerplate for 20260529
    https://leetcode.com/problems/minimum-element-after-replacement-with-digit-sum/

---
+++ solutions/2026/202605/20260529.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/minimum-element-after-replacement-with-digit-sum/
+
+
+class Solution:
+    """3300. Minimum Element After Replacement With Digit Sum
+
+    You are given an integer array `nums`.
+
+    You replace each element in `nums` with the **sum** of its digits.
+
+    Return the **minimum** element in `nums` after all replacements."""
+
+    def min_element(self, nums: list[int]) -> int: ...
+
+    minElement = min_element
