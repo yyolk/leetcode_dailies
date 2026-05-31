5d99fe18d9d9e4d5b9d36cea842449271a141794
Joseph Chiocchi <input.output@gmail.com>
2026-05-30 20:09:51 -0500

    solution 20260531.py
    - 71ms (beats 73.70%)
    - 33.86mb (beats 75.43%)

--- solutions/2026/202605/20260531.py
+++ solutions/2026/202605/20260531.py
@@ -1,21 +1,28 @@
 # https://leetcode.com/problems/destroying-asteroids/
 
-
 class Solution:
     """2126. Destroying Asteroids
-
-    You are given an integer `mass`, which represents the original mass of a planet. You
-    are further given an integer array `asteroids`, where `asteroids[i]` is the mass of
-    the `ith` asteroid.
-
-    You can arrange for the planet to collide with the asteroids in **any arbitrary
-    order**. If the mass of the planet is **greater than or equal to** the mass of the
-    asteroid, the asteroid is **destroyed** and the planet **gains** the mass of the
+    
+    You are given an integer mass, which represents the original mass of a planet.
+    You are further given an integer array asteroids, where asteroids[i] is the mass
+    of the ith asteroid.
+    
+    You can arrange for the planet to collide with the asteroids in any arbitrary
+    order. If the mass of the planet is greater than or equal to the mass of the
+    asteroid, the asteroid is destroyed and the planet gains the mass of the
     asteroid. Otherwise, the planet is destroyed.
-
-    Return `true` *if **all** asteroids can be destroyed. Otherwise, return* `false`*.*
+    
+    Return true if all asteroids can be destroyed. Otherwise, return false.
     """
-
-    def asteroids_destroyed(self, mass: int, asteroids: list[int]) -> bool: ...
+    def asteroids_destroyed(self, mass: int, asteroids: list[int]) -> bool:
+        # Sort asteroids ascending: collide with smallest first to grow planet
+        asteroids.sort()
+        for asteroid in asteroids:
+            # Cannot destroy this asteroid (or any remaining)
+            if mass < asteroid:
+                return False
+            # Destroy and add mass to planet
+            mass += asteroid
+        return True
 
     asteroidsDestroyed = asteroids_destroyed
