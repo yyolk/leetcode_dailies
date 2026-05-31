b16a0046e141944c658abff9396f520cdec040fe
Joseph Chiocchi <joe@yolk.cc>
2026-05-31 01:03:52 +0000

    Boilerplate for 20260531
    https://leetcode.com/problems/destroying-asteroids/

---
+++ solutions/2026/202605/20260531.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/destroying-asteroids/
+
+
+class Solution:
+    """2126. Destroying Asteroids
+
+    You are given an integer `mass`, which represents the original mass of a planet. You
+    are further given an integer array `asteroids`, where `asteroids[i]` is the mass of
+    the `ith` asteroid.
+
+    You can arrange for the planet to collide with the asteroids in **any arbitrary
+    order**. If the mass of the planet is **greater than or equal to** the mass of the
+    asteroid, the asteroid is **destroyed** and the planet **gains** the mass of the
+    asteroid. Otherwise, the planet is destroyed.
+
+    Return `true` *if **all** asteroids can be destroyed. Otherwise, return* `false`*.*
+    """
+
+    def asteroids_destroyed(self, mass: int, asteroids: list[int]) -> bool: ...
+
+    asteroidsDestroyed = asteroids_destroyed
