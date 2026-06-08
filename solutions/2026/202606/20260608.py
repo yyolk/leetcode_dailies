04dfdf1894c51159e874282eaee46344e7ca211c
Joseph Chiocchi <input.output@gmail.com>
2026-06-07 20:09:53 -0500

    solution 20260608.py
    - 21ms (beats 87.67%)
    - 33.80mb (beats 55.83%)

--- solutions/2026/202606/20260608.py
+++ solutions/2026/202606/20260608.py
@@ -1,27 +1,36 @@
 # https://leetcode.com/problems/partition-array-according-to-given-pivot/
 
-
 class Solution:
     """2161. Partition Array According to Given Pivot
-
-    You are given a **0-indexed** integer array `nums` and an integer `pivot`. Rearrange
-    `nums` such that the following conditions are satisfied:
-
-    * Every element less than `pivot` appears **before** every element greater than
-    `pivot`.
-
-    * Every element equal to `pivot` appears **in between** the elements less than and
-    greater than `pivot`.
-
-    * The **relative order** of the elements less than `pivot` and the elements greater
-    than `pivot` is maintained.
-
-      + More formally, consider every `pi`, `pj` where `pi` is the new position of the
-    `ith` element and `pj` is the new position of the `jth` element. If `i < j` and
-    **both** elements are smaller (*or larger*) than `pivot`, then `pi < pj`.
-
-    Return `nums` *after the rearrangement.*"""
-
-    def pivot_array(self, nums: list[int], pivot: int) -> list[int]: ...
+    
+    You are given a 0-indexed integer array nums and an integer pivot. Rearrange
+    nums such that the following conditions are satisfied:
+    * Every element less than pivot appears before every element greater than
+      pivot.
+    * Every element equal to pivot appears in between the elements less than and
+      greater than pivot.
+    * The relative order of the elements less than pivot and the elements greater
+      than pivot is maintained.
+      + More formally, consider every pi, pj where pi is the new position of the
+        ith element and pj is the new position of the jth element. If i < j and
+        both elements are smaller (or larger) than pivot, then pi < pj.
+    Return nums after the rearrangement.
+    """
+    def pivot_array(self, nums: list[int], pivot: int) -> list[int]:
+        # Three lists preserve relative order while partitioning
+        less = []      # elements < pivot
+        equal = []     # elements == pivot
+        greater = []   # elements > pivot
+        
+        for num in nums:
+            if num < pivot:
+                less.append(num)
+            elif num == pivot:
+                equal.append(num)
+            else:
+                greater.append(num)
+        
+        # Concatenate to form the final array (O(n) time/space)
+        return less + equal + greater
 
     pivotArray = pivot_array
