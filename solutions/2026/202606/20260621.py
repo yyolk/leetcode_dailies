e87816aad443871b363900c62707df28c44e2699
Joseph Chiocchi <input.output@gmail.com>
2026-06-20 23:27:27 -0500

    solution 20260621.py
    - 77ms (beats 72.71%)
    - 30.54mb (beats 98.91%)

--- solutions/2026/202606/20260621.py
+++ solutions/2026/202606/20260621.py
@@ -1,33 +1,34 @@
 # https://leetcode.com/problems/maximum-ice-cream-bars/
 
-
 class Solution:
     """1833. Maximum Ice Cream Bars
 
     It is a sweltering summer day, and a boy wants to buy some ice cream bars.
+    At the store, there are n ice cream bars. You are given an array costs of
+    length n, where costs[i] is the price of the ith ice cream bar in coins.
+    The boy initially has coins coins to spend, and he wants to buy as many ice
+    cream bars as possible.
 
-    At the store, there are `n` ice cream bars. You are given an array `costs` of length
-    `n`, where `costs[i]` is the price of the `ith` ice cream bar in coins. The boy
-    initially has `coins` coins to spend, and he wants to buy as many ice cream bars as
-    possible.
+    Note: The boy can buy the ice cream bars in any order.
 
-    **Note:** The boy can buy the ice cream bars in any order.
-
-    Return *the **maximum** number of ice cream bars the boy can buy with* `coins`
-    *coins.*
-
+    Return the maximum number of ice cream bars the boy can buy with coins coins.
     You must solve the problem by counting sort.
-
-    Constraints:
-
-    * `costs.length == n`
-
-    * `1 <= n <= 105`
-
-    * `1 <= costs[i] <= 105`
-
-    * `1 <= coins <= 108`"""
-
-    def max_ice_cream(self, costs: list[int], coins: int) -> int: ...
+    """
+    def max_ice_cream(self, costs: list[int], coins: int) -> int:
+        # Count freq of each possible cost (1 <= costs[i] <= 10^5)
+        MAX = 100000
+        count = [0] * (MAX + 1)
+        for cost in costs:
+            count[cost] += 1
+        bars = 0
+        # Buy cheapest possible using counting sort iteration
+        for price in range(1, MAX + 1):
+            if coins < price:
+                # Cannot afford this or any higher price
+                break
+            can_buy = min(count[price], coins // price)
+            bars += can_buy
+            coins -= can_buy * price
+        return bars
 
     maxIceCream = max_ice_cream
