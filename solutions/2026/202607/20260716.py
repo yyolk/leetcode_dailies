6db6703f76eb348ff61074c4f28b711077994e1d
Joseph Chiocchi <input.output@gmail.com>
2026-07-15 21:11:45 -0500

    solution 20260716.py
    - 195ms (beats 62.87%)
    - 33.41mb (beats 85.63%)

--- solutions/2026/202607/20260716.py
+++ solutions/2026/202607/20260716.py
@@ -1,41 +1,38 @@
 # https://leetcode.com/problems/sum-of-gcd-of-formed-pairs/
 
+from math import gcd
 
 class Solution:
     """3867. Sum of GCD of Formed Pairs
-
-    You are given an integer array `nums` of length `n`.
-
-    Construct an array `prefixGcd` where for each index `i`:
-
-    * Let `mxi = max(nums[0], nums[1], ..., nums[i])`.
-
-    * `prefixGcd[i] = gcd(nums[i], mxi)`.
-
-    After constructing `prefixGcd`:
-
-    * Sort `prefixGcd` in **non-decreasing** order.
-
-    * Form pairs by taking the **smallest unpaired** element and the **largest
-    unpaired** element.
-
-    * Repeat this process until no more pairs can be formed.
-
-    * For each formed pair, **compute** the `gcd` of the two elements.
-
-    * If `n` is odd, the **middle** element in the `prefixGcd` array remains
-    **unpaired** and should be ignored.
-
-    Return an integer denoting the **sum of the GCD** values of all formed pairs.
-
-    The term `gcd(a, b)` denotes the **greatest common divisor** of `a` and `b`.
-
-    Constraints:
-
-    * `1 <= n == nums.length <= 105`
-
-    * `1 <= nums[i] <= 10\u200b\u200b\u200b\u200b\u200b\u200b\u200b9`"""
-
-    def gcd_sum(self, nums: list[int]) -> int: ...
+    
+    You are given an integer array nums of length n. Construct an array
+    prefixGcd where for each index i: Let mxi = max(nums[0], nums[1], ...,
+    nums[i]). prefixGcd[i] = gcd(nums[i], mxi). After constructing prefixGcd:
+    Sort prefixGcd in non-decreasing order. Form pairs by taking the smallest
+    unpaired element and the largest unpaired element. Repeat this process
+    until no more pairs can be formed. For each formed pair, compute the gcd
+    of the two elements. If n is odd, the middle element in the prefixGcd
+    array remains unpaired and should be ignored. Return an integer denoting
+    the sum of the GCD values of all formed pairs. The term gcd(a, b)
+    denotes the greatest common divisor of a and b.
+    """
+    def gcd_sum(self, nums: list[int]) -> int:
+        n = len(nums)
+        if n == 0:
+            return 0
+        prefix_gcd = [0] * n
+        running_max = 0
+        for i in range(n):
+            running_max = max(running_max, nums[i])
+            # prefixGcd[i] = gcd(nums[i], max so far)
+            prefix_gcd[i] = gcd(nums[i], running_max)
+        # sort in non-decreasing order
+        prefix_gcd.sort()
+        total = 0
+        # pair smallest with largest, next smallest with next largest, etc.
+        for i in range(n // 2):
+            # gcd of pair (prefix_gcd[i], prefix_gcd[n-1-i])
+            total += gcd(prefix_gcd[i], prefix_gcd[n - 1 - i])
+        return total
 
     gcdSum = gcd_sum
