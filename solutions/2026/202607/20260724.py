c7b8eb5a9e80dc353057bfc74a0f2670f810d140
Joseph Chiocchi <joe@yolk.cc>
2026-07-24 00:39:28 +0000

    Boilerplate for 20260724
    https://leetcode.com/problems/number-of-unique-xor-triplets-ii/

---
+++ solutions/2026/202607/20260724.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/number-of-unique-xor-triplets-ii/
+
+
+class Solution:
+    """3514. Number of Unique XOR Triplets II
+
+    You are given an integer array `nums`.
+
+    A **XOR triplet** is defined as the XOR of three elements `nums[i] XOR nums[j] XOR
+    nums[k]` where `i <= j <= k`.
+
+    Return the number of **unique** XOR triplet values from all possible triplets `(i,
+    j, k)`.
+
+    Constraints:
+
+    * `1 <= nums.length <= 1500`
+
+    * `1 <= nums[i] <= 1500`"""
+
+    def unique_xor_triplets(self, nums: list[int]) -> int: ...
+
+    uniqueXorTriplets = unique_xor_triplets
