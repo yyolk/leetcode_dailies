fde150f951cd7f63d836c98b842a6065b0ad9d3f
Joseph Chiocchi <input.output@gmail.com>
2026-07-26 20:56:07 -0500

    solution 20260727.py 💯
    - 0ms (beats 100%) 💯
    - 19.29mb (beats 68.04%)

--- solutions/2026/202607/20260727.py
+++ solutions/2026/202607/20260727.py
@@ -1,18 +1,27 @@
 # https://leetcode.com/problems/maximum-product-of-two-elements-in-an-array/
 
-
 class Solution:
     """1464. Maximum Product of Two Elements in an Array
 
-    Given the array of integers `nums`, you will choose two different indices `i` and
-    `j` of that array. *Return the maximum value of* `(nums[i]-1)*(nums[j]-1)`.
-
+    Given the array of integers `nums`, you will choose two different indices `i`
+    and `j` of that array. *Return the maximum value of*
+    `(nums[i]-1)*(nums[j]-1)`.
     Constraints:
-
     * `2 <= nums.length <= 500`
-
-    * `1 <= nums[i] <= 10^3`"""
-
-    def max_product(self, nums: list[int]) -> int: ...
+    * `1 <= nums[i] <= 10^3`
+    """
+    def max_product(self, nums: list[int]) -> int:
+        # Initialize trackers for the two largest values
+        max1 = max2 = 0
+        for num in nums:
+            # If current exceeds the largest, demote previous largest
+            if num > max1:
+                max2 = max1
+                max1 = num
+            # Else update second-largest if needed
+            elif num > max2:
+                max2 = num
+        # Return the product after subtracting 1 from each
+        return (max1 - 1) * (max2 - 1)
 
     maxProduct = max_product
