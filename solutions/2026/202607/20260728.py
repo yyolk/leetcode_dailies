e412715b02d1321eef8009ceeecd440c773c5a6b
Joseph Chiocchi <joe@yolk.cc>
2026-07-28 00:40:19 +0000

    Boilerplate for 20260728
    https://leetcode.com/problems/smallest-palindromic-rearrangement-i/

---
+++ solutions/2026/202607/20260728.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/smallest-palindromic-rearrangement-i/
+
+
+class Solution:
+    """3517. Smallest Palindromic Rearrangement I
+
+    You are given a **palindromic** string `s`.
+
+    Return the **lexicographically smallest** palindromic permutation of `s`.
+
+    Constraints:
+
+    * `1 <= s.length <= 105`
+
+    * `s` consists of lowercase English letters.
+
+    * `s` is guaranteed to be palindromic."""
+
+    def smallest_palindrome(self, s: str) -> str: ...
+
+    smallestPalindrome = smallest_palindrome
