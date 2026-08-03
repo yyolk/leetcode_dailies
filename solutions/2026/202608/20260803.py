304e59754a4c87198b785e84aee2d7227842eb85
Joseph Chiocchi <joe@yolk.cc>
2026-08-03 00:44:06 +0000

    Boilerplate for 20260803
    https://leetcode.com/problems/stone-game-iii/

---
+++ solutions/2026/202608/20260803.py
@@ -0,0 +1,45 @@
+# https://leetcode.com/problems/stone-game-iii/
+
+
+class Solution:
+    """1406. Stone Game III
+
+    Alice and Bob continue their games with piles of stones. There are several stones
+    **arranged in a row**, and each stone has an associated value which is an integer
+    given in the array `stone_value`.
+
+    Alice and Bob take turns, with Alice starting first. On each player's turn, that
+    player can take `1`, `2`, or `3` stones from the **first** remaining stones in the
+    row.
+
+    The score of each player is the sum of the values of the stones taken. The score of
+    each player is `0` initially.
+
+    The objective of the game is to end with the highest score, and the winner is the
+    player with the highest score and there could be a tie. The game continues until all
+    the stones have been taken.
+
+    Assume Alice and Bob **play optimally**.
+
+    Return `"Alice"` *if Alice will win,* `"Bob"` *if Bob will win, or* `"Tie"` *if they
+    will end the game with the same score*.
+
+    Constraints:
+
+    * `1 <= stone_value.length <= 5 * 104`
+
+    * `-1000 <= stone_value[i] <= 1000`"""
+
+    def stone_game_i_i_i(self, stone_value: list[int]) -> str:
+        """...
+
+        Proposed solution ...
+
+        Args:
+            stone_value (list of int): ...
+
+        Returns:
+            str: ..."""
+        ...
+
+    stoneGameIII = stone_game_i_i_i
