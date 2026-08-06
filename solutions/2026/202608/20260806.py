6bf0b7c864c62f71460d3b145522afc6db11b0c2
Joseph Chiocchi <joe@yolk.cc>
2026-08-06 02:46:38 +0000

    Boilerplate for 20260806
    https://leetcode.com/problems/smallest-divisible-digit-product-i/

---
+++ solutions/2026/202608/20260806.py
@@ -0,0 +1,29 @@
+# https://leetcode.com/problems/smallest-divisible-digit-product-i/
+
+
+class Solution:
+    """3345. Smallest Divisible Digit Product I
+
+    You are given two integers `n` and `t`. Return the **smallest** number greater than
+    or equal to `n` such that the **product of its digits** is divisible by `t`.
+
+    Constraints:
+
+    * `1 <= n <= 100`
+
+    * `1 <= t <= 10`"""
+
+    def smallest_number(self, n: int, t: int) -> int:
+        """...
+
+        Proposed solution ...
+
+        Args:
+            n (int): ...
+            t (int): ...
+
+        Returns:
+            int: ..."""
+        ...
+
+    smallestNumber = smallest_number
