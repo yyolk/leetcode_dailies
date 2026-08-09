7e76056674f542d7f1c6e83e84972446dcd9dd51
Joseph Chiocchi <joe@yolk.cc>
2026-08-09 01:55:14 +0000

    Boilerplate for 20260809
    https://leetcode.com/problems/stone-game-ii/

---
+++ solutions/2026/202608/20260809.py
@@ -0,0 +1,40 @@
+# https://leetcode.com/problems/stone-game-ii/
+
+
+class Solution:
+    """1140. Stone Game II
+
+    Alice and Bob continue their games with piles of stones. There are a number of piles
+    **arranged in a row**, and each pile has a positive integer number of stones
+    `piles[i]`. The objective of the game is to end with the most stones.
+
+    Alice and Bob take turns, with Alice starting first.
+
+    On each player's turn, that player can take **all the stones** in the **first** `X`
+    remaining piles, where `1 <= X <= 2M`. Then, we set `M = max(M, X)`. Initially, M =
+    1.
+
+    The game continues until all the stones have been taken.
+
+    Assuming Alice and Bob play optimally, return the maximum number of stones Alice can
+    get.
+
+    Constraints:
+
+    * `1 <= piles.length <= 100`
+
+    * `1 <= piles[i] <= 104`"""
+
+    def stone_game_i_i(self, piles: list[int]) -> int:
+        """...
+
+        Proposed solution ...
+
+        Args:
+            piles (list of int): ...
+
+        Returns:
+            int: ..."""
+        ...
+
+    stoneGameII = stone_game_i_i
