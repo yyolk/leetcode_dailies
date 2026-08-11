9b75d75e4167421f4c5c807f4ffe3341be43ed9e
Joseph Chiocchi <joe@yolk.cc>
2026-08-11 01:55:30 +0000

    Boilerplate for 20260811
    https://leetcode.com/problems/smallest-missing-integer-greater-than-sequential-prefix-sum/

---
+++ solutions/2026/202608/20260811.py
@@ -0,0 +1,33 @@
+# https://leetcode.com/problems/smallest-missing-integer-greater-than-sequential-prefix-sum/
+
+
+class Solution:
+    """2996. Smallest Missing Integer Greater Than Sequential Prefix Sum
+
+    You are given a **0-indexed** array of integers `nums`.
+
+    A prefix `nums[0..i]` is **sequential** if, for all `1 <= j <= i`, `nums[j] = nums[j
+    - 1] + 1`. In particular, the prefix consisting only of `nums[0]` is **sequential**.
+
+    Return *the **smallest** integer* `x` *missing from* `nums` *such that* `x` *is
+    greater than or equal to the sum of the **longest** sequential prefix.*
+
+    Constraints:
+
+    * `1 <= nums.length <= 50`
+
+    * `1 <= nums[i] <= 50`"""
+
+    def missing_integer(self, nums: list[int]) -> int:
+        """...
+
+        Proposed solution ...
+
+        Args:
+            nums (list of int): ...
+
+        Returns:
+            int: ..."""
+        ...
+
+    missingInteger = missing_integer
--- solutions/2026/202608/README.md
+++ solutions/2026/202608/README.md
@@ -95,7 +95,9 @@
    </a>
   </td>
   <td class="tue">
-   11
+   <a href="20260811.py">
+    11
+   </a>
   </td>
   <td class="wed">
    12
