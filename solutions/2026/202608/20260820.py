5bc7cafd79cb90c0bead22d7bf88d6764ed3389a
Joseph Chiocchi <joe@yolk.cc>
2026-08-20 01:21:41 +0000

    Boilerplate for 20260820
    https://leetcode.com/problems/distribute-elements-into-two-arrays-i/

---
+++ solutions/2026/202608/20260820.py
@@ -0,0 +1,42 @@
+# https://leetcode.com/problems/distribute-elements-into-two-arrays-i/
+
+
+class Solution:
+    """3069. Distribute Elements Into Two Arrays I
+
+    You are given a **1-indexed** array of **distinct** integers `nums` of length `n`.
+
+    You need to distribute all the elements of `nums` between two arrays `arr1` and
+    `arr2` using `n` operations. In the first operation, append `nums[1]` to `arr1`. In
+    the second operation, append `nums[2]` to `arr2`. Afterwards, in the `ith`
+    operation:
+
+    * If the last element of `arr1` is **greater** than the last element of `arr2`,
+    append `nums[i]` to `arr1`. Otherwise, append `nums[i]` to `arr2`.
+
+    The array `result` is formed by concatenating the arrays `arr1` and `arr2`. For
+    example, if `arr1 == [1,2,3]` and `arr2 == [4,5,6]`, then `result = [1,2,3,4,5,6]`.
+
+    Return *the array* `result`.
+
+    Constraints:
+
+    * `3 <= n <= 50`
+
+    * `1 <= nums[i] <= 100`
+
+    * All elements in `nums` are distinct."""
+
+    def result_array(self, nums: list[int]) -> list[int]:
+        """...
+
+        Proposed solution ...
+
+        Args:
+            nums (list of int): ...
+
+        Returns:
+            list of int: ..."""
+        ...
+
+    resultArray = result_array
