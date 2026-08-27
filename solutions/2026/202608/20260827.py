ddf5cbd39b33110b5cc0833274cb32887c5ea271
Joseph Chiocchi <joe@yolk.cc>
2026-08-27 08:15:24 +0000

    Boilerplate for 20260827
    https://leetcode.com/problems/lexicographically-smallest-permutation-greater-than-target/

---
+++ solutions/2026/202608/20260827.py
@@ -0,0 +1,37 @@
+# https://leetcode.com/problems/lexicographically-smallest-permutation-greater-than-target/
+
+
+class Solution:
+    """3720. Lexicographically Smallest Permutation Greater Than Target
+
+    You are given two strings `s` and `target`, both having length `n`, consisting of
+    lowercase English letters.
+
+    Return the **lexicographically smallest permutation** of `s` that is **strictly**
+    greater than `target`. If no permutation of `s` is lexicographically strictly
+    greater than `target`, return an empty string.
+
+    A string `a` is **lexicographically strictly greater** than a string `b` (of the
+    same length) if in the first position where `a` and `b` differ, string `a` has a
+    letter that appears later in the alphabet than the corresponding letter in `b`.
+
+    Constraints:
+
+    * `1 <= s.length == target.length <= 300`
+
+    * `s` and `target` consist of only lowercase English letters."""
+
+    def lex_greater_permutation(self, s: str, target: str) -> str:
+        """...
+
+        Proposed solution ...
+
+        Args:
+            s (str): ...
+            target (str): ...
+
+        Returns:
+            str: ..."""
+        ...
+
+    lexGreaterPermutation = lex_greater_permutation
