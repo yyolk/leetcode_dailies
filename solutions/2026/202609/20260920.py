220943497d94034bb61c9efff8819b0e5ea058d0
Joseph Chiocchi <joe@yolk.cc>
2026-09-20 04:03:52 +0000

    Boilerplate for 20260920
    https://leetcode.com/problems/reverse-degree-of-a-string/

---
+++ solutions/2026/202609/20260920.py
@@ -0,0 +1,36 @@
+# https://leetcode.com/problems/reverse-degree-of-a-string/
+
+
+class Solution:
+    """3498. Reverse Degree of a String
+
+    Given a string `s`, calculate its **reverse degree**.
+
+    The **reverse degree** is calculated as follows:
+
+    1. For each character, multiply its position in the *reversed* alphabet (`'a'` = 26,
+    `'b'` = 25, ..., `'z'` = 1) with its position in the string **(1-indexed)**.
+
+    2. Sum these products for all characters in the string.
+
+    Return the **reverse degree** of `s`.
+
+    Constraints:
+
+    * `1 <= s.length <= 1000`
+
+    * `s` contains only lowercase English letters."""
+
+    def reverse_degree(self, s: str) -> int:
+        """...
+
+        Proposed solution ...
+
+        Args:
+            s (str): ...
+
+        Returns:
+            int: ..."""
+        ...
+
+    reverseDegree = reverse_degree
